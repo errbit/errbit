401eb5670df6a42652080646388c80e036e1ad62
Jared Pace <jared@codewordstudios.com>
2010-08-02 19:48:45 -0500

    Initial Commit - Rails3 + RSpec2 + Haml + Mongoid

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.0.rc'
+gem 'nokogiri'
+gem 'bson_ext', :require => nil
+gem 'mongoid', '2.0.0.beta.15'
+gem 'haml'
+
+group :test do
+  gem 'rspec', '>= 2.0.0.beta.19'
+  gem 'rspec-rails', '>= 2.0.0.beta.19'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hypnotoad::Application.config.secret_token = '41f6b3871b375769786c379a3e542d67a7a234926f926778ed634f40a9480ba6d7b0768993e9852aafcaa73d6b8ddd2ed3b46208b92305e8c21936574f74a9a4'
