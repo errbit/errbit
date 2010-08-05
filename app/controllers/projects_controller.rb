ce442af995f39734dd3d9d827b634b586c476fcd
Jared Pace <jared@codewordstudios.com>
2010-08-05 08:59:32 -0500

    Add full management support for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -9,4 +9,42 @@ class ProjectsController < ApplicationController
     @errs  = @project.errs.paginate
   end
   
+  def new
+    @project = Project.new
+    @project.watchers.build
+  end
+  
+  def edit
+    @project = Project.find(params[:id])
+    @project.watchers.build if @project.watchers.none?
+  end
+  
+  def create
+    @project = Project.new(params[:project])
+    
+    if @project.save
+      flash[:success] = 'Great success! Configure your project with the API key below'
+      redirect_to project_path(@project)
+    else
+      render :new
+    end
+  end
+  
+  def update
+    @project = Project.find(params[:id])
+    
+    if @project.update_attributes(params[:project])
+      flash[:success] = "Good news everyone! '#{@project.name}' was successfully updated."
+      redirect_to project_path(@project)
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    flash[:success] = "'#{@project.name}' was successfully destroyed."
+    redirect_to projects_path
+  end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -8,12 +8,15 @@ class Project
   
   embeds_many :watchers
   embeds_many :deploys
-  references_many :errs
+  references_many :errs, :dependent => :destroy
   
   before_validation :generate_api_key, :on => :create
   
   validates_presence_of :name, :api_key
   validates_uniqueness_of :name, :api_key, :allow_blank => true
+  
+  accepts_nested_attributes_for :watchers,
+    :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }
   
   def self.find_by_api_key!(key)
     where(:api_key => key).first || raise(Mongoid::Errors::DocumentNotFound.new(self,key))
