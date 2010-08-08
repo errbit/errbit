7944485d347487b34223c1afdc65794137672a2e
Jared Pace <jared@codewordstudios.com>
2010-08-08 17:28:48 -0400

    Add a tabbed for the Errs#show page

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,1 +1,31 @@
 // App JS
+
+$(function(){
+  activateTabbedPanels();
+});
+
+function activateTabbedPanels() {
+  $('.tab-bar a').each(function(){
+    var tab = $(this);
+    var panel = $('#'+tab.attr('rel'));
+    panel.addClass('panel');
+    panel.find('h3').hide();
+  })
+  
+  $('.tab-bar a').click(function(){ 
+    activateTab($(this));
+    return(false);
+  });
+  activateTab($('.tab-bar a').first());
+}
+
+function activateTab(tab) {
+  tab = $(tab);
+  var panel = $('#'+tab.attr('rel'));
+  
+  tab.closest('.tab-bar').find('a.active').removeClass('active');
+  tab.addClass('active');
+  
+  $('.panel').hide();
+  panel.show();
+}
