6c0d66abb5114d53cbba25f849233548435aedc4
Barry Hess <barry@bjhess.com>
2010-12-20 10:53:28 -0600

    skip verify authenticity token on DeploysController#create.

--- app/controllers/deploys_controller.rb
+++ app/controllers/deploys_controller.rb
@@ -1,5 +1,6 @@
 class DeploysController < ApplicationController
   
+  skip_before_filter :verify_authenticity_token, :only => :create
   skip_before_filter :authenticate_user!, :only => :create
   
   def create
