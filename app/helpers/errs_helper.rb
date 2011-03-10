e260daf447159c51df9bbbab203937d2ecb90aa7
Nick Recobra <oruen@undev.ru>
2011-03-10 16:05:16 +0300

    Applying config.confirm_resolve_err option to errs list.

--- app/helpers/errs_helper.rb
+++ app/helpers/errs_helper.rb
@@ -3,5 +3,8 @@ module ErrsHelper
   def last_notice_at err
     err.last_notice_at || err.created_at
   end
-  
+
+  def err_confirm
+    Errbit::Config.confirm_resolve_err === false ? nil : 'Seriously?'
+  end
 end
