e51f79752d92465885d0d120d50560f2ea0d7e0b
Nick Recobra <oruen@undev.ru>
2011-03-29 19:48:42 +0400

    Error messages layout fix.

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -6,7 +6,7 @@ module FormHelper
     content_tag(:div, :class => 'error-messages') do
       body  = content_tag(:h2, 'Dang. The following errors are keeping this from being a success.')
       body += content_tag(:ul) do
-        document.errors.full_messages.inject('') {|errs, msg| errs += content_tag(:li, msg) }
+        document.errors.full_messages.inject('') {|errs, msg| errs += content_tag(:li, h(msg)) }.html_safe
       end
     end
   end
