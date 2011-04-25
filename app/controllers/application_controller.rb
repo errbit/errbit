46c690adc14ddc5b3f2d89ed9aec6706e46713ac
Robert Lail <robert.lail@cph.org>
2011-04-25 10:56:39 -0500

    rescue, globally, from RedirectBackError

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,28 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
+  
   before_filter :authenticate_user!
-
+  
   # Devise override - After login, if there is only one app,
   # redirect to that app's path instead of the root path (apps#index).
   def stored_location_for(resource)
     location = super || root_path
     (location == root_path && App.count == 1) ? app_path(App.first) : location
   end
-
-  protected
-
-    def require_admin!
-      redirect_to root_path unless user_signed_in? && current_user.admin?
-    end
-
+  
+  rescue_from ActionController::RedirectBackError, :with => :redirect_to_root
+  
+  
+protected
+  
+  
+  def require_admin!
+    redirect_to_root unless user_signed_in? && current_user.admin?
+  end
+  
+  def redirect_to_root
+    redirect_to(root_path)
+  end
+  
+  
 end
-
