7798d7fd9447c20f440e9cfaf49544fe28fdeed8
Karol Hosiawa <hosiawak@gmail.com>
2011-05-03 14:37:14 +0200

    do not trigger email notifications during migration

--- db/migrate/20110422152027_move_notices_to_separate_collection.rb
+++ db/migrate/20110422152027_move_notices_to_separate_collection.rb
@@ -6,10 +6,14 @@ class MoveNoticesToSeparateCollection < Mongoid::Migration
     errs.each do |err|
       next unless err['notices']
       e = Err.find(err['_id'])
+      # disable email notifications
+      old_notify = e.app.notify_on_errs?
+      e.app.update_attribute(:notify_on_errs, false)
       puts "Copying notices for Err #{err['_id']}"
       err['notices'].each do |notice|
         e.notices.create!(notice)
       end
+      e.app.update_attribute(:notify_on_errs, old_notify)
       mongo_db.collection("errs").update({ "_id" => err['_id']}, { "$unset" => { "notices" => 1}})
     end
     Rake::Task["errbit:db:update_notices_count"].invoke
