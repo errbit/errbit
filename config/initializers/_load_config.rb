083d3470777221a3f7622337dbfd6dcea4f193d7
Nick Recobra <oruenu@gmail.com>
2011-07-02 23:11:02 +0400

    Sendgrid heroku options.

--- config/initializers/_load_config.rb
+++ config/initializers/_load_config.rb
@@ -5,6 +5,14 @@ if ENV['HEROKU']
   Errbit::Config.host = ENV['ERRBIT_HOST']
   Errbit::Config.email_from = ENV['ERRBIT_EMAIL_FROM']
   Errbit::Config.email_at_notices = [1,3,10] #ENV['ERRBIT_EMAIL_AT_NOTICES']
+  Errbit::Application.config.action_mailer.smtp_settings = {
+    :address        => "smtp.sendgrid.net",
+    :port           => "25",
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => ENV['SENDGRID_DOMAIN']
+  }
 else
   yaml = File.read(Rails.root.join('config','config.yml'))
   config = YAML.load(yaml)
