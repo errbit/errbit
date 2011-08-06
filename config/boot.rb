d8e285fd7726248f8bb0118f069510d68b813661
Nathan Broadbent <nathan.f77@gmail.com>
2011-08-06 12:24:01 +0800

    Fix for Ruby 1.9.x: The psych engine for YAML doesn't handle "<<: *defaults" properly.

--- config/boot.rb
+++ config/boot.rb
@@ -1,5 +1,12 @@
 require 'rubygems'
 
+if RUBY_VERSION.to_f >= 1.9
+  # The psych engine for YAML doesn't handle `<<: *defaults` properly.
+  # See: https://github.com/tenderlove/psych/issues/8
+  require 'yaml'
+  YAML::ENGINE.yamler = 'syck'
+end
+
 # Set up gems listed in the Gemfile.
 gemfile = File.expand_path('../../Gemfile', __FILE__)
 begin
@@ -11,3 +18,4 @@ rescue Bundler::GemNotFound => e
   STDERR.puts "Try running `bundle install`."
   exit!
 end if File.exist?(gemfile)
+
