06fd017e58ac0d25bcc3900e03148d22b1b42987
Nick Recobra <oruenu@gmail.com>
2011-08-07 13:08:21 +0400

    Disable dynamic fields.

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -5,5 +5,6 @@ if mongo = ENV['MONGOHQ_URL'] || ENV['MONGOLAB_URI']
   Mongoid.configure do |config|
     config.master = Mongo::Connection.new(settings.host, settings.port).db(database_name)
     config.master.authenticate(settings.user, settings.password) if settings.user
+    config.allow_dynamic_fields = false
   end
 end
