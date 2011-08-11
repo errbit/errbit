0029a95708eb8a3754d1c01db1cd0063f6d6e22d
Nathan Broadbent <nathan.f77@gmail.com>
2011-08-12 00:52:52 +0800

    Apply "disabled" attribute to inputs, not div

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -74,7 +74,7 @@ function activateTypeSelector(field_class, section_class) {
   $('div.'+field_class+' input[name*='+field_class+'_type]').live('click', function(){
     var chosen = $(this).val();
     var wrapper = $(this).closest('.nested');
-    wrapper.find('div.chosen.'+section_class).removeClass('chosen').attr('disabled','disabled');;
+    wrapper.find('div.chosen.'+section_class).removeClass('chosen').find('input').attr('disabled','disabled');
     wrapper.find('div.'+section_class+'.'+chosen).addClass('chosen').find('input').removeAttr('disabled');
   });
 }
