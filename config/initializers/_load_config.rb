19b2c04990f78225bf4c5d59decf4e3da36ab42a
alex@liivid.com <alex@liivid.com>
2011-08-12 10:47:01 -0700

    Load heroku values always if they are available, then override with config.yml

--- config/initializers/_load_config.rb
+++ config/initializers/_load_config.rb
@@ -1,16 +1,8 @@
 require 'ostruct'
 
-config_file = Rails.root.join('config', Rails.env == "test" ? "config.example.yml" : "config.yml")
-Errbit::Config = nil # assign global scope
-if File.exists?(config_file)
-  yaml = File.read(config_file)
-  config = YAML.load(yaml)
+Errbit::Config = OpenStruct.new
 
-  config.merge!(config.delete(Rails.env)) if config.has_key?(Rails.env)
-
-  Errbit::Config = OpenStruct.new(config)
-elsif ENV['HEROKU']
-  Errbit::Config = OpenStruct.new
+if ENV['HEROKU']
   Errbit::Config.host = ENV['ERRBIT_HOST']
   Errbit::Config.email_from = ENV['ERRBIT_EMAIL_FROM']
   Errbit::Config.email_at_notices = [1,3,10] #ENV['ERRBIT_EMAIL_AT_NOTICES']
@@ -22,8 +14,17 @@ elsif ENV['HEROKU']
     :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => ENV['SENDGRID_DOMAIN']
   }
-else
-  raise("Missing config file #{config_file}")
+end
+
+config_file = Rails.root.join('config', Rails.env == "test" ? "config.example.yml" : "config.yml")
+
+if File.exists?(config_file)
+  yaml = File.read(config_file)
+  config = YAML.load(yaml)
+  config.merge!(config.delete(Rails.env)) if config.has_key?(Rails.env)
+  config.each do |k,v|
+    Errbit::Config.send("#{k}=", v)
+  end
 end
 
 # Set config specific values
