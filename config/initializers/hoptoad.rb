1547df7913da56799baca2e09d1413660dc5dac1
Nathan Broadbent <nathan.f77@gmail.com>
2011-08-27 12:22:19 +0800

    Fixed self error-reporting config.

--- config/initializers/hoptoad.rb
+++ config/initializers/hoptoad.rb
@@ -1,8 +1,8 @@
 if Rails.env.production? && Errbit::Config.report_self_errors.to_s != "false"
   HoptoadNotifier.configure do |config|
-    config.api_key = Errbit::Config.api_key || "11e5ce322856e540481e6a0789893179"
-    config.host    = Errbit::Config.host    || "errbit-central.heroku.com"
-    config.port    = Errbit::Config.port    || 80
+    config.api_key = Errbit::Config.self_errors_api_key || "11e5ce322856e540481e6a0789893179"
+    config.host    = Errbit::Config.self_errors_host    || "errbit-central.heroku.com"
+    config.port    = Errbit::Config.self_errors_port    || 80
     config.secure  = config.port == 443
   end
 end
