1bdc92f9b6a1215e85cc3a1bc1b527c0417a2e36
Nathan Broadbent <nathan.f77@gmail.com>
2011-08-31 11:41:14 +0800

    Fixed notices/backtrace spec. Need to set instance variables in a before block.

--- spec/views/notices/_backtrace.html.haml_spec.rb
+++ spec/views/notices/_backtrace.html.haml_spec.rb
@@ -3,16 +3,16 @@ require 'spec_helper'
 describe "notices/_backtrace.html.haml" do
   describe 'missing file in backtrace' do
     before do
-      notice = Factory(:notice, :backtrace => [{
+      @notice = Factory(:notice, :backtrace => [{
         'number'  => rand(999),
         'file'    => nil,
         'method'  => ActiveSupport.methods.shuffle.first
       }])
-      assign :app, notice.err.app
+      assign :app, @notice.err.app
     end
 
     it "should replace nil file with [unknown source]" do
-      render :partial => "notices/backtrace", :locals => {:lines => notice.backtrace}
+      render :partial => "notices/backtrace", :locals => {:lines => @notice.backtrace}
       rendered.should match(/\[unknown source\]/)
     end
   end
