8cedcdc3039a8decdc34a66c1976a6aaee0d0ca9
Nathan Broadbent <nathan.f77@gmail.com>
2011-08-31 11:01:41 +0800

    Extracted test setup into a before block

--- spec/views/notices/_backtrace.html.haml_spec.rb
+++ spec/views/notices/_backtrace.html.haml_spec.rb
@@ -2,13 +2,16 @@ require 'spec_helper'
 
 describe "notices/_backtrace.html.haml" do
   describe 'missing file in backtrace' do
-    it "should replace nil file with [unknown source]" do
+    before do
       notice = Factory(:notice, :backtrace => [{
         'number'  => rand(999),
         'file'    => nil,
         'method'  => ActiveSupport.methods.shuffle.first
       }])
       assign :app, notice.err.app
+    end
+
+    it "should replace nil file with [unknown source]" do
       render :partial => "notices/backtrace", :locals => {:lines => notice.backtrace}
       rendered.should match(/\[unknown source\]/)
     end
