98a340849c198fad49fedb965812f139494676d3
Nathan Broadbent <nathan.f77@gmail.com>
2011-09-11 00:50:35 +0800

    Reorganized Actionmailer inline CSS code. Use sensible default path for mailer stylesheets. Module is small enough to live in initializer.

--- spec/mailers/mailer_spec.rb
+++ spec/mailers/mailer_spec.rb
@@ -5,10 +5,18 @@ describe Mailer do
     include EmailSpec::Helpers
     include EmailSpec::Matchers
 
-    it "should html-escape the notice's message for the html part" do
+    before do
       @notice = Factory(:notice, :message => "class < ActionController::Base")
       @email = Mailer.err_notification(@notice)
+    end
+
+    it "should html-escape the notice's message for the html part" do
       @email.should have_body_text("class &lt; ActionController::Base")
+    end
+
+    it "should inline css" do
+      @email.should have_body_text '<td class="header" style="'
+      # ('style' attribute is not present unless premailer works.)
     end
   end
 end
