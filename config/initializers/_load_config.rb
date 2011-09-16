8ad76e4370554b8479a79d2cf4b4f0b486c67b8f
Nathan Broadbent <nathan.f77@gmail.com>
2011-09-16 19:24:35 +0800

    Replace Errbit::Application.config.action_mailer with ActionMailer::Base. This fixes misc. problems for both rake tasks and production servers. (smtp setttings were previously not being passed through in Rake tasks)

--- config/initializers/_load_config.rb
+++ config/initializers/_load_config.rb
@@ -43,13 +43,12 @@ end
 
 # Set SMTP settings if given.
 if smtp = Errbit::Config.smtp_settings
-  Errbit::Application.config.action_mailer.smtp_settings = smtp
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = smtp
 end
 
 # Set config specific values
-(Errbit::Application.config.action_mailer.default_url_options ||= {}).tap do |default|
+(ActionMailer::Base.default_url_options ||= {}).tap do |default|
   default.merge! :host => Errbit::Config.host if default[:host].blank?
 end
 
