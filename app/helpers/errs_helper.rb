5866b17204bc07addf369d038bf04f345fa4891a
Nathan Broadbent <nathan.f77@gmail.com>
2011-09-26 20:58:21 +0800

    Emulate support for 'word-wrap: break-word' in firefox

--- app/helpers/errs_helper.rb
+++ app/helpers/errs_helper.rb
@@ -6,5 +6,11 @@ module ErrsHelper
   def err_confirm
     Errbit::Config.confirm_resolve_err === false ? nil : 'Seriously?'
   end
+
+  def trucated_err_message(problem)
+    msg = truncate(problem.message, :length => 300)
+    # Insert invisible unicode characters so that firefox can emulate 'word-wrap: break-word' CSS
+    msg.scan(/.{5}/).join("&#8203;").html_safe
+  end
 end
 
