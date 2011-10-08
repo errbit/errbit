48737a27a7ac8b2080238c2450a48c3a497d5157
Daniel Cohen <daniel@codeinvain.com>
2011-10-08 14:37:02 +0200

    [FIX] validate_db_name error

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-
+require 'yaml' 
+YAML::ENGINE.yamler= 'syck'
 # Initialize the rails application
 Errbit::Application.initialize!
 
