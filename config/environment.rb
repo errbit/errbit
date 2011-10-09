389cba436c416605bd2833a260c89cc80a7502ef
Daniel Cohen <daniel@codeinvain.com>
2011-10-09 08:32:23 +0200

    added conditional require based on ruby version

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,9 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-require 'yaml' 
-YAML::ENGINE.yamler= 'syck'
+if RUBY_VERSION >'1.9'
+  require 'yaml' 
+  YAML::ENGINE.yamler= 'syck'
+end
 # Initialize the rails application
 Errbit::Application.initialize!
 
