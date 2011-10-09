a6860208b98f68f119bf611a156c05f31c2447cf
Nathan Broadbent <nathan.f77@gmail.com>
2011-10-09 21:38:32 +0800

    Fixed spec for errs/show

--- spec/views/errs/show.html.haml_spec.rb
+++ spec/views/errs/show.html.haml_spec.rb
@@ -10,6 +10,7 @@ describe "errs/show.html.haml" do
     assign :app, problem.app
     assign :notices, err.notices.paginate(:page => 1, :per_page => 1)
     assign :notice, err.notices.first
+    controller.stub(:current_user) { Factory(:user) }
   end
 
   describe "content_for :action_bar" do
