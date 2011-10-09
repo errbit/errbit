d629915c6584a02602e3a1dc3a82c183660c255a
Nathan Broadbent <nathan.f77@gmail.com>
2011-10-09 21:27:08 +0800

    slight tidy

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-if RUBY_VERSION >'1.9'
-  require 'yaml' 
-  YAML::ENGINE.yamler= 'syck'
+if RUBY_VERSION.to_f >= 1.9
+  require 'yaml'
+  YAML::ENGINE.yamler = 'syck'
 end
 # Initialize the rails application
 Errbit::Application.initialize!
