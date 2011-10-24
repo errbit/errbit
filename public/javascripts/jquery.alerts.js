4f0eace4d89566ebcbd13c40cdd5321abad08aa5
Akzhan Abdulin <akzhan.abdulin@gmail.com>
2011-10-24 17:28:42 +0400

    Test for draggable plugin instead of try..catch

--- public/javascripts/jquery.alerts.js
+++ public/javascripts/jquery.alerts.js
@@ -111,60 +111,58 @@
 			$.alerts._maintainPosition(true);
 			
 			switch( type ) {
-				case 'alert':
-					$("#popup_message").after('<div id="popup_panel"><input type="button" value="' + $.alerts.okButton + '" id="popup_ok" /></div>');
-					$("#popup_ok").click( function() {
-						$.alerts._hide();
-						callback(true);
-					});
-					$("#popup_ok").focus().keypress( function(e) {
-						if( e.keyCode == 13 || e.keyCode == 27 ) $("#popup_ok").trigger('click');
-					});
+			case 'alert':
+				$("#popup_message").after('<div id="popup_panel"><input type="button" value="' + $.alerts.okButton + '" id="popup_ok" /></div>');
+				$("#popup_ok").click( function() {
+					$.alerts._hide();
+					callback(true);
+				});
+				$("#popup_ok").focus().keypress( function(e) {
+					if( e.keyCode == 13 || e.keyCode == 27 ) $("#popup_ok").trigger('click');
+				});
 				break;
-				case 'confirm':
-					$("#popup_message").after('<div id="popup_panel"><input type="button" value="' + $.alerts.okButton + '" id="popup_ok" /> <input type="button" value="' + $.alerts.cancelButton + '" id="popup_cancel" /></div>');
-					$("#popup_ok").click( function() {
-						$.alerts._hide();
-						if( callback ) callback(true);
-					});
-					$("#popup_cancel").click( function() {
-						$.alerts._hide();
-						if( callback ) callback(false);
-					});
-					$("#popup_ok").focus();
-					$("#popup_ok, #popup_cancel").keypress( function(e) {
-						if( e.keyCode == 13 ) $("#popup_ok").trigger('click');
-						if( e.keyCode == 27 ) $("#popup_cancel").trigger('click');
-					});
+			case 'confirm':
+				$("#popup_message").after('<div id="popup_panel"><input type="button" value="' + $.alerts.okButton + '" id="popup_ok" /> <input type="button" value="' + $.alerts.cancelButton + '" id="popup_cancel" /></div>');
+				$("#popup_ok").click( function() {
+					$.alerts._hide();
+					if( callback ) callback(true);
+				});
+				$("#popup_cancel").click( function() {
+					$.alerts._hide();
+					if( callback ) callback(false);
+				});
+				$("#popup_ok").focus();
+				$("#popup_ok, #popup_cancel").keypress( function(e) {
+					if( e.keyCode == 13 ) $("#popup_ok").trigger('click');
+					if( e.keyCode == 27 ) $("#popup_cancel").trigger('click');
+				});
 				break;
-				case 'prompt':
-					$("#popup_message").append('<br /><input type="text" size="30" id="popup_prompt" />').after('<div id="popup_panel"><input type="button" value="' + $.alerts.okButton + '" id="popup_ok" /> <input type="button" value="' + $.alerts.cancelButton + '" id="popup_cancel" /></div>');
-					$("#popup_prompt").width( $("#popup_message").width() );
-					$("#popup_ok").click( function() {
-						var val = $("#popup_prompt").val();
-						$.alerts._hide();
-						if( callback ) callback( val );
-					});
-					$("#popup_cancel").click( function() {
-						$.alerts._hide();
-						if( callback ) callback( null );
-					});
-					$("#popup_prompt, #popup_ok, #popup_cancel").keypress( function(e) {
-						if( e.keyCode == 13 ) $("#popup_ok").trigger('click');
-						if( e.keyCode == 27 ) $("#popup_cancel").trigger('click');
-					});
-					if( value ) $("#popup_prompt").val(value);
-					$("#popup_prompt").focus().select();
+			case 'prompt':
+				$("#popup_message").append('<br /><input type="text" size="30" id="popup_prompt" />').after('<div id="popup_panel"><input type="button" value="' + $.alerts.okButton + '" id="popup_ok" /> <input type="button" value="' + $.alerts.cancelButton + '" id="popup_cancel" /></div>');
+				$("#popup_prompt").width( $("#popup_message").width() );
+				$("#popup_ok").click( function() {
+					var val = $("#popup_prompt").val();
+					$.alerts._hide();
+					if( callback ) callback( val );
+				});
+				$("#popup_cancel").click( function() {
+					$.alerts._hide();
+					if( callback ) callback( null );
+				});
+				$("#popup_prompt, #popup_ok, #popup_cancel").keypress( function(e) {
+					if( e.keyCode == 13 ) $("#popup_ok").trigger('click');
+					if( e.keyCode == 27 ) $("#popup_cancel").trigger('click');
+				});
+				if( value ) $("#popup_prompt").val(value);
+				$("#popup_prompt").focus().select();
 				break;
-        default: break;
+			default: break;
 			}
 			
 			// Make draggable
-			if( $.alerts.draggable ) {
-				try {
-					$("#popup_container").draggable({ handle: $("#popup_title") });
-					$("#popup_title").css({ cursor: 'move' });
-				} catch(e) { /* requires jQuery UI draggables */ }
+			if ($.alerts.draggable && $.fn.draggable) {
+				$("#popup_container").draggable({ handle: $("#popup_title") });
+				$("#popup_title").css({ cursor: 'move' });
 			}
 		},
 		
