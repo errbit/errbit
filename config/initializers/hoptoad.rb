d2acd997be88ff43e8e33e29e2fae9f949c1c0f2
lest <just.lest@gmail.com>
2011-11-16 19:23:53 +0200

    don't log DocumentNotFound exception

--- config/initializers/hoptoad.rb
+++ config/initializers/hoptoad.rb
@@ -2,5 +2,8 @@ HoptoadNotifier.configure do |config|
   # Internal Errbit errors are stored locally, but we need
   # to set a dummy API key so that HoptoadNotifier doesn't complain.
   config.api_key = "---------"
+
+  # Don't log error that causes 404 page
+  config.ignore << "Mongoid::Errors::DocumentNotFound"
 end
 
