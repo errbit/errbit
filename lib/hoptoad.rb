3a04d97a882cdd67f85bbbeedbb10033fcb2eea4
Robert Lail <robert.lail@cph.org>
2011-11-18 10:29:05 -0600

    hoptoad-ios reports its API version as 2.1. Allow that. Also improve the message for ApiVersionError.

--- lib/hoptoad.rb
+++ lib/hoptoad.rb
@@ -3,7 +3,7 @@ require 'hoptoad/v2'
 module Hoptoad
   class ApiVersionError < StandardError
     def initialize
-      super "Wrong API Version: Expecting v2.0"
+      super "Wrong API Version: Expecting 2.0, 2.1, or 2.2"
     end
   end
 
@@ -17,9 +17,8 @@ module Hoptoad
   private
     def self.get_version_processor(version)
       case version
-      when '2.0'; Hoptoad::V2
-      when '2.2'; Hoptoad::V2
-      else;       raise ApiVersionError
+      when /2\.[012]/; Hoptoad::V2
+      else;            raise ApiVersionError
       end
     end
 end
