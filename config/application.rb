f24e5a86c51dda58fea14419f02a96a78bc58e9a
Dean Perry <deanpcmad@gmail.com>
2011-11-24 23:39:07 +0000

    added rack-ssl-enforcer

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,9 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module Errbit
   class Application < Rails::Application
+    
+    config.middleware.use Rack::SslEnforcer, :except => /^\/deploys/
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
