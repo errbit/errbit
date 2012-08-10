a6a2573d0565b8bdebe5ed796a9e4e1a8940a8f8
Ryan Jones <ryan.michael.jones@gmail.com>
2012-08-10 10:44:06 -0600

    hide show notification form

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -8,6 +8,9 @@ $(function(){
   if($('div.issue_tracker.nested').length)
     activateTypeSelector('issue_tracker', 'tracker_params');
 
+  if($('div.notification_service.nested').length)
+    activateTypeSelector('notification_service', 'notification_params');
+
   $('body').addClass('has-js');
   $('.label_radio').click(function(){
     activateLabelIcons();
