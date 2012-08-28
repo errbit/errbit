02130572aa4dfa420d259b8a15cd26c88b538c45
Nathan Broadbent <nathan.f77@gmail.com>
2012-08-29 11:53:09 +1200

    Show gravatar on users index table, and in heading bar of users show page

--- app/helpers/errs_helper.rb
+++ app/helpers/errs_helper.rb
@@ -15,15 +15,17 @@ module ErrsHelper
   end
 
   def gravatar_tag(email, options = {})
+    image_tag gravatar_url(email, options), :alt => email, :class => 'gravatar'
+  end
+
+  def gravatar_url(email, options = {})
     default_options = {
       :d => Errbit::Config.gravatar_default,
-      :alt => email,
-      :class => 'gravatar'
     }
     options.reverse_merge! default_options
     params = options.extract!(:s, :d).delete_if { |k, v| v.blank? }
     email_hash = Digest::MD5.hexdigest(email)
-    image_tag "http://www.gravatar.com/avatar/#{email_hash}?#{params.to_query}", options
+    "http://www.gravatar.com/avatar/#{email_hash}?#{params.to_query}"
   end
 end
 
