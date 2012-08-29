2da2b47168599de7919158df3e60591dafd269ca
Robert Lail <robert.lail@cph.org>
2012-08-28 23:18:07 -0500

    validate Problem#last_notice_at; ensure that it isn't nil
    resolves the apparent sorting fail on problems by last notice timestamp

--- app/helpers/errs_helper.rb
+++ app/helpers/errs_helper.rb
@@ -1,8 +1,4 @@
 module ErrsHelper
-  def last_notice_at(problem)
-    problem.last_notice_at || problem.created_at
-  end
-
   def err_confirm
     Errbit::Config.confirm_resolve_err === false ? nil : 'Seriously?'
   end
