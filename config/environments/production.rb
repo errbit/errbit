d5e33cb12595fc67afba3ca7f2e00848aaa66902
Nathan Broadbent <nathan.f77@gmail.com>
2012-09-19 13:03:55 +1200

    Removed SSL code in config/environments/production.rb in favor of enforce_ssl option in config.yml

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,8 +59,5 @@ Errbit::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-
-  # enable HTTPS
-  config.middleware.insert_before Rack::Lock, "Rack::SSL"
 end
 
