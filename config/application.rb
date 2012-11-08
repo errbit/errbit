f7f6e27713b86b07f3bdc5313b03414e372026cb
Nikita Fedyashev <nfedyashev@gmail.com>
2012-11-08 21:16:43 +0100

    Use Rails 3.2 default convention for requiring gem groups

--- config/application.rb
+++ config/application.rb
@@ -9,9 +9,12 @@ require "action_mailer/railtie"
 require 'mongoid/railtie'
 require "sprockets/railtie"
 
-# If you have a Gemfile, require the gems listed there, including any gems
-# you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env) if defined?(Bundler)
+if defined?(Bundler)
+  # If you precompile assets before deploying to production, use this line
+  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  # If you want your assets lazily compiled in production, use this line
+  # Bundler.require(:default, :assets, Rails.env)
+end
 
 module Errbit
   class Application < Rails::Application
