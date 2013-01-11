ce2443a137d7017046da942c76fbbd65a9c2bff7
Benoit Larroque <benoit.larroque@eyeka.net>
2013-01-11 15:19:03 +0100

    Move destroy app link from show page to edit page
    It was very easy to destroy the application instead of deleting errors
    
    Also add a related test on the confirmation

---
+++ spec/views/apps/edit.html.haml_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe "apps/edit.html.haml" do
+  before do
+    app = stub_model(App)
+    assign :app, app
+    controller.stub(:current_user) { stub_model(User) }
+  end
+
+  describe "content_for :action_bar" do
+    def action_bar
+      view.content_for(:action_bar)
+    end
+
+    it "should confirm the 'destroy' link" do
+      render
+
+      action_bar.should have_selector('a.button[data-confirm="Seriously?"]')
+    end
+
+  end
+end
+
