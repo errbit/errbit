530187bf1e7c010910376d428c7f87bfdaba074e
Nathan Broadbent <nathan.f77@gmail.com>
2013-01-25 01:08:21 -0800

    Let's use .present?

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -10,7 +10,7 @@ if %w(development test).include? Rails.env
   Errbit::Application.config.secret_token = 'f258ed69266dc8ad0ca79363c3d2f945c388a9c5920fc9a1ae99a98fbb619f135001c6434849b625884a9405a60cd3d50fc3e3b07ecd38cbed7406a4fccdb59c'
 else
 
-  if ![nil, ''].include?(ENV['SECRET_TOKEN'])
+  if ENV['SECRET_TOKEN'].present?
     Errbit::Application.config.secret_token = ENV['SECRET_TOKEN']
 
   else
