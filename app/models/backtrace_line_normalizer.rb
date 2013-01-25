9e06ff0fd83f787b8c9378a260a69993b67d0505
Nathan Broadbent <nathan.f77@gmail.com>
2013-01-24 20:30:17 -0800

    Fixed migrations

--- app/models/backtrace_line_normalizer.rb
+++ app/models/backtrace_line_normalizer.rb
@@ -9,11 +9,19 @@ class BacktraceLineNormalizer
 
   private
   def normalized_file
-    @raw_line['file'].blank? ? "[unknown source]" :  @raw_line['file'].to_s.gsub(/\[PROJECT_ROOT\]\/.*\/ruby\/[0-9.]+\/gems/, '[GEM_ROOT]/gems')
+    if @raw_line['file'].blank?
+      "[unknown source]"
+    else
+      @raw_line['file'].to_s.gsub(/\[PROJECT_ROOT\]\/.*\/ruby\/[0-9.]+\/gems/, '[GEM_ROOT]/gems')
+    end
   end
 
   def normalized_method
-    @raw_line['method'].blank? ? "[unknown method]" : @raw_line['method'].gsub(/[0-9_]{10,}+/, "__FRAGMENT__")
+    if @raw_line['method'].blank?
+      "[unknown method]"
+    else
+      @raw_line['method'].to_s.gsub(/[0-9_]{10,}+/, "__FRAGMENT__")
+    end
   end
 
 end
--- db/migrate/20121005142110_regenerate_err_fingerprints.rb
+++ db/migrate/20121005142110_regenerate_err_fingerprints.rb
@@ -1,16 +1,18 @@
 class RegenerateErrFingerprints < Mongoid::Migration
   def self.up
     Err.all.each do |err|
-      fingerprint_source = {
-        :backtrace   => err.notices.first.backtrace_id,
-        :error_class => err.error_class,
-        :component   => err.component,
-        :action      => err.action,
-        :environment => err.environment,
-        :api_key     => err.app.api_key
-      }
-      fingerprint = Digest::SHA1.hexdigest(fingerprint_source.to_s)
-      err.update_attribute(:fingerprint, fingerprint)
+      if err.notices.any?
+        fingerprint_source = {
+          :backtrace   => err.notices.first.backtrace_id,
+          :error_class => err.error_class,
+          :component   => err.component,
+          :action      => err.action,
+          :environment => err.environment,
+          :api_key     => err.app.api_key
+        }
+        fingerprint = Digest::SHA1.hexdigest(fingerprint_source.to_s)
+        err.update_attribute(:fingerprint, fingerprint)
+      end
     end
   end
 
