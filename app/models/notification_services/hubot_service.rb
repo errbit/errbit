84a293a2fbd64c7e251a80988a415b9490b3e4a4
Raffael Schmid <raffael@yux.ch>
2013-01-28 23:34:44 +0100

    add problem url into the chat message

--- app/models/notification_services/hubot_service.rb
+++ app/models/notification_services/hubot_service.rb
@@ -22,7 +22,7 @@ class NotificationServices::HubotService < NotificationService
   end
 
   def message_for_hubot(problem)
-    notification_description(problem)
+    "#{notification_description(problem)} #{problem_url(problem)}"
   end
 
   def create_notification(problem)
