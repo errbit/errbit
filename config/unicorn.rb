2f2f6606d3b1252ed2730cf86ae1daf5d227da00
Patrick Veverka <patrick.veverka@gmail.com>
2013-02-08 15:18:35 -0500

    unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,3 +3,23 @@
 worker_processes 3 # amount of unicorn workers to spin up
 timeout 30         # restarts workers that hang for 30 seconds
 preload_app true
+working_directory "/var/www/errbit/"
+
+# This is where we specify the socket.
+# We will point the upstream Nginx module to this socket later on
+listen "/var/www/errbit/tmp/sockets/unicorn.sock", :backlog => 64
+listen 3001, :tcp_nopush => true
+
+pid "/var/www/errbit/tmp/pids/unicorn.pid"
+# Set the path of the log files inside the log folder of the testapp
+stderr_path "/var/www/errbit/log/unicorn.stderr.log"
+stdout_path "/var/www/errbit/log/unicorn.stdout.log"
+before_fork do |server, worker|
+    # This option works in together with preload_app true setting
+    defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+    defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
+
