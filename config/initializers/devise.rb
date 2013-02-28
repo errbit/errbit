b432df84d4bae999234660182bb0cc66894a1fe5
Daniel Beardsley <daniel@ifixit.com>
2013-02-28 16:39:19 -0700

    Passwords: bump length to 1024
    Why was it at 20?
    Why even have a max length anyway?

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -69,7 +69,7 @@ Devise.setup do |config|
 
   # ==> Configuration for :validatable
   # Range for password length
-  config.password_length = 6..20
+  config.password_length = 6..1024
 
   # Regex to use to validate the email address
   config.email_regexp = /^([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})$/i
