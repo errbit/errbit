13fd21ed218ae039438e805d5b52f18b1935e920
Nathan Broadbent <nathan.f77@gmail.com>
2013-03-06 12:49:30 +1300

    Don't raise mail delivery errors in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Errbit::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   # Print deprecation notices to the Rails logger
