a90ecf79cd92dc48df7cdac2257782d18c87e751
Ivan Shumkov <ivan@shumkov.ru>
2013-04-17 14:13:30 +0400

    Add integration_id support to pivotal tracker integration

--- app/models/issue_trackers/pivotal_labs_tracker.rb
+++ app/models/issue_trackers/pivotal_labs_tracker.rb
@@ -5,11 +5,14 @@ if defined? PivotalTracker
       [:api_token, {
         :placeholder => "API Token for your account"
       }],
-      [:project_id, {}]
+      [:project_id, {}],
+      [:integration_id, {
+        :optional    => true,
+      }]
     ]
 
     def check_params
-      if Fields.detect {|f| self[f[0]].blank? }
+      if Fields.detect {|f| self[f[0]].blank? && !f[1][:optional]}
         errors.add :base, 'You must specify your Pivotal Tracker API token and Project ID'
       end
     end
@@ -17,10 +20,21 @@ if defined? PivotalTracker
     def create_issue(problem, reported_by = nil)
       PivotalTracker::Client.token = api_token
       PivotalTracker::Client.use_ssl = true
+
       project = PivotalTracker::Project.find project_id.to_i
-      story = project.stories.create :name => issue_title(problem),
-        :story_type => 'bug', :description => body_template.result(binding),
-        :requested_by => reported_by.name
+
+      params = { :name         => issue_title(problem),
+                 :story_type   => 'bug',
+                 :description  => body_template.result(binding),
+                 :requested_by => reported_by.name
+      }
+
+      if integration_id.present?
+        params[:integration_id] = integration_id.to_i
+        params[:other_id] = problem.id.to_s
+      end
+
+      story = project.stories.create params
 
       if story.errors.present?
         raise IssueTrackers::IssueTrackerError, story.errors.first
