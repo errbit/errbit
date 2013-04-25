f17c54954705cd70953ba307c42ed5141922fb7e
Sascha Depold <sascha@depold.com>
2013-04-25 10:08:44 +0300

    report the actual url as request_url
    Reporting the hash turns (for me) out to be error prone as I do not get the actual host.

--- public/javascripts/notifier.js
+++ public/javascripts/notifier.js
@@ -856,7 +856,7 @@ printStackTrace.implementation.prototype = {
                     error = Util.merge(this.options.errorDefaults, errorWithoutDefaults),
                     
                     component = error.component || '',
-                    request_url = (error.url || '' + location.hash),
+                    request_url = (error.url || '' + location.href),
                     
                     methods = ['cgi-data', 'params', 'session'],
                     _outputData = null;
