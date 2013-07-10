f021f13444f727888986ea79fad36b77ca0f6ae7
Arthur Neves <arthurnn@gmail.com>
2013-07-10 11:16:14 -0400

    fixing mongo.rb initializer to use mongoid 3

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -20,14 +20,22 @@ elsif ENV['HEROKU'] || ENV['USE_ENV']
   end
 
   Mongoid.configure do |config|
-    config.master = Mongo::Connection.new(
-      settings.host,
-      settings.port
-    ).db(database_name)
-    config.master.authenticate(settings.user, settings.password) if settings.user
+
+    hash = {
+      sessions: {
+        default: {
+          database: database_name,
+          hosts: [ "#{settings.host}:#{settings.port}" ]
+        }
+      },
+      options: { allow_dynamic_fields: false, use_activesupport_time_zone: true, identity_map_enabled: true }
+    }
+
+    if settings.user && settings.password
+      hash[:sessions][:default][:username] = settings.user
+      hash[:sessions][:default][:password] = settings.password
+    end
+
+    config.load_configuration(hash)
   end
 end
-
-Mongoid.allow_dynamic_fields = false
-Mongoid.use_activesupport_time_zone = true
-Mongoid.identity_map_enabled = true
