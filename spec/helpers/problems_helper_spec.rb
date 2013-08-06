6940384c862d5b096923bdc10aa2d6672a43a7ac
Jeroen Jacobs <git@jeroenj.be>
2013-08-06 09:27:18 +0200

    Makes avatar url tests in problem_helper_spec compatible with ruby 1.8

--- spec/helpers/problems_helper_spec.rb
+++ spec/helpers/problems_helper_spec.rb
@@ -62,7 +62,7 @@ describe ProblemsHelper do
       let(:email_hash) { Digest::MD5.hexdigest email }
 
       it "should return the http url" do
-        ActionController::TestRequest.any_instance.stub ssl?: true
+        ActionController::TestRequest.any_instance.stub :ssl? => true
         helper.gravatar_url(email).should eq("https://secure.gravatar.com/avatar/#{email_hash}?d=identicon")
       end
     end
