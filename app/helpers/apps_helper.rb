792102d0e6619f550c88ef0e98c25450e2520bff
Sven Pachnit <sven@bmonkeys.net>
2013-08-07 14:00:37 +0200

    Sort list of apps to copy settings from.

--- app/helpers/apps_helper.rb
+++ app/helpers/apps_helper.rb
@@ -4,7 +4,7 @@ module AppsHelper
       html =  link_to('copy settings from another app', '#',
                        :class => 'button copy_config')
       html << select("duplicate", "app",
-                     App.all.reject{|a| a == @app }.
+                     App.all.asc(:name).reject{|a| a == @app }.
                      collect{|p| [ p.name, p.id ] }, {:include_blank => "[choose app]"},
                      {:class => "choose_other_app", :style => "display: none;"})
       return html
