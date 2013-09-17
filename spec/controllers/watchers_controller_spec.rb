e17ea8256c7f1c049535a81f2b774fec5e1ce954
Cyril Mougel <cyril.mougel@gmail.com>
2013-09-17 22:40:14 +0200

    Fix the watcher destroy controller because not work before

--- spec/controllers/watchers_controller_spec.rb
+++ spec/controllers/watchers_controller_spec.rb
@@ -1,30 +1,35 @@
 require 'spec_helper'
 
 describe WatchersController do
-  let(:app) { Fabricate(:app_with_watcher) }
+  let(:app) do
+    a = Fabricate(:app)
+    Fabricate(:user_watcher, :app => a)
+    a
+  end
 
   describe "DELETE /apps/:app_id/watchers/:id/destroy" do
-    render_views
 
-    before(:each) do
-      sign_in Fabricate(:admin)
-    end
-
-    context "successful watcher deletion" do
-      let(:problem) { Fabricate(:problem_with_comments) }
-      let(:watcher) { app.watchers.first }
-
+    context "with admin user" do
       before(:each) do
-        delete :destroy, :app_id => app.id, :id => watcher.id.to_s
-        problem.reload
+        sign_in Fabricate(:admin)
       end
 
-      it "should delete the watcher" do
-        app.watchers.detect{|w| w.id.to_s == watcher.id }.should == nil
-      end
+      context "successful watcher deletion" do
+        let(:problem) { Fabricate(:problem_with_comments) }
+        let(:watcher) { app.watchers.first }
 
-      it "should redirect to index page" do
-        response.should redirect_to(root_path)
+        before(:each) do
+          delete :destroy, :app_id => app.id, :id => watcher.user.id.to_s
+          problem.reload
+        end
+
+        it "should delete the watcher" do
+          app.watchers.detect{|w| w.id.to_s == watcher.id }.should == nil
+        end
+
+        it "should redirect to index page" do
+          response.should redirect_to(root_path)
+        end
       end
     end
   end
