97b591536547ae8955ff9ed75db386b50f9163b7
Arthur Neves <arthurnn@gmail.com>
2013-10-19 13:24:58 -0400

    Moped::BSON -> BSON

--- app/models/app.rb
+++ app/models/app.rb
@@ -21,7 +21,7 @@ class App
   field :_id,
     type: String,
     pre_processed: true,
-    default: ->{ Moped::BSON::ObjectId.new.to_s }
+    default: ->{ BSON::ObjectId.new.to_s }
 
 
   embeds_many :watchers
