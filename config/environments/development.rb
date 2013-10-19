c77c98ceb16a64015aea7ca8847fe6074dd28bc0
Arthur Neves <arthurnn@gmail.com>
2013-10-19 13:24:28 -0400

    Changing some rails 4 config flags

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,8 +6,7 @@ Errbit::Application.configure do
   # since you don't have to restart the webserver when you make code changes.
   config.cache_classes = false
 
-  # Log error messages when you accidentally call methods on nil.
-  config.whiny_nils = true
+  config.eager_load = false
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -7,8 +7,7 @@ Errbit::Application.configure do
   # and recreated between test runs.  Don't rely on the data there!
   config.cache_classes = true
 
-  # Log error messages when you accidentally call methods on nil.
-  config.whiny_nils = true
+  config.eager_load = false
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
