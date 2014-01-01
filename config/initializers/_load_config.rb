b28437270a838d0f49edeb860ec674274b9e4e79
Maxim Pechnikov <parallel588@gmail.com>
2014-01-01 21:45:01 +0400

    fixed smtp settings for OpenShift

--- config/initializers/_load_config.rb
+++ config/initializers/_load_config.rb
@@ -26,14 +26,17 @@ unless defined?(Errbit::Config)
     Errbit::Config.github_secret = ENV['GITHUB_SECRET']
     Errbit::Config.github_access_scope = ENV['GITHUB_ACCESS_SCOPE'].split(',').map(&:strip) if ENV['GITHUB_ACCESS_SCOPE']
 
-    Errbit::Config.smtp_settings = {
+    smtp_settings = {
       :address        => ENV['SMTP_SERVER'] || 'smtp.sendgrid.net',
       :port           => ENV['SMTP_PORT']   || 25,
       :authentication => :plain,
       :user_name      => ENV['SMTP_USERNAME']   || ENV['SENDGRID_USERNAME'],
       :password       => ENV['SMTP_PASSWORD']   || ENV['SENDGRID_PASSWORD'],
-      :domain         => ENV['SMTP_DOMAIN'] || ENV['SENDGRID_DOMAIN'] || ENV['ERRBIT_EMAIL_FROM'].split('@').last
+      :domain         => ENV['SMTP_DOMAIN'] || ENV['SENDGRID_DOMAIN'] || ENV['ERRBIT_EMAIL_FROM'].split('@').last,
+      :openssl_verify_mode=>ENV["OPENSSL_VERIFY_MODE"]
     }
+
+    Errbit::Config.smtp_settings = smtp_settings.select{ |_, v| v.present?  }
   end
 
   # Use example config for test environment.
