b0fee8d639a0769289ce9ff0066cfe2ee9fabd16
deathowl <csergo.balint@ustream.tv>
2014-01-08 14:13:36 +0100

    outdated error clearer

---
+++ app/interactors/outdated_problem_clearer.rb
@@ -0,0 +1,31 @@
+require 'problem_destroy'
+
+class OutdatedProblemClearer
+  ##
+  # Clear all problem not present for more than one week.
+  #
+  def execute
+    nb_problem_outdated.tap do |nb|
+      if nb > 0
+        criteria.each do |problem|
+          ProblemDestroy.new(problem).execute
+        end
+        repair_database
+      end
+    end
+  end
+
+private
+
+  def nb_problem_outdated
+    @count ||= criteria.count
+  end
+
+  def criteria
+    @criteria ||= Problem.where(:last_notice_at.lt => Errbit::Config.notice_deprecation_days.to_f.days.ago)
+  end
+
+  def repair_database
+    Mongoid.default_client.command repairDatabase: 1
+  end
+end
