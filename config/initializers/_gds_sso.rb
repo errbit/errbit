22902a48479218d7fb7c910f6065be56ae8b9f79
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2014-01-24 11:20:16 +0000

    Add comment explaining initializer load order

--- config/initializers/_gds_sso.rb
+++ config/initializers/_gds_sso.rb
@@ -1,6 +1,8 @@
 # This file is overwritten on deploy
 
 # This file name has a leading _ to ensure it's loaded before devise_overrides.rb
+# Rails guarantees to load these files in alphabetical order. See
+# http://guides.rubyonrails.org/configuring.html#using-initializer-files
 
 require 'ostruct'
 
