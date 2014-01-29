643c37814064ed463f0e3f72f189df29aa589c98
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2014-01-29 14:47:42 +0000

    Fix actionmailer default hostname etc.
    The override setting were being loaded too late because ActionMailer is
    configured at the end of _load_config.rb, so move the loading of
    overrides into the main _load_config.rb

--- config/initializers/_load_config.rb
+++ config/initializers/_load_config.rb
@@ -55,6 +55,20 @@ unless defined?(Errbit::Config)
   Errbit::Config.devise_modules = [:database_authenticatable,
                                    :recoverable, :rememberable, :trackable,
                                    :validatable, :omniauthable]
+
+  # Set the hostname automatically
+  unless Rails.env.test?
+    errbit_uri = URI.parse(Plek.new.find('errbit'))
+    Errbit::Config.host = errbit_uri.host
+    Errbit::Config.protocol = errbit_uri.scheme
+  end
+
+  # Load any overrides set at deploy time.
+  overrides_config_file = Rails.root.join("config", "config_overrides.yml")
+  config = YAML.load_file(overrides_config_file)
+  config.each do |k,v|
+    Errbit::Config.send("#{k}=", v)
+  end
 end
 
 # Set default settings from config.example.yml if key is missing from config.yml
