e57e87c5228247e104b565592b19e7ab94914bc7
enric <enric@RS-Administrators-MacBook-Pro.local>
2014-02-24 17:59:25 +0100

    red_14_04_acu155062_redtoolify_errbit

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -248,4 +248,5 @@ Devise.setup do |config|
   # When using omniauth, Devise cannot automatically set Omniauth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = "/my_engine/users/auth"
+  config.secret_key = '@@ERRBIT_SECRET_TOKEN@@'
 end
