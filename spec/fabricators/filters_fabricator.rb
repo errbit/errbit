a3cb436b8463b9a61d373996c9123c867b4b953c
Brian van Burken <info@brianvanburken.nl>
2014-03-28 11:18:09 +0100

    Changed Fabricator from FilterCriteria to Filter.

--- spec/fabricators/filters_fabricator.rb
+++ spec/fabricators/filters_fabricator.rb
@@ -1,11 +1,11 @@
-Fabricator :filter_criteria do
+Fabricator :filter do
   message 'Foo'
   error_class 'FooBar'
   url 'example\.com'
   where 'test'
 end
 
-Fabricator :empty_filter, :from => :filter_criteria do
+Fabricator :empty_filter, :from => :filter do
   message 'some message'
   error_class ''
   url ''
