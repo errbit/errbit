728da5f06c44aa06592112fc800850c4ed544b5c
Arthur Neves <arthurnn@gmail.com>
2014-03-28 15:19:22 -0400

    Unsed backtrace array on migration, instead of nil it

--- db/migrate/20121003223358_extract_backtraces.rb
+++ db/migrate/20121003223358_extract_backtraces.rb
@@ -1,11 +1,11 @@
 class ExtractBacktraces < Mongoid::Migration
   def self.up
     say "It could take long time (hours if you have many Notices)"
-    Notice.unscoped.all.each do |notice|
+    Notice.unscoped.where(backtrace_id: nil).each do |notice|
       next if notice.backtrace.present? || notice['backtrace'].nil?
       backtrace = Backtrace.find_or_create(:raw => notice['backtrace'] || [])
+      notice.unset(:backtrace)
       notice.backtrace = backtrace
-      notice['backtrace'] = nil
       notice.save!
     end
     say "run `db.repairDatabase()` (in mongodb console) to recover deleted space"
