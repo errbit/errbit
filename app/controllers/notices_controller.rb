07b76009a9a687a22da0798b50241ce78fc87bf1
Brian van Burken <info@brianvanburken.nl>
2014-04-09 12:06:52 +0200

    Fixed typo.

--- app/controllers/notices_controller.rb
+++ app/controllers/notices_controller.rb
@@ -18,7 +18,7 @@ class NoticesController < ApplicationController
         end
         render :xml => api_xml
       else
-        render :text => "Notice for ignored. It's a old version or doesn't pass the filters."
+        render :text => "Notice ignored. It's a old version or doesn't pass the filters."
       end
     else
       render :text => "Your API key is unknown", :status => 422
