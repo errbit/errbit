f39fed7db9d0275da4c0889c32a23103e1f847fb
Callum Dryden <callum.dryden@rightscale.com>
2014-05-09 20:43:01 +0100

    Add show action to the api v1 for problems
    This allows a problem id to be passed to the problem api and only
    information for that problem will be returned.

--- app/controllers/api/v1/problems_controller.rb
+++ app/controllers/api/v1/problems_controller.rb
@@ -1,6 +1,17 @@
 class Api::V1::ProblemsController < ApplicationController
   respond_to :json, :xml
 
+  def show
+    result = benchmark("[api/v1/problems_controller/show] query time") do
+      Problem.find(params[:id])
+    end
+
+    respond_to do |format|
+      format.any(:html, :json) { render :json => Yajl.dump(result) } # render JSON if no extension specified on path
+      format.xml  { render :xml  => result }
+    end
+  end
+
   def index
     query = {}
     fields = %w{app_id app_name environment message where first_notice_at last_notice_at resolved resolved_at notices_count}
@@ -11,7 +22,7 @@ class Api::V1::ProblemsController < ApplicationController
       query = {:first_notice_at=>{"$lte"=>end_date}, "$or"=>[{:resolved_at=>nil}, {:resolved_at=>{"$gte"=>start_date}}]}
     end
 
-    results = benchmark("[api/v1/problems_controller] query time") do
+    results = benchmark("[api/v1/problems_controller/index] query time") do
       Problem.where(query).with(:consistency => :strong).only(fields).to_a
     end
 
