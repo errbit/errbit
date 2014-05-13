0f3c5a506a0b975074aed106b73cf5bd5ce6b462
Bob Lail <bob.lail@cph.org>
2014-05-13 13:39:59 -0500

    [refactor] Refactored the BacktraceLineNormalizer (5m)

--- app/models/backtrace_line_normalizer.rb
+++ app/models/backtrace_line_normalizer.rb
@@ -1,32 +1,28 @@
 class BacktraceLineNormalizer
+
   def initialize(raw_line)
     @raw_line = raw_line || {}
   end
 
   def call
-    @raw_line.merge 'file' => normalized_file, 'method' => normalized_method
+    @raw_line.merge "file" => normalized_file, "method" => normalized_method
   end
 
-  private
+private
+
   def normalized_file
-    if @raw_line['file'].blank?
-      "[unknown source]"
-    else
-      file = @raw_line['file'].to_s
-      # Detect lines from gem
-      file.gsub!(/\[PROJECT_ROOT\]\/.*\/ruby\/[0-9.]+\/gems/, '[GEM_ROOT]/gems')
-      # Strip any query strings
-      file.gsub!(/\?[^\?]*$/, '')
-      @raw_line['file'] = file
-    end
+    return "[unknown source]" if @raw_line["file"].blank?
+
+    @raw_line["file"].to_s
+      .gsub(/\[PROJECT_ROOT\]\/.*\/ruby\/[0-9.]+\/gems/, "[GEM_ROOT]/gems") # Detect lines from gem
+      .gsub(/\?[^\?]*$/, "") # Strip any query strings
   end
 
   def normalized_method
-    if @raw_line['method'].blank?
-      "[unknown method]"
-    else
-      @raw_line['method'].to_s.gsub(/[0-9_]{10,}+/, "__FRAGMENT__")
-    end
+    return "[unknown method]" if @raw_line["method"].blank?
+
+    @raw_line["method"].to_s
+      .gsub(/[0-9_]{10,}+/, "__FRAGMENT__")
   end
 
 end
