f5a4576776cb027709ba8faa7c0fcca110c5bc1f
Tsyren Ochirov <nsu1team@gmail.com>
2014-11-13 17:51:56 +0100

    Fixed layout navigation bar, fixed apps edit page.

--- app/helpers/apps_helper.rb
+++ app/helpers/apps_helper.rb
@@ -2,7 +2,7 @@ module AppsHelper
   def link_to_copy_attributes_from_other_app
     if App.count > 1
       html =  link_to('copy settings from another app', '#',
-                       :class => 'button copy_config')
+                       :class => 'btn btn-default copy_config')
       html << select("duplicate", "app",
                      App.all.asc(:name).reject{|a| a == @app }.
                      collect{|p| [ p.name, p.id ] }, {:include_blank => "[choose app]"},
