59ede7cdbe8756a2c72ab4096a6acddbc2cd8d73
Bob Lail <bob.lail@cph.org>
2014-11-22 19:17:34 -0600

    [refactor] Rewrote the ProblemMerge spec (5m)

--- spec/interactors/problem_merge_spec.rb
+++ spec/interactors/problem_merge_spec.rb
@@ -1,21 +1,21 @@
 require 'spec_helper'
 
 describe ProblemMerge do
-  let(:problem) { Fabricate(:problem_with_errs) }
-  let(:problem_1) { Fabricate(:problem_with_errs) }
+  let(:problem) { Fabricate(:problem_with_errs, opened_at: 4.weeks.ago) }
+  let(:problem_1) { Fabricate(:problem_with_errs, opened_at: 5.weeks.ago) }
 
   describe "#initialize" do
-    it 'failed if less than 2 uniq problem pass in args' do
+    it "requires at least 2 unique problems" do
       expect {
         ProblemMerge.new(problem)
       }.to raise_error(ArgumentError)
     end
 
-    it 'extract first problem like merged_problem' do
+    it "extracts the first problem as merged_problem" do
       problem_merge = ProblemMerge.new(problem, problem, problem_1)
       expect(problem_merge.merged_problem).to eql problem
     end
-    it 'extract other problem like child_problems' do
+    it "extracts the other problems as child_problems" do
       problem_merge = ProblemMerge.new(problem, problem, problem_1)
       expect(problem_merge.child_problems).to eql [problem_1]
     end
@@ -27,38 +27,35 @@ describe ProblemMerge do
     let(:merged_errs) { problem_1.errs }
     let!(:notice) { Fabricate(:notice, err: first_errs.first) }
     let!(:notice_1) { Fabricate(:notice, err: merged_errs.first) }
+    let!(:comment) { Fabricate(:comment, err: problem.errs.first ) }
+    let!(:comment_2) { Fabricate(:comment, err: problem_1.errs.first, user: comment.user ) }
 
-    it 'delete one of problem' do
+    it "deletes all but one of the problems" do
       expect {
         problem_merge.merge
       }.to change(Problem, :count).by(-1)
     end
 
-    it 'move all err in one problem' do
+    it "associates all errs with the remaining problem" do
       problem_merge.merge
       expect(problem.reload.errs.map(&:id).sort).to eq (first_errs | merged_errs).map(&:id).sort
     end
 
-    it 'update problem cache' do
-      expect(ProblemUpdaterCache).to receive(:new).with(problem).and_return(double(update: true))
-      problem_merge.merge
+    it "associates all comments with the remaining problem" do
+      problem.comments(true)
+      problem_1.comments(true)
+      
+      expect {
+        problem_merge.merge
+      }.to change {
+        problem.comments.count
+      }.from(1).to(2)
+      expect(comment_2.reload.problem).to eq problem
     end
 
-    context "with problem with comment" do
-      let!(:comment) { Fabricate(:comment, err: problem.errs.first ) }
-      let!(:comment_2) { Fabricate(:comment, err: problem_1.errs.first, user: comment.user ) }
-      
-      it 'merge comment' do
-        problem.comments(true)
-        problem_1.comments(true)
-        
-        expect {
-          problem_merge.merge
-        }.to change{
-          problem.comments.count
-        }.from(1).to(2)
-        expect(comment_2.reload.problem).to eq problem
-      end
+    it "updates the problem's cached attributes" do
+      expect(ProblemUpdaterCache).to receive(:new).with(problem).and_return(double(update: true))
+      problem_merge.merge
     end
   end
 end
