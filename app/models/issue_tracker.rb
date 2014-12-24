242fd923457bbb06d7b8fc3810b0868e69afe02f
Arthur Neves <arthurnn@gmail.com>
2014-12-23 19:23:59 -0500

    Issue tracker model should not have any view/render concerns

--- app/models/issue_tracker.rb
+++ app/models/issue_tracker.rb
@@ -2,11 +2,6 @@ class IssueTracker
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  include Rails.application.routes.url_helpers
-
-  default_url_options[:host] = ActionMailer::Base.default_url_options[:host]
-  default_url_options[:port] = ActionMailer::Base.default_url_options[:port]
-
   embedded_in :app, :inverse_of => :issue_tracker
 
   field :type_tracker, :type => String
@@ -14,22 +9,12 @@ class IssueTracker
 
   validate :validate_tracker
 
-  ##
-  # Update default_url_option with valid data from the request information
-  #
-  # @param [ Request ] a request with host, port and protocol
-  #
-  def self.update_url_options(request)
-    IssueTracker.default_url_options[:host] = request.host
-    IssueTracker.default_url_options[:port] = request.port
-    IssueTracker.default_url_options[:protocol] = request.scheme
-  end
-
   def tracker
-    klass = ErrbitPlugin::Registry.issue_trackers[self.type_tracker]
-    klass = ErrbitPlugin::NoneIssueTracker unless klass
-
-    @tracker = klass.new(app, self.options)
+    @tracker ||=
+      begin
+        klass = ErrbitPlugin::Registry.issue_trackers[self.type_tracker] || ErrbitPlugin::NoneIssueTracker
+        klass.new(self.options)
+      end
   end
 
   # Allow the tracker to validate its own params
