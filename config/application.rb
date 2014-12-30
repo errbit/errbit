e38850e7f38d9c5c678924a24c76e355a010d2af
Arthur Neves <arthurnn@gmail.com>
2014-12-29 19:15:58 -0500

    small cleanup on application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,22 +1,13 @@
 require File.expand_path('../boot', __FILE__)
 
-# Pick the frameworks you want:
-# require 'rails/all'
-# require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
-# require "active_resource/railtie"
 require 'mongoid/railtie'
 require "sprockets/railtie"
 
 require 'draper'
 
-if defined?(Bundler)
-  # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
-  # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
-end
+Bundler.require(*Rails.groups)
 
 module Errbit
   class Application < Rails::Application
