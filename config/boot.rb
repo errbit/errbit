5ea0e27655be4d791718621edc11b575dd8d2a86
Stephen Crosby <stevecrozz@gmail.com>
2015-01-26 13:02:29 -0800

    add experimental support for jruby-9000-dev

--- config/boot.rb
+++ config/boot.rb
@@ -2,3 +2,11 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
+require 'json'
+
+begin
+  # try to use Yajl, the json_gem compatibility layer must be loaded after json
+  require 'yajl/json_gem'
+rescue LoadError
+  # fail silently because json gem is fine
+end
