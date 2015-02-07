27c84c43677c69af5bf32651e26a18ec6bbeb19d
toshi <me@toshimaru.net>
2015-02-08 00:03:52 +0900

    New app form

--- app/helpers/apps_helper.rb
+++ app/helpers/apps_helper.rb
@@ -1,8 +1,7 @@
 module AppsHelper
   def link_to_copy_attributes_from_other_app
     if App.count > 1
-      html =  link_to('copy settings from another app', '#',
-                       :class => 'button copy_config')
+      html =  link_to('copy settings from another app', '#', :class => 'btn btn-default')
       html << select("duplicate", "app",
                      App.all.asc(:name).reject{|a| a == @app }.
                      collect{|p| [ p.name, p.id ] }, {:include_blank => "[choose app]"},
