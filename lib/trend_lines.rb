37f5090b99ccaef346e772779f586d8b3ddd5b8a
Nick Evans <me@nicolasevans.org>
2015-02-27 15:10:52 -0500

    ensure NO_OF_DAYS_TO_CHART isn't set to 0 when the env var is nil

--- lib/trend_lines.rb
+++ lib/trend_lines.rb
@@ -1,6 +1,6 @@
 module TrendLines
   MINIMUM_BAR_CHART_SCALE_STEPS = 10
-  NO_OF_DAYS_TO_CHART           = ENV["ERROR_TREND_CHART_DAYS"].to_i || 14
+  NO_OF_DAYS_TO_CHART           = ENV["ERROR_TREND_CHART_DAYS"].present? ? ENV["ERROR_TREND_CHART_DAYS"].to_i : 14
 
   def self.enabled?
     ENV["ERROR_TREND_CHART_ENABLE"] == "true"
