177aa62ebc0ceea3b8594d69f876805d168a76f8
Nick Evans <me@nicolasevans.org>
2015-02-27 14:07:17 -0500

    allow disabling of trendlines through env variable

--- lib/trend_lines.rb
+++ lib/trend_lines.rb
@@ -2,6 +2,10 @@ module TrendLines
   MINIMUM_BAR_CHART_SCALE_STEPS = 10
   NO_OF_DAYS_TO_CHART           = 14
 
+  def self.enabled?
+    ENV["ERROR_TREND_CHART_ENABLE"] == "true"
+  end
+
   def self.beginning_of_range
     NO_OF_DAYS_TO_CHART.days.ago
   end
