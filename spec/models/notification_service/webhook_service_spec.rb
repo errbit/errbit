6574b745c311c663cdf860e36539eec7250434c5
mallowlabs <mallowlabs@gmail.com>
2015-03-21 00:49:46 +0900

    add url to webhook service

--- spec/models/notification_service/webhook_service_spec.rb
+++ spec/models/notification_service/webhook_service_spec.rb
@@ -4,7 +4,8 @@ describe NotificationService::WebhookService, type: 'model' do
     notification_service = Fabricate :webhook_notification_service, :app => notice.app
     problem = notice.problem
 
-    expect(HTTParty).to receive(:post).with(notification_service.api_token, :body => {:problem => problem.to_json}).and_return(true)
+    payload = notification_service.message_for_webhook(problem)
+    expect(HTTParty).to receive(:post).with(notification_service.api_token, :body => payload).and_return(true)
 
     notification_service.create_notification(problem)
   end
