9c45124156adff715a28feb2f2dffb1eef01b0c7
Stephen Crosby <stevecrozz@gmail.com>
2015-09-08 11:01:12 -0700

    Refs #933 remove remnants of BacktraceLineHelper

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -4,7 +4,6 @@ require Rails.root.join('config/routes.rb')
 
 class Mailer < ActionMailer::Base
   helper ApplicationHelper
-  helper BacktraceLineHelper
 
   default :from => Errbit::Config.email_from,
           'X-Errbit-Host' => Errbit::Config.host,
