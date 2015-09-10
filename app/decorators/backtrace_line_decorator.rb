437e9ef9d2059cb936d000af95f3578450b602c4
Russell Osborne <russell@burningpony.com>
2015-09-10 09:47:34 -0400

    Fix bitbucket link to bitbucket source.

--- app/decorators/backtrace_line_decorator.rb
+++ app/decorators/backtrace_line_decorator.rb
@@ -78,7 +78,7 @@ class BacktraceLineDecorator < Draper::Decorator
 
   def link_to_bitbucket(app, text = nil)
     return unless app.bitbucket_repo?
-    href = "%s#cl-%s" % [app.bitbucket_url_to_file(decorated_path + file_name), number]
+    href = "%s#%s-%s" % [app.bitbucket_url_to_file(decorated_path + file_name), file_name , number]
     h.link_to(text || file_name, href, :target => '_blank')
   end
 
