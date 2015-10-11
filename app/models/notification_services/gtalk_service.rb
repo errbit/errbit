a7ceeac84abbb2665ad98f1efd7c650ea61b9199
Laust Rud Jacobsen <laust@object.io>
2015-10-11 14:27:58 +0200

    Rubocop: perfer .tr over .gsub when just replacing chars

--- app/models/notification_services/gtalk_service.rb
+++ app/models/notification_services/gtalk_service.rb
@@ -60,7 +60,7 @@ class NotificationServices::GtalkService < NotificationService
   private
  
   def send_to_users client, message
-    user_id.gsub(/ /i, ",").gsub(/;/i, ",").split(",").map(&:strip).reject(&:empty?).each do |user|
+    user_id.tr(' ', ",").tr(';', ",").split(",").map(&:strip).reject(&:empty?).each do |user|
       client.send(Jabber::Message.new(user, message))
     end
   end
