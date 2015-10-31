a5cb09a3d9f5ea76c5708eec7e9d8cd7a2f8c4fe
Laust Rud Jacobsen <laust@object.io>
2015-10-31 10:37:37 +0100

    Rubocop: multiple lines of arguments only indented one level

--- app/models/notification_services/gtalk_service.rb
+++ app/models/notification_services/gtalk_service.rb
@@ -30,8 +30,8 @@ class NotificationServices::GtalkService < NotificationService
   def check_params
     if Fields.detect { |f| self[f[0]].blank? && self[f[2]].blank? } 
       errors.add :base, 
-      """You must specify your Username, Password, service, service_url 
-         and either rooms or users to send to or both"""
+        """You must specify your Username, Password, service, service_url 
+           and either rooms or users to send to or both"""
     end
   end
 
