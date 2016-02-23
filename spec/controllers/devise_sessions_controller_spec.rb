79fb96ed1919d5c8c226b24fed44ec259fe53a85
Marvin Frederickson <marvin.frederickson@gmail.com>
2016-02-22 16:37:05 -0900

    revert

--- spec/controllers/devise_sessions_controller_spec.rb
+++ spec/controllers/devise_sessions_controller_spec.rb
@@ -10,24 +10,13 @@ describe Devise::SessionsController, type: 'controller' do
     let(:user) { Fabricate(:user) }
 
     it 'redirects to app index page if there are no apps for the user' do
-      details = { 'email' => user.email, 'password' => user.password }
-      if Errbit::Config.user_has_username
-        details['username'] = user.username 
-      end
-
-      post :create, user: details
+      post :create, user: { 'email' => user.email, 'password' => user.password }
       expect(response).to redirect_to(root_path)
     end
 
     it 'displays a friendly error when credentials are invalid' do
-      details = { 'email' => 'whatever', 'password' => 'somethinginvalid' }
-      msg = I18n.t('devise.failure.user.email_invalid')
-      if Errbit::Config.user_has_username
-        details['username'] = 'somethinginvalid'
-        msg = I18n.t('devise.failure.user.username_invalid')
-      end
-      post :create, user: details
-      expect(request.flash["alert"]).to eq(msg)
+      post :create, user: { 'email' => 'whatever', 'password' => 'somethinginvalid' }
+      expect(request.flash["alert"]).to eq(I18n.t 'devise.failure.user.email_invalid')
     end
   end
 end
--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -1,9 +1,6 @@
 Fabricator :user do
   name 'Clyde Frog'
   email { sequence(:user_email) { |n| "user.#{n}@example.com" } }
-  if Errbit::Config.user_has_username
-    username { sequence(:username) { |n| "User Name #{n}" } }
-  end
   password 'password'
   password_confirmation 'password'
 end
