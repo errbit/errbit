00a6c7fe92430a685e734ebe958864ab3601abc2
Stephen Crosby <stevecrozz@gmail.com>
2016-07-15 15:52:17 -0700

    add rouge css

---
+++ _plugins/rouge_css.rb
@@ -0,0 +1,16 @@
+class RougeCss < Liquid::Tag
+  def initialize(tag_name, args, tokens)
+    super
+    args_array = args.strip.split(' ')
+    @theme_name = args_array.shift
+    @theme_mode = args_array.shift
+  end
+
+  def render(context)
+    theme = Rouge::Theme.registry[@theme_name]
+    theme.mode(@theme_mode) if @theme_mode
+    "<style>#{theme.render(scope: '.highlight')}</style>"
+  end
+end
+
+Liquid::Template.register_tag('rouge_css', RougeCss)
