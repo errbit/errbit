be2708559770e71c62cb78c9a8c1fe15a24462ef
Patrik Wehrli <weh@semabit.ch>
2016-10-06 19:23:42 +0200

    prevent NoMethodError for optional environment field. (#1123)
    closes errbit/errbit#1110

--- spec/lib/airbrake_api/v3/notice_parser_spec.rb
+++ spec/lib/airbrake_api/v3/notice_parser_spec.rb
@@ -18,6 +18,12 @@ describe AirbrakeApi::V3::NoticeParser do
     end.to raise_error(AirbrakeApi::ParamsError)
   end
 
+  it 'does not raise an error for the optional environment field' do
+    expect do
+      described_class.new('errors' => ['MyError']).report
+    end.not_to raise_error
+  end
+
   it 'parses JSON payload and returns ErrorReport' do
     params = build_params_for('api_v3_request.json', key: app.api_key)
 
