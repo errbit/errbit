4fef6087ecb28ad8b085531f42045a8af9c32482
Gabriel Gizotti <gizotti@users.noreply.github.com>
2017-01-05 03:08:32 +1000

    add channel-specific slack notifications (#1145)
    * add hint to notification fields label on App form
    * add the ability to select what channel slack notifications will be send to
    * enhance slack_service_spec.rb to test slack notifications with and without channel
    * add validation specs to NotificationServices::SlackService
    * fabricate the correct room_id for :slack_notification_service fabricator
    * only display em tag for service_notification_fields if field[:info] is present

--- app/models/notification_services/slack_service.rb
+++ app/models/notification_services/slack_service.rb
@@ -1,15 +1,27 @@
 class NotificationServices::SlackService < NotificationService
+  CHANNEL_NAME_REGEXP = /^#[a-z\d_-]+$/
   LABEL = "slack"
   FIELDS += [
     [:service_url, {
       placeholder: 'Slack Hook URL (https://hooks.slack.com/services/XXXXXXXXX/XXXXXXXXX/XXXXXXXXX)',
       label:       'Hook URL'
+    }],
+    [:room_id, {
+      placeholder: '#general',
+      label:       'Notification channel',
+      hint:        'If empty Errbit will use the default channel for the webook'
     }]
   ]
 
+  # Make room_id optional in case users want to use the default channel
+  # setup on Slack when creating the webhook
   def check_params
-    if FIELDS.detect { |f| self[f[0]].blank? }
-      errors.add :base, "You must specify your Slack Hook url."
+    if service_url.blank?
+      errors.add :service_url, "You must specify your Slack Hook url"
+    end
+
+    if room_id.present? && !CHANNEL_NAME_REGEXP.match(room_id)
+      errors.add :room_id, "Slack channel name must be lowercase, with no space, special character, or periods."
     end
   end
 
@@ -21,6 +33,7 @@ class NotificationServices::SlackService < NotificationService
     {
       username:    "Errbit",
       icon_url:    "https://raw.githubusercontent.com/errbit/errbit/master/docs/notifications/slack/errbit.png",
+      channel:     room_id,
       attachments: [
         {
           fallback:   message_for_slack(problem),
@@ -52,7 +65,7 @@ class NotificationServices::SlackService < NotificationService
           ]
         }
       ]
-    }.to_json
+    }.compact.to_json # compact to remove empty channel in case it wasn't selected by user
   end
 
   def create_notification(problem)
--- spec/models/notification_service/slack_service_spec.rb
+++ spec/models/notification_service/slack_service_spec.rb
@@ -1,26 +1,24 @@
 describe NotificationServices::SlackService, type: 'model' do
   let(:notice) { Fabricate :notice }
+  let(:problem) { notice.problem }
   let(:service_url) do
     "https://hooks.slack.com/services/XXXXXXXXX/XXXXXXXXX/XXXXXXXXX"
   end
-
   let(:service) do
     Fabricate :slack_notification_service, app:         notice.app,
-                                           service_url: service_url
+                                           service_url: service_url,
+                                           room_id:     room_id
+  end
+  let(:room_id) do
+    "#general"
   end
 
-  it "should have icon for slack" do
-    expect(Rails.root.join("docs/notifications/slack/errbit.png")).to exist
-  end
-
-  it "should send a notification to Slack with hook url" do
-    # setup
-    problem = notice.problem
-
-    # faraday stubbing
-    payload = {
+  # faraday stubbing
+  let(:payload_hash) do
+    {
       username:    "Errbit",
       icon_url:    "https://raw.githubusercontent.com/errbit/errbit/master/docs/notifications/slack/errbit.png",
+      channel:     room_id,
       attachments: [
         {
           fallback:   service.message_for_slack(problem),
@@ -52,9 +50,64 @@ describe NotificationServices::SlackService, type: 'model' do
           ]
         }
       ]
-    }.to_json
-    expect(HTTParty).to receive(:post).with(service.service_url, body: payload, headers: { "Content-Type" => "application/json" }).and_return(true)
+    }
+  end
 
-    service.create_notification(problem)
+  it "should have icon for slack" do
+    expect(Rails.root.join("docs/notifications/slack/errbit.png")).to exist
+  end
+
+  context 'Validations' do
+    it 'validates presence of service_url' do
+      service.service_url = ""
+      service.valid?
+
+      expect(service.errors[:service_url]).
+        to include("You must specify your Slack Hook url")
+
+      service.service_url = service_url
+      service.valid?
+
+      expect(service.errors[:service_url]).to be_blank
+    end
+
+    it 'validates format of room_id' do
+      service.room_id = "INVALID NAME"
+      service.valid?
+
+      expect(service.errors[:room_id]).
+        to include("Slack channel name must be lowercase, with no space, special character, or periods.")
+
+      service.room_id = "#valid-room-name"
+      service.valid?
+
+      expect(service.errors[:room_id]).to be_blank
+    end
+  end
+
+  context 'with room_id' do
+    it "should send a notification to Slack with hook url and channel" do
+      payload = payload_hash.to_json
+
+      expect(HTTParty).to receive(:post).
+        with(service.service_url, body: payload, headers: { "Content-Type" => "application/json" }).
+        and_return(true)
+
+      service.create_notification(problem)
+    end
+  end
+
+  context 'without room_id' do
+    let(:room_id) { nil }
+
+    it "should send a notification to Slack with hook url and without channel" do
+      payload = payload_hash.except(:channel).to_json
+
+      expect(HTTParty).to receive(:post).
+        with(service.service_url, body: payload, headers: { "Content-Type" => "application/json" }).
+        and_return(true)
+
+      service.create_notification(problem)
+    end
   end
 end
