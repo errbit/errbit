1cb2b900a6517723821db13af358bf8a4eee91ba
Stephen Crosby <stevecrozz@gmail.com>
2017-01-19 14:44:18 -0800

    preserving sorting on problems index #1067
    Commit is a squash merge to master from mfrederickson:fix-1052-sorting
    since. The source branch became stale and required a conflict
    resolution. Thanks to @mfrederickson for the contribution.

--- spec/views/problems/index.html.haml_spec.rb
+++ spec/views/problems/index.html.haml_spec.rb
@@ -8,7 +8,8 @@ describe "problems/index.html.haml", type: 'view' do
     allow(view).to receive(:problems).and_return(
       Kaminari.paginate_array([problem_1, problem_2]).page(1).per(10)
     )
-    allow(view).to receive(:params_sort).and_return('asc')
+    allow(view).to receive(:params_sort).and_return('last_notice_at')
+    allow(view).to receive(:params_order).and_return('asc')
     allow(controller).to receive(:current_user).and_return(Fabricate(:user))
   end
 
