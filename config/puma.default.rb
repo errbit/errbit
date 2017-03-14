c43070d9d01de44f56b7c13c68aee36c7826942d
Hassan Schroeder <hassan@cognoa.com>
2017-03-14 12:41:00 -0700

    Update for systemd with example puma.service file

--- config/puma.default.rb
+++ config/puma.default.rb
@@ -1,4 +1,5 @@
-# https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server
+directory '/home/ubuntu/errbit'
+pidfile '/home/ubuntu/errbit/tmp/pids/puma.pid'
 
 workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['MAX_THREADS'] || 5)
@@ -10,11 +11,7 @@ rackup DefaultRackup
 port ENV['PORT'] || 8080
 environment ENV['RACK_ENV'] || 'development'
 
-daemonize
-
 on_worker_boot do
-  # Worker specific setup for Rails 4.1+
-  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
   ActiveSupport.on_load(:active_record) do
     ActiveRecord::Base.establish_connection
   end
