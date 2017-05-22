7127c53fb0f6a6dd2a563ec62728d6f559494466
James Mead <james@floehopper.org>
2017-05-23 00:03:22 +0100

    Extract ErrorReport#should_email?
    Similar to the `ErrorReport#should_notify?` method.

--- app/models/error_report.rb
+++ app/models/error_report.rb
@@ -80,10 +80,14 @@ class ErrorReport
     @problem = Problem.cache_notice(@error.problem_id, @notice)
   end
 
+  def should_email?
+    app.email_at_notices.include?(@problem.notices_count)
+  end
+
   # Send email notification if needed
   def email_notification
     return false unless app.emailable?
-    return false unless app.email_at_notices.include?(@problem.notices_count)
+    return false unless should_email?
     Mailer.err_notification(self).deliver_now
   rescue => e
     HoptoadNotifier.notify(e)
