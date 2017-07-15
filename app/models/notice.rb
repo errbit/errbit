a9cccfc43fcda476e24a9493a3be95ef2d133396
Stephen Crosby <stevecrozz@gmail.com>
2017-07-14 18:44:25 -0700

    remove to_curl functionality #1205

--- app/models/notice.rb
+++ app/models/notice.rb
@@ -87,17 +87,6 @@ class Notice
     "N/A"
   end
 
-  def to_curl
-    return "N/A" if url.blank?
-    headers = %w(Accept Accept-Encoding Accept-Language Cookie Referer User-Agent).each_with_object([]) do |name, h|
-      if (value = env_vars["HTTP_#{name.underscore.upcase}"])
-        h << "-H '#{name}: #{value}'"
-      end
-    end
-
-    "curl -X #{env_vars['REQUEST_METHOD'] || 'GET'} #{headers.join(' ')} #{url}"
-  end
-
   def env_vars
     vars = request['cgi-data']
     vars.is_a?(Hash) ? vars : {}
