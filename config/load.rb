04e3c20dad0c547ab7a9eae8637b040db87c4395
Henne Vogelsang <hvogel@opensuse.org>
2017-08-15 18:15:54 +0200

    add outdated problem clearer (#1204)
    * outdated error clearer background task
    * configure deprecation interval in environment
    * add a spec for OutdatedProblemClearer

--- config/load.rb
+++ config/load.rb
@@ -23,6 +23,7 @@ Errbit::Config = Configurator.run(
   per_app_notify_at_notices: ['ERRBIT_PER_APP_NOTIFY_AT_NOTICES'],
   log_location:              ['ERRBIT_LOG_LOCATION'],
   log_level:                 ['ERRBIT_LOG_LEVEL'],
+  notice_deprecation_days:   ['ERRBIT_PROBLEM_DESTROY_AFTER_DAYS'],
 
   serve_static_assets:       ['SERVE_STATIC_ASSETS'],
   secret_key_base:           ['SECRET_KEY_BASE'],
