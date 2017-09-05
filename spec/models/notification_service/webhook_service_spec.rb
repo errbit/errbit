6c2fde9e2c50822f0dafee32ee0f52668c8b252e
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2017-09-05 19:18:19 +0200

    Fix webhook service notification payload (#1216)
    NotificationServices::WebhookService#message_for_webhook
    previously returned an Hash with a single key "problem"
    with a string as value:
    
    {
        "problem": "Stringified json here"
    }
    
    actually loosing almost all the "problem" properties.
    
    Now the "problem" key bring on a child Hash as expected.

--- spec/models/notification_service/webhook_service_spec.rb
+++ spec/models/notification_service/webhook_service_spec.rb
@@ -9,4 +9,16 @@ describe NotificationServices::WebhookService, type: 'model' do
 
     notification_service.create_notification(problem)
   end
+
+  context "#message_for_webhook" do
+    it "should return an hash with all the keys nested inside 'problem' key" do
+      notice = Fabricate :notice
+      notification_service = Fabricate :webhook_notification_service, app: notice.app
+      problem = notice.problem
+
+      payload = notification_service.message_for_webhook(problem)
+
+      expect(payload[:problem]).to be_a(Hash)
+    end
+  end
 end
