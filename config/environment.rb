99799710beaf42e83d6c61f1fae4d4afa4ff0c87
Nathan Clark <nathan@brandnewbox.com>
2018-03-20 11:11:46 -0500

    So much configuration woes.

--- config/environment.rb
+++ config/environment.rb
@@ -4,16 +4,18 @@ require File.expand_path('../application', __FILE__)
 # Load up Errbit::Config with values from the environment
 require Rails.root.join('config/load')
 
-if Errbit::Config.log_location == 'STDOUT'
-  Rails.logger = ActiveSupport::Logger.new STDOUT
-elsif Errbit::Config.log_location == 'Syslog::Logger'
-  require 'syslog/logger'
-  Rails.logger = Syslog::Logger.new('errbit', Syslog::LOG_LOCAL0)
-else
-  Rails.logger = ActiveSupport::Logger.new Errbit::Config.log_location
-end
+unless ENV["RAILS_COMPILING_ASSETS"].present?
+  if Errbit::Config.log_location == 'STDOUT'
+    Rails.logger = ActiveSupport::Logger.new STDOUT
+  elsif Errbit::Config.log_location == 'Syslog::Logger'
+    require 'syslog/logger'
+    Rails.logger = Syslog::Logger.new('errbit', Syslog::LOG_LOCAL0)
+  else
+    Rails.logger = ActiveSupport::Logger.new Errbit::Config.log_location
+  end
 
-Rails.logger.level = Errbit::Config.log_level.to_sym
+  Rails.logger.level = Errbit::Config.log_level.to_sym
+end
 
 # Initialize the Rails application.
 Rails.application.initialize!
