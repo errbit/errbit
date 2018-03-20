fa7e75d8e583212b8ee49f669c50b5042888f344
Nathan Clark <nathan@brandnewbox.com>
2018-03-20 11:16:51 -0500

    Forgot to wrap false in array.

--- config/environment.rb
+++ config/environment.rb
@@ -4,18 +4,16 @@ require File.expand_path('../application', __FILE__)
 # Load up Errbit::Config with values from the environment
 require Rails.root.join('config/load')
 
-unless ENV["RAILS_COMPILING_ASSETS"].present?
-  if Errbit::Config.log_location == 'STDOUT'
-    Rails.logger = ActiveSupport::Logger.new STDOUT
-  elsif Errbit::Config.log_location == 'Syslog::Logger'
-    require 'syslog/logger'
-    Rails.logger = Syslog::Logger.new('errbit', Syslog::LOG_LOCAL0)
-  else
-    Rails.logger = ActiveSupport::Logger.new Errbit::Config.log_location
-  end
-
-  Rails.logger.level = Errbit::Config.log_level.to_sym
+if Errbit::Config.log_location == 'STDOUT'
+  Rails.logger = ActiveSupport::Logger.new STDOUT
+elsif Errbit::Config.log_location == 'Syslog::Logger'
+  require 'syslog/logger'
+  Rails.logger = Syslog::Logger.new('errbit', Syslog::LOG_LOCAL0)
+else
+  Rails.logger = ActiveSupport::Logger.new Errbit::Config.log_location
 end
+
+Rails.logger.level = Errbit::Config.log_level.to_sym
 
 # Initialize the Rails application.
 Rails.application.initialize!
