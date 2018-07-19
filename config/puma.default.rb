edf9cb6bd5a93fddd21248e681a65ee6f613434a
Laust Rud Jacobsen <laust@valuestream.io>
2018-07-19 12:10:58 +0200

    Puma default config: ActiveRecord is not installed, do not initialize it

--- config/puma.default.rb
+++ config/puma.default.rb
@@ -9,11 +9,3 @@ preload_app!
 rackup DefaultRackup
 port ENV['PORT'] || 8080
 environment ENV['RACK_ENV'] || 'development'
-
-on_worker_boot do
-  # Worker specific setup for Rails 4.1+
-  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveSupport.on_load(:active_record) do
-    ActiveRecord::Base.establish_connection
-  end
-end
