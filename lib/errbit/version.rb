d3d1c0a036f322494fec55c03681c98a770f770e
Laust Rud Jacobsen <laust@valuestream.io>
2018-08-06 15:22:20 +0200

    Errbit::VERSION: add ENV['SOURCE_VERSION'] when available to version number

--- lib/errbit/version.rb
+++ lib/errbit/version.rb
@@ -1,3 +1,9 @@
+# frozen_string_literal: true
+
 module Errbit
-  VERSION = '0.8.0-dev'
+  VERSION = [
+    '0.8.0',
+    'dev',
+    ENV.fetch('SOURCE_VERSION', '')[0..8]
+  ].compact.join('-')
 end
