e6af1b06797dae40a3f53e574df908cb94801499
Lara Aasem <lara-aasem@hotmail.com>
2018-08-13 17:17:12 +0200

    Get relevant backtrace lines and map them to line numbers

--- app/decorators/backtrace_decorator.rb
+++ app/decorators/backtrace_decorator.rb
@@ -9,4 +9,12 @@ class BacktraceDecorator < Draper::Decorator
       line.in_app? || false
     end
   end
+
+  def non_gem_numbers_to_relative_file_paths
+    new_map = {}
+    lines.each do |line|
+      new_map[line.file_relative] = line.number if line.not_in_gem?
+    end
+    new_map
+  end
 end
