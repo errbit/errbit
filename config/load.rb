e34d9545fc951b73f2c253b96d40b1d8e8d22e4f
Ashley Hutton <ashley@brandnewbox.com>
2020-02-10 13:52:00 -0600

    Use the env vars so devise doesn't get mad.

--- config/load.rb
+++ config/load.rb
@@ -35,10 +35,10 @@ github_url:                ['GITHUB_URL', lambda do |values|
   values[:github_url].gsub(%r{/*\z}, '')
 end],
 github_authentication:     ['GITHUB_AUTHENTICATION'],
-# github_client_id:          ['GITHUB_CLIENT_ID'],
-# github_secret:             ['GITHUB_SECRET'],
-# github_org_id:             ['GITHUB_ORG_ID'],
-# github_access_scope:       ['GITHUB_ACCESS_SCOPE'],
+github_client_id:          ['GITHUB_CLIENT_ID'],
+github_secret:             ['GITHUB_SECRET'],
+github_org_id:             ['GITHUB_ORG_ID'],
+github_access_scope:       ['GITHUB_ACCESS_SCOPE'],
 # github_api_url:            ['GITHUB_API_URL'],
 # github_site_title:         ['GITHUB_SITE_TITLE'],
 # google
