dafbeec2028af144dd62b7441f4f269371d738b7
Stephen Crosby <stevecrozz@gmail.com>
2020-03-06 14:01:17 -0800

    Appease the cops

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -29,14 +29,16 @@ require 'fabrication'
 require 'sucker_punch/testing/inline'
 require 'errbit_plugin/mock_issue_tracker'
 
-if RUBY_VERSION>='2.6.0'
+if RUBY_VERSION >= '2.6.0'
   if Rails.version < '5'
-    class ActionController::TestResponse < ActionDispatch::TestResponse
-      def recycle!
-        # hack to avoid MonitorMixin double-initialize error:
-        @mon_mutex_owner_object_id = nil
-        @mon_mutex = nil
-        initialize
+    module ActionController
+      class TestResponse < ActionDispatch::TestResponse
+        def recycle!
+          # HACK: to avoid MonitorMixin double-initialize error:
+          @mon_mutex_owner_object_id = nil
+          @mon_mutex = nil
+          initialize
+        end
       end
     end
   else
