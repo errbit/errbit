027860fc65ffce979ddac3b8beced2f6533983ca
Przemysław Wróblewski <przemyslaw.wroblewski@gmail.com>
2021-07-30 18:17:01 +0200

    Change deprecated mongodb repairDatabase command to compact (#1502)

--- app/interactors/outdated_problem_clearer.rb
+++ app/interactors/outdated_problem_clearer.rb
@@ -10,7 +10,7 @@ class OutdatedProblemClearer
         criteria.each do |problem|
           ProblemDestroy.new(problem).execute
         end
-        repair_database
+        compact_database
       end
     end
   end
@@ -25,7 +25,10 @@ private
     @criteria ||= Problem.where(:last_notice_at.lt => Errbit::Config.notice_deprecation_days.to_f.days.ago)
   end
 
-  def repair_database
-    Mongoid.default_client.command repairDatabase: 1
+  def compact_database
+    collections = Mongoid.default_client.collections
+    collections.map(&:name).map do |collection|
+      Mongoid.default_client.command compact: collection
+    end
   end
 end
