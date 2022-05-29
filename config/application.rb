3ecf1bc22659383d92bdcb04750e91b52a5c4bbe
Igor Zubkov <igor.zubkov@gmail.com>
2022-05-30 00:05:45 +0100

    Update

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,18 @@
-require File.expand_path('../boot', __FILE__)
+require_relative "boot"
 
-require 'action_controller/railtie'
-require 'action_mailer/railtie'
-# require 'mongoid/railtie'
-require 'sprockets/railtie'
+require "rails"
+# Pick the frameworks you want:
+# require "active_model/railtie"
+# require "active_job/railtie"
+# require "active_record/railtie"
+# require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
+# require "action_view/railtie"
+# require "action_cable/engine"
+# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,12 +20,21 @@ Bundler.require(*Rails.groups)
 
 module Errbit
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 7.0
 
-    # Custom directories with classes and modules you want to eager load.
-    config.eager_load_paths << Rails.root.join('lib').to_s
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+
+    # Don't generate system test files.
+    config.generators.system_tests = nil
+
+    config.eager_load_paths << Rails.root.join('lib')
 
     config.before_initialize do
       config.secret_key_base = Errbit::Config.secret_key_base
@@ -26,14 +44,6 @@ module Errbit
     initializer 'errbit.mongoid', before: 'mongoid.load-config' do
       require Rails.root.join('config/mongo')
     end
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
 
     # > rails generate - config
     config.generators do |g|
