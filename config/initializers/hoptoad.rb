37235c1ec0eb33c3089178ce2a48e83f3d10fc01
Igor Zubkov <igor.zubkov@gmail.com>
2022-05-30 00:54:38 +0100

    Fix

--- config/initializers/hoptoad.rb
+++ config/initializers/hoptoad.rb
@@ -1,8 +1,26 @@
-HoptoadNotifier.configure do |config|
-  # Internal Errbit errors are stored locally, but we need
-  # to set a dummy API key so that HoptoadNotifier doesn't complain.
-  config.api_key = "---------"
+require 'hoptoad/v2'
 
-  # Don't log error that causes 404 page
-  config.ignore << "Mongoid::Errors::DocumentNotFound"
+module Hoptoad
+  class ApiVersionError < StandardError
+    def initialize
+      super "Wrong API Version: Expecting 2.0, 2.1, 2.2, 2.3 or 2.4"
+    end
+  end
+
+  class << self
+    def parse_xml!(xml)
+      parsed = ActiveSupport::XmlMini.backend.parse(xml)['notice'] || fail(ApiVersionError)
+      processor = get_version_processor(parsed['version'])
+      processor.process_notice(parsed)
+    end
+
+  private
+
+    def get_version_processor(version)
+      case version
+      when /2\.[01234]/ then Hoptoad::V2
+      else; fail ApiVersionError
+      end
+    end
+  end
 end
