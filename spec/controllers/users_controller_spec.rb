70583642d39309344cfaae11d2a989387ceefe88
Igor Zubkov <igor.zubkov@gmail.com>
2022-07-31 00:01:41 +0100

    Fix deprecations

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -23,14 +23,14 @@ describe UsersController, type: 'controller' do
 
     context "GET /users/:other_id/edit" do
       it "redirects to the home page" do
-        get :edit, id: other_user.id
+        get :edit, params: { id: other_user.id }
         expect(response).to redirect_to(root_path)
       end
     end
 
     context "GET /users/:my_id/edit" do
       it 'finds the user' do
-        get :edit, id: user.id
+        get :edit, params: { id: user.id }
         expect(controller.user).to eq(user)
         expect(response).to render_template 'edit'
       end
@@ -38,7 +38,7 @@ describe UsersController, type: 'controller' do
 
     context "PUT /users/:other_id" do
       it "redirects to the home page" do
-        put :update, id: other_user.id
+        put :update, params: { id: other_user.id }
         expect(response).to redirect_to(root_path)
       end
     end
@@ -46,47 +46,47 @@ describe UsersController, type: 'controller' do
     context "PUT /users/:my_id/id" do
       context "when the update is successful" do
         it "sets a message to display" do
-          put :update, id: user.to_param, user: { name: 'Kermit' }
+          put :update, params: { id: user.to_param, user: { name: 'Kermit' } }
           expect(request.flash[:success]).to include('updated')
         end
 
         it "redirects to the user's page" do
-          put :update, id: user.to_param, user: { name: 'Kermit' }
+          put :update, params: { id: user.to_param, user: { name: 'Kermit' } }
           expect(response).to redirect_to(user_path(user))
         end
 
         it "should not be able to become an admin" do
           expect do
-            put :update, id: user.to_param, user: { admin: true }
+            put :update, params: { id: user.to_param, user: { admin: true } }
           end.to_not change {
             user.reload.admin
           }.from(false)
         end
 
         it "should be able to set per_page option" do
-          put :update, id: user.to_param, user: { per_page: 555 }
+          put :update, params: { id: user.to_param, user: { per_page: 555 } }
           expect(user.reload.per_page).to eq 555
         end
 
         it "should be able to set time_zone option" do
-          put :update, id: user.to_param, user: { time_zone: "Warsaw" }
+          put :update, params: { id: user.to_param, user: { time_zone: "Warsaw" } }
           expect(user.reload.time_zone).to eq "Warsaw"
         end
 
         it "should be able to not set github_login option" do
-          put :update, id: user.to_param, user: { github_login: " " }
+          put :update, params: { id: user.to_param, user: { github_login: " " } }
           expect(user.reload.github_login).to eq nil
         end
 
         it "should be able to set github_login option" do
-          put :update, id: user.to_param, user: { github_login: "awesome_name" }
+          put :update, params: { id: user.to_param, user: { github_login: "awesome_name" } }
           expect(user.reload.github_login).to eq "awesome_name"
         end
       end
 
       context "when the update is unsuccessful" do
         it "renders the edit page" do
-          put :update, id: user.to_param, user: { name: nil }
+          put :update, params: { id: user.to_param, user: { name: nil } }
           expect(response).to render_template(:edit)
         end
       end
@@ -109,7 +109,7 @@ describe UsersController, type: 'controller' do
 
     context "GET /users/:id" do
       it 'finds the user' do
-        get :show, id: user.id
+        get :show, params: { id: user.id }
         expect(controller.user).to eq user
       end
     end
@@ -124,7 +124,7 @@ describe UsersController, type: 'controller' do
 
     context "GET /users/:id/edit" do
       it 'finds the user' do
-        get :edit, id: user.id
+        get :edit, params: { id: user.id }
         expect(controller.user).to eq user
       end
     end
@@ -134,24 +134,24 @@ describe UsersController, type: 'controller' do
         let(:attrs) { { user: Fabricate.to_params(:user) } }
 
         it "sets a message to display" do
-          post :create, attrs
+          post :create, params: { **attrs }
           expect(request.flash[:success]).to include('part of the team')
         end
 
         it "redirects to the user's page" do
-          post :create, attrs
+          post :create, params: { **attrs }
           expect(response).to redirect_to(user_path(controller.user))
         end
 
         it "should be able to create admin" do
           attrs[:user][:admin] = true
-          post :create, attrs
+          post :create, params: { **attrs }
           expect(response).to be_redirect
           expect(User.find(controller.user.to_param).admin).to be(true)
         end
 
         it "should has auth token" do
-          post :create, attrs
+          post :create, params: { **attrs }
           expect(User.last.authentication_token).to_not be_blank
         end
       end
@@ -160,13 +160,14 @@ describe UsersController, type: 'controller' do
         let(:user) do
           Struct.new(:admin, :attributes).new(true, {})
         end
+
         before do
           expect(User).to receive(:new).and_return(user)
           expect(user).to receive(:save).and_return(false)
         end
 
         it "renders the new page" do
-          post :create, user: { username: 'foo' }
+          post :create, params: { user: { username: 'foo' } }
           expect(response).to render_template(:new)
         end
       end
@@ -175,7 +176,7 @@ describe UsersController, type: 'controller' do
     context "PUT /users/:id" do
       context "when the update is successful" do
         before do
-          put :update, id: user.to_param, user: user_params
+          put :update, params: { id: user.to_param, user: user_params }
         end
 
         context "with normal params" do
@@ -188,7 +189,7 @@ describe UsersController, type: 'controller' do
       end
       context "when the update is unsuccessful" do
         it "renders the edit page" do
-          put :update, id: user.to_param, user: { name: nil }
+          put :update, params: { id: user.to_param, user: { name: nil } }
           expect(response).to render_template(:edit)
         end
       end
@@ -200,7 +201,7 @@ describe UsersController, type: 'controller' do
 
         before do
           expect(UserDestroy).to receive(:new).with(user).and_return(user_destroy)
-          delete :destroy, id: user.id
+          delete :destroy, params: { id: user.id }
         end
 
         it 'should destroy user' do
@@ -212,7 +213,7 @@ describe UsersController, type: 'controller' do
       context "with trying destroy himself" do
         before do
           expect(UserDestroy).to_not receive(:new)
-          delete :destroy, id: admin.id
+          delete :destroy, params: { id: admin.id }
         end
 
         it 'should not destroy user' do
