bc412ec682ef5f9c9fb95feec9640e0c9ca40637
Igor Zubkov <igor.zubkov@gmail.com>
2022-07-30 20:44:13 +0100

    Sync with rails 5.0.7.2

--- config/boot.rb
+++ config/boot.rb
@@ -1,22 +1,3 @@
-require 'rubygems'
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
-detected_ruby_version = Gem::Version.new(RUBY_VERSION.dup)
-required_ruby_version = Gem::Version.new('2.1.0') # minimum supported version
-
-if detected_ruby_version < required_ruby_version
-  fail "RUBY_VERSION must be at least #{required_ruby_version}, " \
-       "detected RUBY_VERSION #{RUBY_VERSION}"
-end
-
-# Set up gems listed in the Gemfile.
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
-
-require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
-require 'json'
-
-begin
-  # try to use Yajl, the json_gem compatibility layer must be loaded after json
-  require 'yajl/json_gem'
-rescue LoadError
-  warn "JSON: unable to load Yajl; just using the json gem"
-end
+require 'bundler/setup' # Set up gems listed in the Gemfile.
