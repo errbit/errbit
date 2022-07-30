dd5f703731d413d915b0fca794f1fb49af69c19a
Igor Zubkov <igor.zubkov@gmail.com>
2022-07-30 20:59:26 +0100

    Sync with rails 5.0.7.2

--- config/initializers/new_framework_defaults.rb
+++ config/initializers/new_framework_defaults.rb
@@ -2,6 +2,8 @@
 #
 # This file contains migration options to ease your Rails 5.0 upgrade.
 #
+# Once upgraded flip defaults one by one to migrate to the new default.
+#
 # Read the Guide for Upgrading Ruby on Rails for more info on each option.
 
 Rails.application.config.action_controller.raise_on_unfiltered_parameters = true
