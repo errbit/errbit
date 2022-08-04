9be67bab556dcbe05ce18d3cb5f042fa0af1d6e6
Igor Zubkov <igor.zubkov@gmail.com>
2022-08-05 00:19:57 +0100

    Sync with rails 5.0

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,9 +34,6 @@ Rails.application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
-  # Raise an error on page load if there are pending migrations.
-  # config.active_record.migration_error = :page_load
-
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
@@ -50,5 +47,5 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
