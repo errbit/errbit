851de6516623c79602e88c68a3b75f5b712841fb
Igor Zubkov <igor.zubkov@gmail.com>
2022-08-27 02:59:58 +0100

    Sync with Rails 5.1

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,6 @@ require_relative 'boot'
 
 require 'action_controller/railtie'
 require 'action_mailer/railtie'
-# require 'mongoid/railtie'
 require 'sprockets/railtie'
 
 # Require the gems listed in Gemfile, including any gems
@@ -11,6 +10,9 @@ Bundler.require(*Rails.groups)
 
 module Errbit
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
