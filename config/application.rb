7cdac6ab814810d73c813570d5ef177fb125101e
Igor Zubkov <igor.zubkov@gmail.com>
2022-09-24 03:25:10 +0100

    Load rails default from 6.1

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,10 @@ Bundler.require(*Rails.groups)
 module Errbit
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 6.1
+
+    # config.autoloader = :zeitwerk
+    config.autoloader = :classic
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
