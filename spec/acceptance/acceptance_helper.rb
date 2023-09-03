4c5829f4f000be219213f2718d991bd396b4fc4a
runner <runner@berkeley.edu>
2023-09-02 19:54:01 -0700

    wip

---
+++ spec/acceptance/acceptance_helper.rb
@@ -0,0 +1,35 @@
+require 'phantomjs/poltergeist'
+require 'capybara/rspec'
+require 'capybara/poltergeist'
+
+Capybara.javascript_driver = :poltergeist
+
+OmniAuth.config.test_mode = true
+
+def mock_auth(user = "test_user", token = "abcdef")
+  OmniAuth.config.mock_auth[:github] = Hashie::Mash.new(
+    'provider'    => 'github',
+    'uid'         => '1763',
+    'extra'       => {
+      'raw_info' => {
+        'login' => user
+      }
+    },
+    'credentials' => {
+      'token' => token
+    }
+  )
+
+  OmniAuth.config.mock_auth[:google_oauth2] = Hashie::Mash.new(
+    provider: 'google_oauth2',
+    uid:      user,
+    info:     { email: 'errbit@errbit.example.com', name: "Existing User" }
+  )
+end
+
+def log_in(user)
+  visit '/'
+  fill_in :user_email, with: user.email
+  fill_in :user_password, with: 'password'
+  click_on I18n.t('devise.sessions.new.sign_in')
+end
