9d2c12272c593ce2c543ab95dfcfe1cf644da115
Igor Zubkov <igor.zubkov@gmail.com>
2025-02-28 21:40:45 +0100

    Cleanup

--- config/initializers/ssl_enforcer.rb
+++
@@ -1,8 +0,0 @@
-# Enforce SSL connections, if configured
-if Errbit::Config.enforce_ssl
-  require 'rack/ssl-enforcer'
-  ActionMailer::Base.default_url_options[:protocol] = 'https://'
-  Rails.application.configure do
-    config.middleware.use Rack::SslEnforcer
-  end
-end
