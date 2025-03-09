74efce1699c9602eb805952e660aac6fe82ccf68
Igor Zubkov <igor.zubkov@gmail.com>
2025-03-09 15:34:40 +0100

    Rails: load_defaults 5.1

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ Bundler.require(*Rails.groups)
 
 module Errbit
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
