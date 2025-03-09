74005c060c8c16d54e199c1e37684759e91b11c2
Igor Zubkov <igor.zubkov@gmail.com>
2025-03-09 15:44:36 +0100

    Deprecate and remove SERVE_STATIC_ASSETS env support

--- config/initializers/deprecations.rb
+++ config/initializers/deprecations.rb
@@ -1,8 +1,16 @@
 if ENV["USER_GEMFILE"].present?
   # Make it error in v0.11.0 release and remove in v0.12.0
   ActiveSupport::Deprecation.warn(
-    "ENV['USER_GEMFILE'] support is and has no effect in Errbit v0.10.0. " \
+    "ENV['USER_GEMFILE'] support is removed and has no effect in Errbit v0.10.0. " \
     "Remove it from configuration. " \
     "From Errbit v0.10.0+ it's always 'UserGemfile'."
+  )
+end
+
+if ENV["SERVE_STATIC_ASSETS"].present?
+  # Make it error in v0.11.0 release and remove in v0.12.0
+  ActiveSupport::Deprecation.warn(
+    "ENV['SERVE_STATIC_ASSETS'] support is removed and has no effect in Errbit v0.10.0. " \
+    "Replace it with Ruby on Rails ENV['RAILS_SERVE_STATIC_FILES']. "
   )
 end
