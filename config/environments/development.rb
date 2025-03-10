3bf0c383355a5b06acaeea76da8ed2fe79385431
Igor Zubkov <igor.zubkov@gmail.com>
2025-03-10 12:06:57 +0100

    Sync with Rails 5.2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,12 +13,13 @@ Rails.application.configure do
   config.consider_all_requests_local = true
 
   # Enable/disable caching. By default caching is disabled.
+  # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = true
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      'Cache-Control' => 'public, max-age=172800'
+      'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
   else
     config.action_controller.perform_caching = false
