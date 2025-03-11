38258b860689082cca73e7dd9d7b3e9129a17d29
Igor Zubkov <igor.zubkov@gmail.com>
2025-03-11 13:52:50 +0100

    Switch rails autoloader from :classic to :zeitwerk

--- config/application.rb
+++ config/application.rb
@@ -14,8 +14,6 @@ module Errbit
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    config.autoloader = :classic
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
