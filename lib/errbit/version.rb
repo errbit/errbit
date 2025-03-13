264f248329b6862751461868cbbefa280c25cacd
Igor Zubkov <igor.zubkov@gmail.com>
2025-03-13 14:20:59 +0100

    Rubocop: Style/StringLiterals

--- lib/errbit/version.rb
+++ lib/errbit/version.rb
@@ -13,17 +13,17 @@ module Errbit
         full << "dev"
         full << source_version
       end
-      full.compact.join('-')
+      full.compact.join("-")
     end
 
     def source_version
-      source_version = ENV['SOURCE_VERSION']
+      source_version = ENV["SOURCE_VERSION"]
       source_version[0...8] if source_version.present?
     end
 
     class << self
       def to_s
-        new('0.10.0', true).full_version
+        new("0.10.0", true).full_version
       end
     end
   end
