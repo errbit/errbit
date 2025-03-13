3f8ecbc763b4243c2263f04cf29a8564d1203b9d
Igor Zubkov <igor.zubkov@gmail.com>
2025-03-13 14:24:40 +0100

    Rubocop: Style/StringLiterals

--- spec/views/users/edit.html.haml_spec.rb
+++ spec/views/users/edit.html.haml_spec.rb
@@ -1,15 +1,15 @@
-describe 'users/edit.html.haml', type: 'view' do
-  let(:user) { stub_model(User, name: 'shingara') }
+describe "users/edit.html.haml", type: "view" do
+  let(:user) { stub_model(User, name: "shingara") }
   before do
     allow(view).to receive(:current_user).and_return(user)
     allow(view).to receive(:user).and_return(user)
   end
-  it 'should have per_page option' do
+  it "should have per_page option" do
     render
     expect(rendered).to match(/id="user_per_page"/)
   end
 
-  it 'should have time_zone option' do
+  it "should have time_zone option" do
     render
     expect(rendered).to match(/id="user_time_zone"/)
   end
