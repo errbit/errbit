d933d0a3d2df16ec27e0fb74d0d71a9d107aa8f7
Igor Zubkov <igor.zubkov@gmail.com>
2025-03-19 00:39:24 +0100

    Namespace

--- app/controllers/api/v1/notices_controller.rb
+++ app/controllers/api/v1/notices_controller.rb
@@ -1,25 +1,29 @@
 # frozen_string_literal: true
 
-class Api::V1::NoticesController < ApplicationController
-  respond_to :json, :xml
+module Api
+  module V1
+    class NoticesController < ApplicationController
+      respond_to :json, :xml
 
-  def index
-    query = {}
-    fields = ["created_at", "message", "error_class"]
+      def index
+        query = {}
+        fields = ["created_at", "message", "error_class"]
 
-    if params.key?(:start_date) && params.key?(:end_date)
-      start_date = Time.zone.parse(params[:start_date]).utc
-      end_date = Time.zone.parse(params[:end_date]).utc
-      query = {created_at: {"$lte" => end_date, "$gte" => start_date}}
-    end
+        if params.key?(:start_date) && params.key?(:end_date)
+          start_date = Time.zone.parse(params[:start_date]).utc
+          end_date = Time.zone.parse(params[:end_date]).utc
+          query = {created_at: {"$lte" => end_date, "$gte" => start_date}}
+        end
 
-    results = benchmark("[api/v1/notices_controller] query time") do
-      Notice.where(query).only(fields).to_a
-    end
+        results = benchmark("[api/v1/notices_controller] query time") do
+          Notice.where(query).only(fields).to_a
+        end
 
-    respond_to do |format|
-      format.any(:html, :json) { render json: JSON.dump(results) } # render JSON if no extension specified on path
-      format.xml { render xml: results }
+        respond_to do |format|
+          format.any(:html, :json) { render json: JSON.dump(results) } # render JSON if no extension specified on path
+          format.xml { render xml: results }
+        end
+      end
     end
   end
 end
