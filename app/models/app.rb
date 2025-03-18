c5aa5a9f048d093bb2974c6e75048febabc12993
Igor Zubkov <igor.zubkov@gmail.com>
2025-03-18 23:06:30 +0100

    Fix

--- app/models/app.rb
+++ app/models/app.rb
@@ -221,10 +221,12 @@ class App
 
   def normalize_github_repo
     return if github_repo.blank?
+
     github_host = URI.parse(Errbit::Config.github_url).host
     github_host = ::Regexp.escape(github_host)
-    github_repo.strip!
-    github_repo.sub!(%r{(git@|https?://)#{github_host}(/|:)}, "")
-    github_repo.sub!(/\.git$/, "")
+
+    self.github_repo = github_repo.strip
+    self.github_repo = github_repo.sub(%r{(git@|https?://)#{github_host}(/|:)}, "")
+    self.github_repo = github_repo.sub(/\.git$/, "")
   end
 end
