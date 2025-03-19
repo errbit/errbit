43f094a3a95420e4f570b1d62b2ea3ab812c1192
Igor Zubkov <igor.zubkov@gmail.com>
2025-03-19 20:31:11 +0100

    Remove Errbit::Config.port

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -27,8 +27,7 @@ end
 # Set config specific values
 (ActionMailer::Base.default_url_options ||= {}).tap do |default|
   options_from_config = {
-    host: Errbit::Config.host,
-    port: Errbit::Config.port
+    host: Errbit::Config.host
   }.select { |_k, v| v }
 
   default.reverse_merge!(options_from_config)
