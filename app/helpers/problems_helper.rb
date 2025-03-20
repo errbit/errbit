515284a0b34f3ba776d028b4d7d52aeccf6402d5
Igor Zubkov <igor.zubkov@gmail.com>
2025-03-20 11:45:46 +0100

    Cleanup

--- app/helpers/problems_helper.rb
+++ app/helpers/problems_helper.rb
@@ -14,20 +14,21 @@ module ProblemsHelper
   end
 
   def gravatar_tag(email, options = {})
-    return unless email.present?
+    return if email.blank?
 
     image_tag gravatar_url(email, options), alt: email, class: "gravatar"
   end
 
   def gravatar_url(email, options = {})
-    return unless email.present?
+    return if email.blank?
 
     default_options = {
       d: Errbit::Config.gravatar_default
     }
     options.reverse_merge! default_options
-    params = options.extract!(:s, :d).delete_if { |_k, v| v.blank? }
+    params = options.extract!(:s, :d).delete_if { |_, v| v.blank? }
     email_hash = Digest::MD5.hexdigest(email)
+    # TODO: remove request.ssl? and related code
     url = request.ssl? ? "https://secure.gravatar.com" : "http://www.gravatar.com"
     "#{url}/avatar/#{email_hash}?#{params.to_query}"
   end
