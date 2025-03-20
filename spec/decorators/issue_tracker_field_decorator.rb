8aebc844a21ed3b0771af0fbbe0e3f6ff115c25e
Igor Zubkov <igor.zubkov@gmail.com>
2025-03-20 23:48:15 +0100

    Add TODO note

--- spec/decorators/issue_tracker_field_decorator.rb
+++ spec/decorators/issue_tracker_field_decorator.rb
@@ -2,6 +2,7 @@
 
 require "rails_helper"
 
+# TODO: rename this file to _spec.rb
 RSpec.describe IssueTrackerFieldDecorator, type: :decorator do
   describe "#label" do
     it "return the label of field_info by default" do
