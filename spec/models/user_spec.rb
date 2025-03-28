a4669f5f2ad30aef29c2cc3ce04003bcfea31b4f
Igor Zubkov <igor.zubkov@gmail.com>
2025-03-28 10:43:05 +0100

    Devise password length (#1803)
    * Devise: config.password_length. 6..128 -> 8..128.
    
    * Cleanup tests
    
    * Fix tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,50 +6,60 @@ RSpec.describe User, type: :model do
   context "validations" do
     it "require that a name is present" do
       user = Fabricate.build(:user, name: nil)
-      expect(user).not_to be_valid
-      expect(user.errors[:name]).to include("can't be blank")
+
+      expect(user.valid?).to eq(false)
+
+      expect(user.errors[:name]).to eq(["can't be blank"])
     end
 
     it "requires password without github login" do
       user = Fabricate.build(:user, password: nil)
-      expect(user).not_to be_valid
-      expect(user.errors[:password]).to include("can't be blank")
+
+      expect(user.valid?).to eq(false)
+
+      expect(user.errors[:password]).to eq(["can't be blank"])
     end
 
     it "doesn't require password with github login" do
       user = Fabricate.build(:user, password: nil, github_login: "nashby")
-      expect(user).to be_valid
+
+      expect(user.valid?).to eq(true)
     end
 
     it "requires uniq github login" do
       user1 = Fabricate(:user, github_login: "nashby")
-      expect(user1).to be_valid
+      expect(user1.valid?).to eq(true)
 
       user2 = Fabricate.build(:user, github_login: "nashby")
       user2.save
-      expect(user2).not_to be_valid
+      expect(user2.valid?).to eq(false)
+
       expect(user2.errors[:github_login]).to eq(["has already been taken"])
     end
 
     it "allows blank / null github_login" do
       user1 = Fabricate(:user, github_login: " ")
-      expect(user1).to be_valid
+      expect(user1.valid?).to eq(true)
 
       user2 = Fabricate.build(:user, github_login: " ")
       user2.save
-      expect(user2).to be_valid
+
+      expect(user2.valid?).to eq(true)
     end
 
     it "disables validations when reset password" do
       user = Fabricate.build(:user, email: "")
       user.save(validate: false)
-      expect(user.reset_password("Password123", "Password123")).to be_truthy
+
+      expect(user.reset_password("Password123", "Password123")).to eq(true)
     end
 
     it "should require a password with minimum of 6 characters" do
       user = Fabricate.build(:user)
-      user.reset_password("12345", "12345")
-      expect(user.errors[:password]).to include("is too short (minimum is 6 characters)", "is too short (minimum is 6 characters)")
+
+      user.reset_password("1234567", "1234578")
+
+      expect(user.errors[:password]).to eq(["is too short (minimum is 8 characters)"])
     end
   end
 
