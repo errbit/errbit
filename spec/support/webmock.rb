6cd1666bbf540a376dee1dbf034269905ca79984
Igor Zubkov <igor.zubkov@gmail.com>
2025-04-01 11:27:23 +0200

    Configure webmock gem

---
+++ spec/support/webmock.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "webmock/rspec"
+
+allowed_sites = [
+  "https://chromedriver.storage.googleapis.com",
+  "https://github.com/mozilla/geckodriver/releases",
+  "https://selenium-release.storage.googleapis.com",
+  "https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver"
+]
+
+WebMock.disable_net_connect!(allow_localhost: true, allow: allowed_sites)
