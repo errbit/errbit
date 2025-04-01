8bc2ffb3b8c72f7c9551cab035768180d6caeb7c
Igor Zubkov <igor.zubkov@gmail.com>
2025-04-01 13:15:09 +0200

    Cleanup

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -4,12 +4,6 @@ VCR.configure do |c|
   c.cassette_library_dir = Rails.root.join("spec/cassettes")
   c.hook_into :webmock
   c.ignore_localhost = true
-  c.ignore_hosts(
-    # "chromedriver.storage.googleapis.com",
-    # "github.com/mozilla/geckodriver/releases",
-    # "selenium-release.storage.googleapis.com",
-    # "developer.microsoft.com/en-us/microsoft-edge/tools/webdriver"
-  )
   c.default_cassette_options = {
     decode_compressed_response: true,
     allow_unused_http_interactions: false
--- spec/support/webmock.rb
+++ spec/support/webmock.rb
@@ -2,11 +2,4 @@
 
 require "webmock/rspec"
 
-allowed_sites = [
-  # "https://chromedriver.storage.googleapis.com",
-  # "https://github.com/mozilla/geckodriver/releases",
-  # "https://selenium-release.storage.googleapis.com",
-  # "https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver"
-]
-
-WebMock.disable_net_connect!(allow_localhost: true, allow: allowed_sites)
+WebMock.disable_net_connect!
