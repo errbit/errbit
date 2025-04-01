faee4ecfc3125ec9d98b31dab6996a0140064689
Igor Zubkov <igor.zubkov@gmail.com>
2025-04-01 11:28:22 +0200

    Indent

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -4,10 +4,12 @@ VCR.configure do |c|
   c.cassette_library_dir = Rails.root.join("spec/cassettes")
   c.hook_into :webmock
   c.ignore_localhost = true
-  c.ignore_hosts("chromedriver.storage.googleapis.com",
+  c.ignore_hosts(
+    "chromedriver.storage.googleapis.com",
     "github.com/mozilla/geckodriver/releases",
     "selenium-release.storage.googleapis.com",
-    "developer.microsoft.com/en-us/microsoft-edge/tools/webdriver")
+    "developer.microsoft.com/en-us/microsoft-edge/tools/webdriver"
+  )
   c.default_cassette_options = {
     decode_compressed_response: true,
     allow_unused_http_interactions: false
