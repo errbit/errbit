0724f513d78766bfc5d5f7518a33f0d43f13cd26
Igor Zubkov <igor.zubkov@gmail.com>
2025-04-01 11:31:33 +0200

    Block all

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -5,10 +5,10 @@ VCR.configure do |c|
   c.hook_into :webmock
   c.ignore_localhost = true
   c.ignore_hosts(
-    "chromedriver.storage.googleapis.com",
-    "github.com/mozilla/geckodriver/releases",
-    "selenium-release.storage.googleapis.com",
-    "developer.microsoft.com/en-us/microsoft-edge/tools/webdriver"
+    # "chromedriver.storage.googleapis.com",
+    # "github.com/mozilla/geckodriver/releases",
+    # "selenium-release.storage.googleapis.com",
+    # "developer.microsoft.com/en-us/microsoft-edge/tools/webdriver"
   )
   c.default_cassette_options = {
     decode_compressed_response: true,
