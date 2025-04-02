ff1bab17239eb55ac3c727c4a6be862e9a4503aa
Igor Zubkov <igor.zubkov@gmail.com>
2025-04-02 12:57:01 +0200

    More tests

--- spec/acceptance/sign_in_with_google_spec.rb
+++ spec/acceptance/sign_in_with_google_spec.rb
@@ -2,28 +2,6 @@
 
 require "acceptance/acceptance_helper"
 
-RSpec.feature "Sign in with Google" do
-  background do
-    allow(Errbit::Config).to receive(:google_authentication).and_return(true)
-    Fabricate(:user, google_uid: "nashby")
-    visit root_path
-  end
-
-  scenario "log in via Google with recognized user" do
-    mock_auth("nashby")
-
-    click_link "Sign in with Google"
-    expect(page).to have_content I18n.t("devise.omniauth_callbacks.success", kind: "Google")
-  end
-
-  scenario "reject unrecognized user if authenticating via Google" do
-    mock_auth("unknown_user")
-
-    click_link "Sign in with Google"
-    expect(page).to have_content "There are no authorized users with Google login"
-  end
-end
-
 RSpec.feature "Sign in with Google with auto provision" do
   background do
     allow(Errbit::Config).to receive(:google_authentication).and_return(true)
--- spec/system/sign_in_with_google_spec.rb
+++ spec/system/sign_in_with_google_spec.rb
@@ -5,7 +5,7 @@ require "rails_helper"
 RSpec.describe "Sign in with Google", type: :system do
   # before { driven_by(:selenium_chrome_headless) }
 
-  context "log in via Google with recognized user" do
+  context "sign in via Google with recognized user" do
     let!(:user) { Fabricate(:user, google_uid: "123456789") }
 
     before { expect(Errbit::Config).to receive(:google_authentication).and_return(true) }
@@ -20,6 +20,16 @@ RSpec.describe "Sign in with Google", type: :system do
       click_link "Sign in with Google"
 
       expect(page).to have_content(I18n.t("devise.omniauth_callbacks.success", kind: "Google"))
+    end
+  end
+
+  context "reject unrecognized user" do
+    it "is expected to reject unrecognized user" do
+      visit root_path
+
+      click_link "Sign in with Google"
+
+      expect(page).to have_content("There are no authorized users with Google login")
     end
   end
 end
