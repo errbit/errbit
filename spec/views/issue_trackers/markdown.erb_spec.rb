d7c032153c9330c0f124e50ce28781254e4ab947
Igor Zubkov <igor.zubkov@gmail.com>
2025-04-04 19:01:53 +0200

    Fix tests

--- spec/views/issue_trackers/markdown.erb_spec.rb
+++ spec/views/issue_trackers/markdown.erb_spec.rb
@@ -2,7 +2,7 @@
 
 require "rails_helper"
 
-RSpec.describe "issue_trackers/issue.txt.erb", type: :view do
+RSpec.describe "issue_trackers/markdown.erb", type: :view do
   let(:problem) do
     problem = Fabricate(:problem)
     Fabricate(:notice, err: Fabricate(:err, problem: problem))
@@ -10,13 +10,12 @@ RSpec.describe "issue_trackers/issue.txt.erb", type: :view do
   end
 
   before do
-    allow(view).to receive(:problem).and_return(
-      ProblemDecorator.new(problem)
-    )
+    allow(view).to receive(:problem).and_return(ProblemDecorator.new(problem))
   end
 
   it "has the problem url" do
     render
+
     expect(rendered).to match(app_problem_url(problem.app, problem))
   end
 end
--- spec/views/issue_trackers/text.erb_spec.rb
+++ spec/views/issue_trackers/text.erb_spec.rb
@@ -2,7 +2,7 @@
 
 require "rails_helper"
 
-RSpec.describe "issue_trackers/issue.md.erb", type: :view do
+RSpec.describe "issue_trackers/text.erb", type: :view do
   let(:problem) do
     problem = Fabricate(:problem)
     Fabricate(:notice, err: Fabricate(:err, problem: problem))
@@ -10,11 +10,14 @@ RSpec.describe "issue_trackers/issue.md.erb", type: :view do
   end
 
   before do
-    allow(view).to receive(:problem).and_return(ProblemDecorator.new(problem))
+    allow(view).to receive(:problem).and_return(
+      ProblemDecorator.new(problem)
+    )
   end
 
   it "has the problem url" do
-    render template: "issue_trackers/issue.md.erb"
+    render
+
     expect(rendered).to match(app_problem_url(problem.app, problem))
   end
 end
