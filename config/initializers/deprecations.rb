f33320e6933beb98965920b5ea8d996cd1fe6d36
Igor Zubkov <igor.zubkov@gmail.com>
2025-04-06 23:04:51 +0200

    Cleanup

--- config/initializers/deprecations.rb
+++ config/initializers/deprecations.rb
@@ -13,7 +13,7 @@ if ENV["SERVE_STATIC_ASSETS"].present?
   # Make it error in v0.11.0 release and remove in v0.12.0
   ActiveSupport::Deprecation.warn(
     "ENV['SERVE_STATIC_ASSETS'] support is removed and has no effect in Errbit v0.10.0. " \
-    "Replace it with build-in Ruby on Rails ENV['RAILS_SERVE_STATIC_FILES']."
+    "Remove it from configuration."
   )
 end
 
