096753d05b6d59f82c168fececd114744b196324
Igor Zubkov <igor.zubkov@gmail.com>
2025-04-06 15:47:44 +0200

    Add Rails health check at: /up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
   devise_for :users, controllers: {omniauth_callbacks: "users/omniauth_callbacks"}
 
   # Hoptoad Notifier Routes
