4d3a76552c9c9d92612f9e8a359aad6259d82f44
Igor Zubkov <igor.zubkov@gmail.com>
2025-04-10 15:53:07 +0200

    Add debug

--- spec/support/selenium-debug.rb
+++ spec/support/selenium-debug.rb
@@ -1,24 +1,24 @@
 # frozen_string_literal: true
 
 # https://bibwild.wordpress.com/2024/10/08/getting-rspec-capybara-browser-console-output-for-failed-tests/
-RSpec.configure do |config|
-  # hacky way to inject browser logs into failure message for failed ones
-  config.after(:each) do
-    if example.exception
-      browser_logs = page.driver.browser.logs.get(:browser).collect { |log| "#{log.level}: #{log.message}" }
-
-      if browser_logs.present?
-        # pretty hacky internal way to get browser logs into
-        # existing long-form failure message, when that is
-        # stored in exception associated with assertion failure
-        new_exception = example.exception.class.new("#{example.exception.message}\n\nBrowser console:\n\n#{browser_logs.join("\n")}\n")
-        new_exception.set_backtrace(example.exception.backtrace)
-
-        example.display_exception = new_exception
-      end
-    end
-  end
-end
+# RSpec.configure do |config|
+#   # hacky way to inject browser logs into failure message for failed ones
+#   config.after(:each) do
+#     if example.exception
+#       browser_logs = page.driver.browser.logs.get(:browser).collect { |log| "#{log.level}: #{log.message}" }
+#
+#       if browser_logs.present?
+#         # pretty hacky internal way to get browser logs into
+#         # existing long-form failure message, when that is
+#         # stored in exception associated with assertion failure
+#         new_exception = example.exception.class.new("#{example.exception.message}\n\nBrowser console:\n\n#{browser_logs.join("\n")}\n")
+#         new_exception.set_backtrace(example.exception.backtrace)
+#
+#         example.display_exception = new_exception
+#       end
+#     end
+#   end
+# end
 
 Capybara.javascript_driver = :my_headless_chrome
 
