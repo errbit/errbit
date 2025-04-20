0fd6a6fb74ab633db61fd591bdab013278dfffc5
Igor Zubkov <igor.zubkov@gmail.com>
2025-04-20 15:38:09 +0200

    Fix i18n (#1902)
    * Fix test
    
    * Add .tool-versions
    
    * Enable i18n.raise_on_missing_translations
    
    * Add missing translations
    
    * Fix i18n
    
    * Fix i18n
    
    * Cleanup
    
    * Cleanup
    
    * Fix missing translations
    
    * Fix i18n
    
    * Fix typo
    
    * Fix i18n
    
    * Missing translation
    
    * Missing translations
    
    * Remove dead translations
    
    * Missing translations
    
    * Raises error for missing translations for development env
    
    * Fix i18n
    
    * Add missing translations
    
    * Add missing translations
    
    * Fix strict locals
    
    * Fix i18n
    
    * Cleanup
    
    * Cleanup

--- spec/controllers/problems_controller_spec.rb
+++ spec/controllers/problems_controller_spec.rb
@@ -302,7 +302,7 @@ RSpec.describe ProblemsController, type: :controller do
           )
           post :create_issue, params: {app_id: problem.app.id, id: problem.id, format: "html"}
           line = issue_tracker.tracker.output.shift
-          expect(line[1]).to eq("one #{problem.id} two")
+          expect(line[1]).to include("one #{problem.id} two")
         end
       end
     end
