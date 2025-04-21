99342ef066520e42e9d1a45371c14100b3dfbe85
Igor Zubkov <igor.zubkov@gmail.com>
2025-04-21 13:46:46 +0200

    Add tests

--- spec/policies/application_policy_spec.rb
+++ spec/policies/application_policy_spec.rb
@@ -27,3 +27,33 @@ RSpec.describe ApplicationPolicy do
     it { expect { subject }.to raise_error(Pundit::NotAuthorizedError) }
   end
 end
+
+RSpec.describe ApplicationPolicy::Scope do
+  describe "#initialize" do
+    context "when user is not present" do
+      let(:user) { nil }
+
+      let(:scope) { instance_double(Mongoid::Criteria) }
+
+      it { expect { described_class.new(user, scope) }.to raise_error(Pundit::NotAuthorizedError) }
+    end
+
+    context "when user is present" do
+      let(:user) { Fabricate(:user) }
+
+      let(:scope) { instance_double(Mongoid::Criteria) }
+
+      it { expect { described_class.new(user, scope) }.not_to raise_error }
+    end
+  end
+
+  describe "#resolve" do
+    let(:user) { Fabricate(:user) }
+
+    let(:scope) { instance_double(Mongoid::Criteria) }
+
+    subject { described_class.new(user, scope) }
+
+    it { expect { subject.resolve }.to raise_error(NotImplementedError, "You must define #resolve in ApplicationPolicy::Scope") }
+  end
+end
