a328167db656131f92f8f49c45651991ce444b5d
Igor Zubkov <igor.zubkov@gmail.com>
2025-04-23 19:50:59 +0200

    Add tests

--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -50,4 +50,22 @@ RSpec.describe UsersController, type: :request do
       end
     end
   end
+
+  describe "#show" do
+    context "when user is logged in" do
+
+    end
+
+    context "when user is not logged in" do
+      let!(:user) { create(:user) }
+
+      before { get user_path(user) }
+
+      it "is expected to redirect to new user session url with status found" do
+        expect(response).to redirect_to(new_user_session_url)
+
+        expect(response).to have_http_status(:found)
+      end
+    end
+  end
 end
