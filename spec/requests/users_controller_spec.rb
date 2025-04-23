e51962d7fc04941d3fbfd76aed04597121130bed
Igor Zubkov <igor.zubkov@gmail.com>
2025-04-23 19:39:33 +0200

    Add tests

--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -6,7 +6,17 @@ RSpec.describe UsersController, type: :request do
   describe "#index" do
     context "when user is logged in" do
       context "when user is an admin" do
+        let(:user) { create(:user, admin: true) }
 
+        before { sign_in(user) }
+
+        before { get users_path }
+
+        it "is expected to render template index with status ok" do
+          expect(response).to render_template(:index)
+
+          expect(response).to have_http_status(:ok)
+        end
       end
 
       context "when user is not an admin" do
