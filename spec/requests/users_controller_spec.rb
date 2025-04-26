0b33620b9a9f904a4937d9915be2b5d71644cf9b
Igor Zubkov <igor.zubkov@gmail.com>
2025-04-26 21:19:50 +0200

    More tests

--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -149,6 +149,58 @@ RSpec.describe UsersController, type: :request do
     end
   end
 
+  describe "#edit" do
+    context "when user is logged in" do
+      context "when user has access" do
+        let(:current_user) { create(:user, admin: true) }
+
+        let!(:user) { create(:user, admin: false) }
+
+        before { sign_in(current_user) }
+
+        before { get edit_user_path(user) }
+
+        it "is expected to render template edit with status ok" do
+          expect(response).to render_template(:edit)
+
+          expect(response).to have_http_status(:ok)
+
+          expect(assigns(:user)).to eq(user)
+        end
+      end
+
+      context "when user has not access" do
+        let(:current_user) { create(:user, admin: false) }
+
+        let(:user) { create(:user, admin: false) }
+
+        before { sign_in(current_user) }
+
+        before { get edit_user_path(user) }
+
+        it "is expected to redirect to root path with status found" do
+          expect(response).to redirect_to(root_path)
+
+          expect(response).to have_http_status(:found)
+
+          expect(request.flash[:alert]).to eq("You are not authorized to perform this action.")
+        end
+      end
+    end
+
+    context "when user is not logged in" do
+      let(:user) { create(:user) }
+
+      before { get edit_user_path(user) }
+
+      it "is expected to redirect to new user session path with status found" do
+        expect(response).to redirect_to(new_user_session_path)
+
+        expect(response).to have_http_status(:found)
+      end
+    end
+  end
+
 #   describe "#edit" do
 #     context "when user is logged in" do
 #       context "when user is an admin" do
