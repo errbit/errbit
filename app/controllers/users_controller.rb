c0515366943fc99e5d854ea424a8758d07740cd8
Igor Zubkov <igor.zubkov@gmail.com>
2025-04-26 22:56:44 +0200

    Cleanup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -84,23 +84,4 @@ class UsersController < ApplicationController
     can_edit = current_user == user || current_user.admin?
     redirect_to(root_path) unless can_edit
   end
-
-  # def user_params
-  #   params.require(:user).permit(policy(@user).permitted_attributes)
-  # end
-
-  # def user_params
-  #   @user_params ||= params[:user] ? params.require(:user).permit(*user_permit_params) : {}
-  # end
-  #
-  # def user_permit_params
-  #   @user_permit_params ||= [:name, :username, :email, :github_login, :per_page, :time_zone]
-  #   @user_permit_params << :admin if current_user.admin? && current_user.id != params[:id]
-  #   @user_permit_params |= [:password, :password_confirmation] if user_password_params.values.all?(&:present?)
-  #   @user_permit_params
-  # end
-
-  # def user_password_params
-  #   @user_password_params ||= params[:user] ? params.require(:user).permit(:password, :password_confirmation) : {}
-  # end
 end
