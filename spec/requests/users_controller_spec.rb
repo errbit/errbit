5312f50f80bda198449399db4751335bf5118a1c
Igor Zubkov <igor.zubkov@gmail.com>
2025-04-26 20:23:43 +0200

    More tests

--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -175,52 +175,52 @@ RSpec.describe UsersController, type: :request do
     end
   end
 
-  # describe "#new" do
-  #   context "when user is logged in" do
-  #     context "when user is an admin" do
-  #       let(:user) { create(:user, admin: true) }
-  #
-  #       before { sign_in(user) }
-  #
-  #       before { get new_user_path }
-  #
-  #       it "is expected to render template new with status ok" do
-  #         expect(response).to render_template(:new)
-  #
-  #         expect(response).to have_http_status(:ok)
-  #
-  #         expect(assigns(:user).new_record?).to eq(true)
-  #       end
-  #     end
-  #
-  #     context "when user is not an admin" do
-  #       let(:user) { create(:user, admin: false) }
-  #
-  #       before { sign_in(user) }
-  #
-  #       before { get new_user_path }
-  #
-  #       it "is expected to redirect to root path with status found" do
-  #         expect(response).to redirect_to(root_path)
-  #
-  #         expect(response).to have_http_status(:found)
-  #
-  #         expect(request.flash[:alert]).to eq("You are not authorized to perform this action.")
-  #       end
-  #     end
-  #   end
-  #
-  #   context "when user is not logged in" do
-  #     before { get new_user_path }
-  #
-  #     it "is expected to redirect to new user session path with status found" do
-  #       expect(response).to redirect_to(new_user_session_path)
-  #
-  #       expect(response).to have_http_status(:found)
-  #     end
-  #   end
-  # end
-  #
+  describe "#new" do
+    context "when user is logged in" do
+      context "when user is an admin" do
+        let(:current_user) { create(:user, admin: true) }
+
+        before { sign_in(current_user) }
+
+        before { get new_user_path }
+
+        it "is expected to render template new with status ok" do
+          expect(response).to render_template(:new)
+
+          expect(response).to have_http_status(:ok)
+
+          expect(assigns(:user).new_record?).to eq(true)
+        end
+      end
+
+      context "when user is not an admin" do
+        let(:current_user) { create(:user, admin: false) }
+
+        before { sign_in(current_user) }
+
+        before { get new_user_path }
+
+        it "is expected to redirect to root path with status found" do
+          expect(response).to redirect_to(root_path)
+
+          expect(response).to have_http_status(:found)
+
+          expect(request.flash[:alert]).to eq("You are not authorized to perform this action.")
+        end
+      end
+    end
+
+    context "when user is not logged in" do
+      before { get new_user_path }
+
+      it "is expected to redirect to new user session path with status found" do
+        expect(response).to redirect_to(new_user_session_path)
+
+        expect(response).to have_http_status(:found)
+      end
+    end
+  end
+
   # describe "#edit" do
   #   context "when user is logged in" do
   #     context "when user is an admin" do
