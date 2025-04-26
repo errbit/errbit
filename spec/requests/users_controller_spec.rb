6cba4fb63d6fce28d38fd91d36e9a06df1187bf3
Igor Zubkov <igor.zubkov@gmail.com>
2025-04-26 20:25:32 +0200

    More tests

--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -505,8 +505,8 @@ RSpec.describe UsersController, type: :request do
   #     end
   #   end
   # end
-  #
-  # describe "#destroy" do
+
+  describe "#destroy" do
   #   context "when user is logged in" do
   #     context "when user is an admin" do
   #       context "when admin removes himself" do
@@ -628,17 +628,17 @@ RSpec.describe UsersController, type: :request do
   #       end
   #     end
   #   end
-  #
-  #   context "when user is not logged in" do
-  #     let!(:user) { create(:user) }
-  #
-  #     before { delete user_path(user) }
-  #
-  #     it "is expected to redirect to new user session path with status found" do
-  #       expect(response).to redirect_to(new_user_session_path)
-  #
-  #       expect(response).to have_http_status(:found)
-  #     end
-  #   end
-  # end
+
+    context "when user is not logged in" do
+      let(:user) { create(:user) }
+
+      before { delete user_path(user) }
+
+      it "is expected to redirect to new user session path with status found" do
+        expect(response).to redirect_to(new_user_session_path)
+
+        expect(response).to have_http_status(:found)
+      end
+    end
+  end
 end
