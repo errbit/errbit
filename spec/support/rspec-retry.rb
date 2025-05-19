6a9fdea3b61a26cded7d1e63c299f60f37d9280e
Igor Zubkov <igor.zubkov@gmail.com>
2025-05-19 18:01:06 +0200

    Update

--- spec/support/rspec-retry.rb
+++ spec/support/rspec-retry.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+# https://github.com/NoRedInk/rspec-retry/pull/123
+require "rspec/retry"
+
 RSpec.configure do |config|
   # show retry status in spec process
   config.verbose_retry = true
