9518a6ff8642a9eb3201db466be7f49642fb4fab
Igor Zubkov <igor.zubkov@gmail.com>
2025-05-26 12:52:01 +0200

    More tests

--- app/controllers/unlink_googles_controller.rb
+++ app/controllers/unlink_googles_controller.rb
@@ -2,12 +2,12 @@
 
 class UnlinkGooglesController < ApplicationController
   def update
-    user = User.find(params[:user_id])
+    @user = User.find(params[:user_id])
 
-    authorize user
+    authorize @user
 
-    user.update!(google_uid: nil)
+    @user.update!(google_uid: nil)
 
-    redirect_to user_path(user)
+    redirect_to user_path(@user)
   end
 end
--- spec/requests/unlink_googles_controller_spec.rb
+++ spec/requests/unlink_googles_controller_spec.rb
@@ -6,11 +6,37 @@ RSpec.describe UnlinkGooglesController, type: :request do
   describe "#update" do
     context "when user is logged in" do
       context "when user has access" do
+        let!(:current_user) { create(:user, google_uid: "google-uid") }
 
+        before { sign_in(current_user) }
+
+        before { patch user_unlink_google_path(current_user) }
+
+        it "is expected to unlink github for a user with status found" do
+          expect(response).to redirect_to(user_path(assigns(:user)))
+
+          expect(response).to have_http_status(:found)
+
+          expect(assigns(:user).google_uid).to eq(nil)
+        end
       end
 
       context "when user has not access" do
+        let!(:current_user) { create(:user, admin: false) }
 
+        let!(:user) { create(:user) }
+
+        before { sign_in(current_user) }
+
+        before { patch user_unlink_google_path(user) }
+
+        it "is expected to redirect to root path with status found" do
+          expect(response).to redirect_to(root_path)
+
+          expect(response).to have_http_status(:found)
+
+          expect(request.flash[:alert]).to eq("You are not authorized to perform this action.")
+        end
       end
     end
 
