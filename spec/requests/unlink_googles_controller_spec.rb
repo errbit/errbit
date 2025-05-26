c14527ac560b02dc59cbd5c5c188a018d510931e
Igor Zubkov <igor.zubkov@gmail.com>
2025-05-26 11:10:31 +0200

    Add tests

--- spec/requests/unlink_googles_controller_spec.rb
+++ spec/requests/unlink_googles_controller_spec.rb
@@ -3,4 +3,27 @@
 require "rails_helper"
 
 RSpec.describe UnlinkGooglesController, type: :request do
+  describe "#update" do
+    context "when user is logged in" do
+      context "when user has access" do
+
+      end
+
+      context "when user has not access" do
+
+      end
+    end
+
+    context "when user is not logged in" do
+      let(:user) { create(:user) }
+
+      before { patch user_unlink_google_path(user) }
+
+      it "is expected to redirect to new user session path with status found" do
+        expect(response).to redirect_to(new_user_session_path)
+
+        expect(response).to have_http_status(:found)
+      end
+    end
+  end
 end
