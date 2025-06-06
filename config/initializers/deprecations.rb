cf34cbd4026fc9bcd1b045ead0d6dad977d02468
Espen Antonsen <espen@inspired.no>
2025-06-06 12:37:26 -0400

    deprecation deprecated. Fixes #2042

--- config/initializers/deprecations.rb
+++ config/initializers/deprecations.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
+deprecator = ActiveSupport::Deprecation.new(nil, "Errbit")
+
 if ENV["USER_GEMFILE"].present?
   # Make it error in v0.11.0 release and remove in v0.12.0
-  ActiveSupport::Deprecation.warn(
+  deprecator.warn(
     "ENV['USER_GEMFILE'] support is removed and has no effect in Errbit v0.10.0. " \
     "Remove it from configuration. " \
     "From Errbit v0.10.0+ it's always 'UserGemfile'."
@@ -11,7 +13,7 @@ end
 
 if ENV["SERVE_STATIC_ASSETS"].present?
   # Make it error in v0.11.0 release and remove in v0.12.0
-  ActiveSupport::Deprecation.warn(
+  deprecator.warn(
     "ENV['SERVE_STATIC_ASSETS'] support is removed and has no effect in Errbit v0.10.0. " \
     "Remove it from configuration."
   )
@@ -19,7 +21,7 @@ end
 
 if ENV["RACK_ENV"].present?
   # Make it error in v0.11.0 release and remove in v0.12.0
-  ActiveSupport::Deprecation.warn(
+  deprecator.warn(
     "ENV['RACK_ENV'] support is removed and has no effect in Errbit v0.10.0. " \
     "Replace it with build-in Ruby on Rails ENV['RAILS_ENV']."
   )
@@ -27,7 +29,7 @@ end
 
 if ENV["ERRBIT_ENFORCE_SSL"].present?
   # Make it error in v0.11.0 release and remove in v0.12.0
-  ActiveSupport::Deprecation.warn(
+  deprecator.warn(
     "ENV['ERRBIT_ENFORCE_SSL'] support is removed and has no effect in Errbit v0.10.0. " \
     "You should run Errbit behind reverse proxy with HTTPS support. " \
     "e.g. Traefik."
@@ -36,7 +38,7 @@ end
 
 if ENV["ERRBIT_PROTOCOL"].present?
   # Make it error in v0.11.0 release and remove in v0.12.0
-  ActiveSupport::Deprecation.warn(
+  deprecator.warn(
     "ENV['ERRBIT_PROTOCOL'] support is removed and has no effect in Errbit v0.10.0. " \
     "When you are running Errbit behind reverse proxy with HTTPS support, protocol is already set to https " \
     "and can't be changed."
@@ -45,7 +47,7 @@ end
 
 if ENV["ERRBIT_PORT"].present?
   # Make it error in v0.11.0 release and remove in v0.12.0
-  ActiveSupport::Deprecation.warn(
+  deprecator.warn(
     "ENV['ERRBIT_PORT'] support is removed and has no effect in Errbit v0.10.0. " \
     "When you are running Errbit behind reverse proxy with HTTPS support, port is already set to 443 " \
     "and can't be changed."
@@ -54,7 +56,7 @@ end
 
 if ENV["ERRBIT_LOG_LEVEL"].present?
   # Make it error in v0.11.0 release and remove in v0.12.0
-  ActiveSupport::Deprecation.warn(
+  deprecator.warn(
     "ENV['ERRBIT_LOG_LEVEL'] support is removed and has no effect in Errbit v0.10.0. " \
     "Replace it with build-in Ruby on Rails ENV['RAILS_LOG_LEVEL']."
   )
