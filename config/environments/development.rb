c7abb45948cda902fb3ec51def109370bc24440f
Igor Zubkov <igor.zubkov@gmail.com>
2025-07-11 15:21:58 +0200

    Rails 8.0 deprecations (#2096)
    * Sync with Rails 8.0
    
    * Sync with Rails 8.0
    
    * Sync with Rails 8.0
    
    * Sync with Rails 8.0
    
    * Sync with Rails 8.0
    
    * Sync with Rails 8.0
    
    * Sync with Rails 8.0
    
    * Sync with Rails 8.0
    
    * Sync with Rails 8.0
    
    * Sync with Rails 8.0
    
    * Sync with Rails 8.0
    
    * Sync with Rails 8.0
    
    * Sync with Rails 8.0
    
    * Sync with Rails 8.0
    
    * Sync with Rails 8.0
    
    * Sync with Rails 8.0
    
    * Sync with Rails 8.0
    
    * Sync with Rails 8.0
    
    * Sync with Rails 8.0

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,9 +5,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # In the development environment your application's code is reloaded any time
-  # it changes. This slows down response time but is perfect for development
-  # since you don't have to restart the web server when you make code changes.
+  # Make code changes take effect immediately without server restart.
   config.enable_reloading = true
 
   # Do not eager load code on boot.
@@ -36,26 +34,17 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
-  # Disable caching for Action Mailer templates even if Action Controller
-  # caching is enabled.
+  # Make template changes take effect immediately.
   config.action_mailer.perform_caching = false
 
+  # Set localhost to be used by links generated in mailer templates.
   config.action_mailer.default_url_options = {host: "localhost", port: 3000}
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
-  # Raise exceptions for disallowed deprecations.
-  config.active_support.disallowed_deprecation = :raise
-
-  # Tell Active Support which deprecation messages to disallow.
-  config.active_support.disallowed_deprecation_warnings = []
-
   # Highlight code that enqueued background job in logs.
   config.active_job.verbose_enqueue_logs = true
-
-  # Suppress logger output for asset requests.
-  config.assets.quiet = true
 
   # Raises error for missing translations.
   config.i18n.raise_on_missing_translations = true
