8dac771c6545aa74ffa698120cdc46de20914f14
Ramon <rith.torres@gmail.com>
2025-08-31 23:11:58 -0600

    Migrate users view from html.haml to html.erb. See #1853

---
+++ spec/views/users/edit.html.erb_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "users/edit.html.erb", type: :view do
+  let(:user) { stub_model(User, name: "shingara") }
+
+  before do
+    allow(view).to receive(:current_user).and_return(user)
+
+    assign(:user, user)
+  end
+
+  it "should have per_page option" do
+    render
+
+    expect(rendered).to match(/id="user_per_page"/)
+  end
+
+  it "should have time_zone option" do
+    render
+
+    expect(rendered).to match(/id="user_time_zone"/)
+  end
+end
