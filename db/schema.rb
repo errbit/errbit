3d6c4550d4e3ad23bae39289c39f6f9a52198775
Igor Zubkov <igor.zubkov@gmail.com>
2025-09-11 11:27:11 +0200

    Update

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,11 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_08_191818) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_errbit_users_on_email", unique: true
