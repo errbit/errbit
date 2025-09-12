7df5bd476d5ceaeb532f5aa84990bf34546f3c5b
Igor Zubkov <igor.zubkov@gmail.com>
2025-09-12 16:26:44 +0200

    Update

--- app/policies/errbit/user_policy.rb
+++ app/policies/errbit/user_policy.rb
@@ -16,5 +16,55 @@ module Errbit
     def index?
       false
     end
+
+    # def show?
+    #   scope.where(id: record.id).exists?
+    # end
+
+    def create?
+      user.admin?
+    end
+
+    # def new?
+    #   create?
+    # end
+    #
+    # def update?
+    #   scope.where(id: record.id).exists?
+    # end
+    #
+    # def edit?
+    #   scope.where(id: record.id).exists?
+    # end
+    #
+    # def destroy?
+    #   scope.where(id: record.id).exists? && user.id != record.id
+    # end
+    #
+    # def permitted_attributes
+    #   if user.admin? && user.id != record.id
+    #     FIELDS + [:admin]
+    #   else
+    #     FIELDS
+    #   end
+    # end
+
+    class Scope < ApplicationPolicy::Scope
+      attr_reader :user, :scope
+
+      # @param user [Errbit::User]
+      # @param scope [Errbit::User, Object] The scope of records being accessed
+      def initialize(user, scope)
+        super
+      end
+
+      # def resolve
+      #   if user.admin?
+      #     scope.all
+      #   else
+      #     scope.where(id: user.id)
+      #   end
+      # end
+    end
   end
 end
