aeb392a0e87e8a61d2c9dde1953f885ebb9f2f31
Igor Zubkov <igor.zubkov@gmail.com>
2025-09-12 14:57:21 +0200

    Update

--- spec/models/errbit/user_spec.rb
+++ spec/models/errbit/user_spec.rb
@@ -4,4 +4,8 @@ require "rails_helper"
 
 RSpec.describe Errbit::User, type: :model do
   it { expect(subject).to be_an(Errbit::ApplicationRecord) }
+
+  it { is_expected.to validate_presence_of(:name) }
+
+  it { is_expected.to validate_uniqueness_of(:github_login).allow_nil }
 end
