a544fadd3f8946f258184a8e4fdf34b1c2e2b230
Igor Zubkov <igor.zubkov@gmail.com>
2025-09-12 16:41:49 +0200

    Update

--- spec/policies/errbit/user_policy_spec.rb
+++ spec/policies/errbit/user_policy_spec.rb
@@ -124,32 +124,32 @@ RSpec.describe Errbit::UserPolicy do
     end
   end
 
-  # describe "#edit?" do
-  #   context "when user is an admin" do
-  #     let(:user) { create(:user, admin: true) }
-  #
-  #     let(:record) { create(:user, admin: false) }
-  #
-  #     it { is_expected.to permit_action(:edit) }
-  #   end
-  #
-  #   context "when user is not an admin" do
-  #     let(:user) { create(:user, admin: false) }
-  #
-  #     let(:record) { create(:user, admin: false) }
-  #
-  #     it { is_expected.to forbid_action(:edit) }
-  #   end
-  #
-  #   context "when user is an owner" do
-  #     let(:user) { create(:user, admin: false) }
-  #
-  #     let(:record) { user }
-  #
-  #     it { is_expected.to permit_action(:edit) }
-  #   end
-  # end
-  #
+  describe "#edit?" do
+    context "when user is an admin" do
+      let(:user) { create(:errbit_user, admin: true) }
+
+      let(:record) { create(:errbit_user, admin: false) }
+
+      it { is_expected.to permit_action(:edit) }
+    end
+
+    context "when user is not an admin" do
+      let(:user) { create(:errbit_user, admin: false) }
+
+      let(:record) { create(:errbit_user, admin: false) }
+
+      it { is_expected.to forbid_action(:edit) }
+    end
+
+    context "when user is an owner" do
+      let(:user) { create(:errbit_user, admin: false) }
+
+      let(:record) { user }
+
+      it { is_expected.to permit_action(:edit) }
+    end
+  end
+
   # describe "#destroy?" do
   #   context "when user is an admin" do
   #     let(:user) { create(:user, admin: true) }
