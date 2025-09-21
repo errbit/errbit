6aa014f28156bb32cfe6c97274fe2c0fddc9ec9d
ramonetorres <76922967+ramonetorres@users.noreply.github.com>
2025-09-21 06:04:51 -0600

    Migrate site_config and shared views from HAML to ERB (#2225)
    * Migrate site configuration view from .haml to .erb. See #1853
    
    * Remove HAML view for site configuration index. See #1853
    
    * Migrate shared views from HAML to ERB. See #1853
    
    * Remove shared HAML views. See #1853
    
    * Return to use `unless` for clarity
    
    * Add TODO comment to change form method from :put to :patch

---
+++ spec/views/site_config/index.html.erb_spec.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "site_config/index.html.erb", type: :view do
+  let(:config) { SiteConfig.document }
+
+  before do
+    assign(:config, config)
+
+    render
+  end
+
+  it "renders a form to edit site configuration" do
+    expect(rendered).to have_selector("form", count: 1)
+    expect(rendered).to have_selector("input[type='submit']")
+  end
+
+  it "has fields for notice fingerprinter attributes" do
+    fingerprinter_fields = %w[
+      error_class
+      message
+      backtrace_lines
+      component
+      action
+      environment_name
+    ]
+
+    fingerprinter_fields.each do |field|
+      expect(rendered).to have_field("site_config_notice_fingerprinter_attributes_#{field}")
+    end
+  end
+end
