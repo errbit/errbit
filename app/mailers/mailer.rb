7a4abe49de59c37f3acf3359a7dc5fc48508010b
Igor Zubkov <igor.zubkov@gmail.com>
2025-09-29 17:26:05 +0200

    Update

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -15,8 +15,10 @@ class Mailer < ActionMailer::Base
     "Auto-Submitted" => "auto-generated"
 
   def err_notification(error_report)
-    @notice = NoticeDecorator.new(error_report.notice)
-    @app = AppDecorator.new(error_report.app)
+    # @notice = NoticeDecorator.new(error_report.notice)
+    @notice = error_report.notice
+    # @app = AppDecorator.new(error_report.app)
+    @app = error_report.app
 
     count = error_report.problem.notices_count
     count = (count > 1) ? "(#{count}) " : ""
@@ -32,8 +34,10 @@ class Mailer < ActionMailer::Base
   def comment_notification(comment)
     @comment = comment
     @user = comment.user
-    @problem = ProblemDecorator.new comment.err
-    @notice = NoticeDecorator.new comment.err.notices.first
+    # @problem = ProblemDecorator.new comment.err
+    @problem = comment.err
+    # @notice = NoticeDecorator.new comment.err.notices.first
+    @notice = comment.err.notices.first
     @app = @problem.app
 
     recipients = @comment.notification_recipients
