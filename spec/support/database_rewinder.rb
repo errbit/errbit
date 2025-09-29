81aea28b830c0bf9243914eea8fc1cf50dcba8b8
Igor Zubkov <igor.zubkov@gmail.com>
2025-09-29 16:39:40 +0200

    Update

--- spec/support/database_rewinder.rb
+++ spec/support/database_rewinder.rb
@@ -1,13 +1,11 @@
 # frozen_string_literal: true
 
-if ENV["ERRBIT_SQL_PORT"].present?
-  RSpec.configure do |config|
-    config.before(:suite) do
-      DatabaseRewinder.clean_all
-    end
+RSpec.configure do |config|
+  config.before(:suite) do
+    DatabaseRewinder.clean_all
+  end
 
-    config.after(:each) do
-      DatabaseRewinder.clean
-    end
+  config.after(:each) do
+    DatabaseRewinder.clean
   end
 end
--- spec/support/shoulda-matchers.rb
+++ spec/support/shoulda-matchers.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
-if ENV["ERRBIT_SQL_PORT"].present?
-  Shoulda::Matchers.configure do |config|
-    config.integrate do |with|
-      with.test_framework :rspec
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
 
-      with.library :rails
-    end
+    with.library :rails
   end
 end
