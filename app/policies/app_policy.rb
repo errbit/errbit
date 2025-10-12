96ebad874b59caf0b171b6fb810c0597e5352c70
Igor Zubkov <igor.zubkov@gmail.com>
2025-10-12 15:51:04 +0200

    Add stubs

--- app/policies/app_policy.rb
+++ app/policies/app_policy.rb
@@ -10,4 +10,22 @@ class AppPolicy < ApplicationPolicy
   def index?
     false
   end
+
+  def show?
+  end
+
+  def create?
+  end
+
+  def new?
+  end
+
+  def update?
+  end
+
+  def edit?
+  end
+
+  def destroy?
+  end
 end
