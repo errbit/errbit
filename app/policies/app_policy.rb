13f01b480ca5d4679b16df93b3e5a6f3e8bb1bfc
Igor Zubkov <igor.zubkov@gmail.com>
2025-10-12 15:48:30 +0200

    Update app policy

--- app/policies/app_policy.rb
+++ app/policies/app_policy.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class AppPolicy < ApplicationPolicy
+  # @param user [User] The user making the request
+  # @param record [App] The record being accessed
+  def initialize(user, record)
+    super
+  end
 end
--- spec/policies/app_policy_spec.rb
+++ spec/policies/app_policy_spec.rb
@@ -3,4 +3,15 @@
 require "rails_helper"
 
 RSpec.describe AppPolicy do
+  subject { described_class.new(user, record) }
+
+  describe "#initialize" do
+    context "when user not present" do
+      let(:record) { create(:app) }
+
+      let(:user) { nil }
+
+      it { expect { subject }.to raise_error(Pundit::NotAuthorizedError) }
+    end
+  end
 end
