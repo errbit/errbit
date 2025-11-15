a97144f6d778cffb4d291cf4e9b9ba11aa567627
Igor Zubkov <igor.zubkov@gmail.com>
2025-11-15 23:27:53 +0100

    Update

--- app/models/errbit/watcher.rb
+++ app/models/errbit/watcher.rb
@@ -2,8 +2,8 @@
 
 module Errbit
   class Watcher < ApplicationRecord
-    belongs_to :user, class_name: "Errbit::User", optional: true
+    belongs_to :user, class_name: "Errbit::User", foreign_key: :errbit_user_id, optional: true
 
-    belongs_to :app, class_name: "Errbit::App"
+    belongs_to :app, class_name: "Errbit::App", foreign_key: :errbit_app_id
   end
 end
--- spec/models/errbit/watcher_spec.rb
+++ spec/models/errbit/watcher_spec.rb
@@ -4,4 +4,8 @@ require "rails_helper"
 
 RSpec.describe Errbit::Watcher, type: :model do
   it { expect(subject).to be_an(Errbit::ApplicationRecord) }
+
+  it { expect(subject).to belong_to(:user).class_name("Errbit::User").optional }
+
+  it { expect(subject).to belong_to(:app).class_name("Errbit::App") }
 end
