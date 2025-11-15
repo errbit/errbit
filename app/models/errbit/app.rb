f6bbd6261ce81108294b4078ef3f2284166f9843
Igor Zubkov <igor.zubkov@gmail.com>
2025-11-15 23:39:59 +0100

    Update

--- app/models/errbit/app.rb
+++ app/models/errbit/app.rb
@@ -2,6 +2,6 @@
 
 module Errbit
   class App < ApplicationRecord
-    has_many :watchers, class_name: "Errbit::Watcher", dependent: :destroy
+    has_many :watchers, class_name: "Errbit::Watcher", foreign_key: :errbit_app_id, dependent: :destroy
   end
 end
