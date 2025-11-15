f557544a6b1c6c13178bc00bd34cf8b92360b301
Igor Zubkov <igor.zubkov@gmail.com>
2025-11-15 23:39:59 +0100

    Update

--- spec/models/errbit/app_spec.rb
+++ spec/models/errbit/app_spec.rb
@@ -5,5 +5,5 @@ require "rails_helper"
 RSpec.describe Errbit::App, type: :model do
   it { expect(subject).to be_an(Errbit::ApplicationRecord) }
 
-  it { is_expected.to have_many(:watchers).dependent(:destroy) }
+  it { is_expected.to have_many(:watchers).with_foreign_key(:errbit_app_id).dependent(:destroy) }
 end
--- spec/models/errbit/watcher_spec.rb
+++ spec/models/errbit/watcher_spec.rb
@@ -5,7 +5,7 @@ require "rails_helper"
 RSpec.describe Errbit::Watcher, type: :model do
   it { expect(subject).to be_an(Errbit::ApplicationRecord) }
 
-  it { expect(subject).to belong_to(:user).class_name("Errbit::User").optional }
+  it { expect(subject).to belong_to(:user).class_name("Errbit::User").with_foreign_key(:errbit_user_id).optional }
 
-  it { expect(subject).to belong_to(:app).class_name("Errbit::App") }
+  it { expect(subject).to belong_to(:app).class_name("Errbit::App").with_foreign_key(:errbit_app_id) }
 end
