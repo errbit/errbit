71c147e0fdddfe8e6c0db41a47470871dd119e07
Igor Zubkov <igor.zubkov@gmail.com>
2025-11-17 16:04:18 +0100

    Remove WatcherDecorator

--- app/decorators/watcher_decorator.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class WatcherDecorator < Draper::Decorator
-  delegate_all
-end
--- spec/decorators/watcher_decorator_spec.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-require "rails_helper"
-
-RSpec.describe WatcherDecorator, type: :decorator do
-end
