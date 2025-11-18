c26ac3a00ddce7e5c9e54fd69268dc0107be7458
Igor Zubkov <igor.zubkov@gmail.com>
2025-11-18 18:17:10 +0100

    Update

--- app/models/errbit/user.rb
+++ app/models/errbit/user.rb
@@ -14,6 +14,8 @@ module Errbit
       validates :username, presence: true
     end
 
+    before_save :ensure_authentication_token
+
     def self.valid_google_domain?(email)
       return true if Errbit::Config.google_authorized_domains.nil?
 
@@ -43,49 +45,49 @@ module Errbit
       super || PER_PAGE
     end
 
-    # def watching?(app)
-    #   # apps.all.include?(app)
-    # end
+    def watching?(app)
+      apps.all.include?(app)
+    end
 
-    # def password_required?
-    #   github_login.present? ? false : super
-    # end
+    def password_required?
+      github_login.present? ? false : super
+    end
 
     def github_account?
       github_login.present? && github_oauth_token.present?
     end
 
-    # def can_create_github_issues?
-    #   # github_account? && Errbit::Config.github_access_scope.include?("repo")
-    # end
+    def can_create_github_issues?
+      github_account? && Errbit::Config.github_access_scope.include?("repo")
+    end
 
-    # def github_login=(login)
-    #   # login = nil if login.is_a?(String) && login.strip.empty?
-    #   # self[:github_login] = login
-    # end
+    def github_login=(login)
+      login = nil if login.is_a?(String) && login.strip.empty?
+      self[:github_login] = login
+    end
 
     def google_account?
       google_uid.present?
     end
 
-    # def ensure_authentication_token
-    #   # if authentication_token.blank?
-    #   #   self.authentication_token = generate_authentication_token
-    #   # end
-    # end
+    def ensure_authentication_token
+      if authentication_token.blank?
+        self.authentication_token = generate_authentication_token
+      end
+    end
 
     def self.token_authentication_key
       :auth_token
     end
 
-    # def reset_password(new_password, new_password_confirmation)
-    #   # self.password = new_password
-    #   # self.password_confirmation = new_password_confirmation
-    #   #
-    #   # self.class.validators_on(:password).map { |v| v.validate_each(self, :password, password) }
-    #   # return false if errors.any?
-    #   # save(validate: false)
-    # end
+    def reset_password(new_password, new_password_confirmation)
+      self.password = new_password
+      self.password_confirmation = new_password_confirmation
+
+      self.class.validators_on(:password).map { |v| v.validate_each(self, :password, password) }
+      return false if errors.any?
+      save(validate: false)
+    end
 
     def attributes_for_super_diff
       {
