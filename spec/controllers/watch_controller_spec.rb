3283bb7b56ea9821954d813241b3b2a05c914681
Igor Zubkov <igor.zubkov@gmail.com>
2025-11-20 19:54:20 +0100

    Update

---
+++ spec/controllers/watch_controller_spec.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe WatchController, type: :controller do
+  let(:user) { Fabricate(:user) }
+  let(:problem) { Fabricate(:problem) }
+
+  before { sign_in user }
+
+  describe "#create" do
+    let(:app) { Fabricate(:app) }
+
+    context "successful watcher update" do
+      before do
+        put :update, params: {app_id: app.id, id: user.id.to_s}
+        problem.reload
+      end
+
+      it "should be watching" do
+        app.reload
+        expect(app.watchers.first.user_id).to eq user.id
+      end
+
+      it "should redirect to app page" do
+        expect(response).to redirect_to(app_path(app))
+      end
+    end
+  end
+
+  describe "#destroy" do
+    let(:app) do
+      a = Fabricate(:app)
+      Fabricate(:user_watcher, app: a, user: user)
+      a
+    end
+
+    context "successful watcher deletion" do
+      let(:watcher) { app.watchers.first }
+
+      before do
+        delete :destroy, params: {app_id: app.id, id: watcher.user.id.to_s}
+        problem.reload
+      end
+
+      it "should delete the watcher" do
+        expect(app.watchers.detect { |w| w.id.to_s == watcher.id }).to be nil
+      end
+
+      it "should redirect to app page" do
+        expect(response).to redirect_to(app_path(app))
+      end
+    end
+  end
+end
