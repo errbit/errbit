8c932a5395f4eb4a75c1ab8193cb42661f5d0e6c
Igor Zubkov <igor.zubkov@gmail.com>
2025-11-24 12:06:08 +0100

    Cleanup

--- spec/controllers/problems_controller_spec.rb
+++ spec/controllers/problems_controller_spec.rb
@@ -21,6 +21,7 @@ RSpec.describe ProblemsController, type: :controller do
   describe "GET /problems" do
     before do
       sign_in user
+
       @problem = Fabricate(:notice, err: Fabricate(:err, problem: Fabricate(:problem, app: app, environment: "production"))).problem
     end
 
@@ -31,19 +32,23 @@ RSpec.describe ProblemsController, type: :controller do
 
       it "should have default per_page value for user" do
         get :index
-        expect(controller.problems.to_a.size).to eq User::PER_PAGE
+
+        expect(controller.problems.to_a.size).to eq(User::PER_PAGE)
       end
 
       it "should be able to override default per_page value" do
         user.update_attribute :per_page, 10
+
         get :index
-        expect(controller.problems.to_a.size).to eq 10
+
+        expect(controller.problems.to_a.size).to eq(10)
       end
     end
 
     context "with environment filters" do
       before do
         environments = ["production", "test", "development", "staging"]
+
         20.times do |i|
           Fabricate(:problem, environment: environments[i % environments.length])
         end
@@ -52,35 +57,38 @@ RSpec.describe ProblemsController, type: :controller do
       context "no params" do
         it "shows problems for all environments" do
           get :index
-          expect(controller.problems.size).to eq 21
+
+          expect(controller.problems.size).to eq(21)
         end
       end
 
       context "environment production" do
         it "shows problems for just production" do
           get :index, params: {environment: "production"}
-          expect(controller.problems.size).to eq 6
+
+          expect(controller.problems.size).to eq(6)
         end
       end
 
       context "environment staging" do
         it "shows problems for just staging" do
           get :index, params: {environment: "staging"}
-          expect(controller.problems.size).to eq 5
+
+          expect(controller.problems.size).to eq(5)
         end
       end
 
       context "environment development" do
         it "shows problems for just development" do
           get :index, params: {environment: "development"}
-          expect(controller.problems.size).to eq 5
+          expect(controller.problems.size).to eq(5)
         end
       end
 
       context "environment test" do
         it "shows problems for just test" do
           get :index, params: {environment: "test"}
-          expect(controller.problems.size).to eq 5
+          expect(controller.problems.size).to eq(5)
         end
       end
     end
@@ -89,14 +97,20 @@ RSpec.describe ProblemsController, type: :controller do
   describe "GET /problems - previously all" do
     it "gets a paginated list of all problems" do
       sign_in Fabricate(:user)
+
       problems = Kaminari.paginate_array((1..30).to_a)
+
       3.times { problems << Fabricate(:err).problem }
+
       3.times { problems << Fabricate(:err, problem: Fabricate(:problem, resolved: true)).problem }
+
       expect(Problem).to receive(:ordered_by).and_return(
         double("proxy", page: double("other_proxy", per: problems))
       )
+
       get :index, params: {all_errs: true}
-      expect(controller.problems).to eq problems
+
+      expect(controller.problems).to eq(problems)
     end
   end
 
@@ -110,23 +124,29 @@ RSpec.describe ProblemsController, type: :controller do
 
     it "renders successfully" do
       get :search
+
       expect(response).to be_successful
     end
 
     it "renders index template" do
       get :search
+
       expect(response).to render_template("problems/index")
     end
 
     it "searches problems for given string" do
       get :search, params: {search: "\"Most important\""}
+
       expect(controller.problems).to include(@problem1)
+
       expect(controller.problems).not_to include(@problem2)
     end
 
     it "works when given string is empty" do
       get :search, params: {search: ""}
+
       expect(controller.problems).to include(@problem1)
+
       expect(controller.problems).to include(@problem2)
     end
   end
@@ -142,6 +162,7 @@ RSpec.describe ProblemsController, type: :controller do
 
     it "should redirect to the standard problems page" do
       get :show_by_id, params: {id: err.problem.id}
+
       expect(response).to redirect_to(app_problem_path(app, err.problem.id))
     end
   end
@@ -153,16 +174,19 @@ RSpec.describe ProblemsController, type: :controller do
 
     it "finds the app" do
       get :show, params: {app_id: app.id, id: err.problem.id}
-      expect(controller.app).to eq app
+
+      expect(controller.app).to eq(app)
     end
 
     it "finds the problem" do
       get :show, params: {app_id: app.id, id: err.problem.id}
-      expect(controller.problem).to eq err.problem
+
+      expect(controller.problem).to eq(err.problem)
     end
 
     it "successfully render page" do
       get :show, params: {app_id: app.id, id: err.problem.id}
+
       expect(response).to be_successful
     end
 
@@ -171,7 +195,9 @@ RSpec.describe ProblemsController, type: :controller do
 
       it "successfully renders the view even when there are no notices attached to the problem" do
         expect(err.problem.notices).to be_empty
+
         get :show, params: {app_id: app.id, id: err.problem.id}
+
         expect(response).to be_successful
       end
     end
@@ -185,13 +211,17 @@ RSpec.describe ProblemsController, type: :controller do
 
       it "paginates the notices 1 at a time, starting with the most recent" do
         get :show, params: {app_id: app.id, id: err.problem.id}
-        expect(assigns(:notices).entries.count).to eq 1
+
+        expect(assigns(:notices).entries.count).to eq(1)
+
         expect(assigns(:notices)).to include(notices.last)
       end
 
       it "paginates the notices 1 at a time, based on then notice param" do
         get :show, params: {app_id: app.id, id: err.problem.id, notice: 3}
-        expect(assigns(:notices).entries.count).to eq 1
+
+        expect(assigns(:notices).entries.count).to eq(1)
+
         expect(assigns(:notices)).to include(notices.first)
       end
     end
@@ -204,6 +234,7 @@ RSpec.describe ProblemsController, type: :controller do
 
     it "renders without error" do
       get :xhr_sparkline, params: {app_id: app.id, id: err.problem.id}
+
       expect(response).to be_successful
     end
   end
@@ -217,29 +248,36 @@ RSpec.describe ProblemsController, type: :controller do
 
     it "finds the app and the problem" do
       put :resolve, params: {app_id: @err.app.id, id: @err.problem.id}
+
       expect(controller.app).to eq @err.app
-      expect(controller.problem).to eq @err.problem
+      expect(controller.problem).to eq(@err.problem)
     end
 
     it "should resolve the issue" do
       put :resolve, params: {app_id: @err.app.id, id: @err.problem.id}
-      expect(@err.problem.reload.resolved).to be(true)
+
+      expect(@err.problem.reload.resolved).to eq(true)
     end
 
     it "should display a message" do
       put :resolve, params: {app_id: @err.app.id, id: @err.problem.id}
+
       expect(request.flash[:success]).to match(/Great news/)
     end
 
     it "should redirect to the app page" do
       request.env["HTTP_REFERER"] = app_path(@err.app)
+
       put :resolve, params: {app_id: @err.app.id, id: @err.problem.id}
+
       expect(response).to redirect_to(app_path(@err.app))
     end
 
     it "should redirect back to problems page" do
       request.env["HTTP_REFERER"] = problems_path
+
       put :resolve, params: {app_id: @err.app.id, id: @err.problem.id}
+
       expect(response).to redirect_to(problems_path)
     end
   end
@@ -264,7 +302,9 @@ RSpec.describe ProblemsController, type: :controller do
 
       it "should redirect to problem page" do
         post :create_issue, params: {app_id: problem.app.id, id: problem.id}
+
         expect(response).to redirect_to(app_problem_path(problem.app, problem))
+
         expect(flash[:error]).to be_blank
       end
 
@@ -283,6 +323,7 @@ RSpec.describe ProblemsController, type: :controller do
 
       it "should update the problem" do
         post :create_issue, params: {app_id: problem.app.id, id: problem.id}
+
         expect(problem.issue_link).to eq("http://example.com/mock-errbit")
         expect(problem.issue_type).to eq("mock")
       end
@@ -292,7 +333,9 @@ RSpec.describe ProblemsController, type: :controller do
 
         it "should save the right body" do
           post :create_issue, params: {app_id: problem.app.id, id: problem.id, format: "html"}
+
           line = issue_tracker.tracker.output.shift
+
           expect(line[1]).to include(app_problem_url(problem.app, problem))
         end
 
@@ -310,8 +353,10 @@ RSpec.describe ProblemsController, type: :controller do
     context "when app has no issue tracker" do
       it "should redirect to problem page" do
         post :create_issue, params: {app_id: problem.app.id, id: problem.id}
+
         expect(response).to redirect_to(app_problem_path(problem.app, problem))
-        expect(flash[:error]).to eql "This app has no issue tracker"
+
+        expect(flash[:error]).to eq("This app has no issue tracker")
       end
     end
   end
@@ -345,7 +390,7 @@ RSpec.describe ProblemsController, type: :controller do
       it "should redirect to problem page" do
         post :close_issue, params: {app_id: problem.app.id, id: problem.id}
         expect(response).to redirect_to(app_problem_path(problem.app, problem))
-        expect(flash[:error]).to eql "This app has no issue tracker"
+        expect(flash[:error]).to eq("This app has no issue tracker")
       end
     end
   end
@@ -368,7 +413,7 @@ RSpec.describe ProblemsController, type: :controller do
       end
 
       it "should clear issue link" do
-        expect(err.problem.issue_link).to be_nil
+        expect(err.problem.issue_link).to eq(nil)
       end
     end
 
@@ -396,7 +441,7 @@ RSpec.describe ProblemsController, type: :controller do
     context "POST /problems/merge_several" do
       it "should require at least two problems" do
         post :merge_several, params: {problems: [@problem1.id.to_s]}
-        expect(request.flash[:notice]).to eql I18n.t("controllers.problems.flash.need_two_errors_merge")
+        expect(request.flash[:notice]).to eq(I18n.t("controllers.problems.flash.need_two_errors_merge"))
       end
 
       it "should merge the problems" do
@@ -408,7 +453,7 @@ RSpec.describe ProblemsController, type: :controller do
     context "POST /problems/unmerge_several" do
       it "should require at least one problem" do
         post :unmerge_several, params: {problems: []}
-        expect(request.flash[:notice]).to eql I18n.t("controllers.problems.flash.no_select_problem")
+        expect(request.flash[:notice]).to eq(I18n.t("controllers.problems.flash.no_select_problem"))
       end
 
       it "should unmerge a merged problem" do
@@ -424,12 +469,12 @@ RSpec.describe ProblemsController, type: :controller do
     context "POST /problems/resolve_several" do
       it "should require at least one problem" do
         post :resolve_several, params: {problems: []}
-        expect(request.flash[:notice]).to eql I18n.t("controllers.problems.flash.no_select_problem")
+        expect(request.flash[:notice]).to eq(I18n.t("controllers.problems.flash.no_select_problem"))
       end
 
       it "should resolve the issue" do
         post :resolve_several, params: {problems: [@problem2.id.to_s]}
-        expect(@problem2.reload.resolved?).to eq true
+        expect(@problem2.reload.resolved?).to eq(true)
       end
 
       it "should display a message about 1 err" do
@@ -440,19 +485,19 @@ RSpec.describe ProblemsController, type: :controller do
       it "should display a message about 2 errs" do
         post :resolve_several, params: {problems: [@problem1.id.to_s, @problem2.id.to_s]}
         expect(flash[:success]).to match(/2 errors have been resolved/)
-        expect(controller.selected_problems).to eq [@problem1, @problem2]
+        expect(controller.selected_problems).to eq([@problem1, @problem2])
       end
     end
 
     context "POST /problems/unresolve_several" do
       it "should require at least one problem" do
         post :unresolve_several, params: {problems: []}
-        expect(request.flash[:notice]).to eql I18n.t("controllers.problems.flash.no_select_problem")
+        expect(request.flash[:notice]).to eq(I18n.t("controllers.problems.flash.no_select_problem"))
       end
 
       it "should unresolve the issue" do
         post :unresolve_several, params: {problems: [@problem1.id.to_s]}
-        expect(@problem1.reload.resolved?).to eq false
+        expect(@problem1.reload.resolved?).to eq(false)
       end
     end
 
@@ -476,7 +521,7 @@ RSpec.describe ProblemsController, type: :controller do
         expect do
           post :destroy_all, params: {app_id: @app.id}
         end.to change(Problem, :count).by(-2)
-        expect(controller.app).to eq @app
+        expect(controller.app).to eq(@app)
       end
 
       it "should display a message" do
