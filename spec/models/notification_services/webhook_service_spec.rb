e1c46c987f7232101e09a6603d044d5689e1caf3
Igor Zubkov <igor.zubkov@gmail.com>
2025-11-30 16:10:37 +0100

    Cleanup

--- spec/models/notification_services/webhook_service_spec.rb
+++ spec/models/notification_services/webhook_service_spec.rb
@@ -4,11 +4,12 @@ require "rails_helper"
 
 RSpec.describe NotificationServices::WebhookService, type: :model do
   it "should send a notification to a user-specified URL" do
-    notice = Fabricate :notice
-    notification_service = Fabricate :webhook_notification_service, app: notice.app
+    notice = Fabricate(:notice)
+    notification_service = Fabricate(:webhook_notification_service, app: notice.app)
     problem = notice.problem
 
     payload = notification_service.message_for_webhook(problem)
+
     expect(HTTParty).to receive(:post).with(notification_service.api_token, headers: {"Content-Type" => "application/json", "User-Agent" => "Errbit"}, body: payload.to_json).and_return(true)
 
     notification_service.create_notification(problem)
@@ -16,8 +17,8 @@ RSpec.describe NotificationServices::WebhookService, type: :model do
 
   describe "#message_for_webhook" do
     it "should return an hash with all the keys nested inside 'problem' key" do
-      notice = Fabricate :notice
-      notification_service = Fabricate :webhook_notification_service, app: notice.app
+      notice = Fabricate(:notice)
+      notification_service = Fabricate(:webhook_notification_service, app: notice.app)
       problem = notice.problem
 
       payload = notification_service.message_for_webhook(problem)
