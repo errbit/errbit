d732a29442f448217a5e76bb1de4aa05703da0c8
Igor Zubkov <igor.zubkov@gmail.com>
2025-12-01 14:42:50 +0100

    Update backtrace factory

--- spec/factories/backtrace_factory.rb
+++ spec/factories/backtrace_factory.rb
@@ -2,15 +2,14 @@
 
 FactoryBot.define do
   factory :backtrace do
+    lines do
+      99.times.map do
+        {
+          number: rand(999),
+          file: "/path/to/file/#{SecureRandom.hex(4)}.rb",
+          method: ActiveSupport.methods.sample
+        }
+      end
+    end
   end
 end
-
-# Fabricator :backtrace do
-#   lines(count: 99) do
-#     {
-#       number: rand(999),
-#       file: "/path/to/file/#{SecureRandom.hex(4)}.rb",
-#       method: ActiveSupport.methods.sample
-#     }
-#   end
-# end
