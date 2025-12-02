229ea3632b303a3baef30c03a81cc4428c703645
Igor Zubkov <igor.zubkov@gmail.com>
2025-12-02 14:11:46 +0100

    Comment

--- spec/models/fabricators_spec.rb
+++ spec/models/fabricators_spec.rb
@@ -2,18 +2,18 @@
 
 require "rails_helper"
 
-Fabrication::Config.fabricator_path.each do |folder|
-  Dir.glob(File.join(Rails.root, folder, "**", "*.rb")).each do |file|
-    require file
-  end
-end
+# Fabrication::Config.fabricator_path.each do |folder|
+#   Dir.glob(File.join(Rails.root, folder, "**", "*.rb")).each do |file|
+#     require file
+#   end
+# end
 
-RSpec.describe "Fabrication", type: :model do
-  Fabrication.manager.schematics.keys.sort.each do |fabricator_name|
-    context "Fabricate(:#{fabricator_name})" do
-      subject { Fabricate.build(fabricator_name) }
-
-      it { expect(subject.valid?).to eq(true) }
-    end
-  end
-end
+# RSpec.describe "Fabrication", type: :model do
+#   Fabrication.manager.schematics.keys.sort.each do |fabricator_name|
+#     context "Fabricate(:#{fabricator_name})" do
+#       subject { Fabricate.build(fabricator_name) }
+#
+#       it { expect(subject.valid?).to eq(true) }
+#     end
+#   end
+# end
