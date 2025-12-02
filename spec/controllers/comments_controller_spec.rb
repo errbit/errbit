72fb14c47d332609a8b5117fd1d1be34096444c5
Igor Zubkov <igor.zubkov@gmail.com>
2025-12-02 11:29:37 +0100

    Fabrication -> FactoryBot

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe CommentsController, type: :controller do
     end
 
     context "successful comment creation" do
-      let(:problem) { Fabricate(:problem) }
+      let(:problem) { create(:problem) }
       let(:user) { create(:user) }
 
       before do
@@ -47,7 +47,7 @@ RSpec.describe CommentsController, type: :controller do
     end
 
     context "successful comment deletion" do
-      let(:problem) { Fabricate(:problem_with_comments) }
+      let(:problem) { create(:problem_with_comments) }
       let(:comment) { problem.reload.comments.first }
 
       before do
