df7218e7bbecf126dd1fe2775b686a89c7093221
Igor Zubkov <igor.zubkov@gmail.com>
2025-12-14 14:06:20 +0100

    Mailer previews

--- spec/mailers/previews/devise/mailer/reset_password_instructions_preview.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class Devise::Mailer::ResetPasswordInstructionsPreview < ActionMailer::Preview
-  def reset_password_instructions
-
-  end
-end
---
+++ spec/mailers/previews/devise/mailer_preview.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Devise
+  class MailerPreview < ActionMailer::Preview
+    def reset_password_instructions
+
+    end
+  end
+end
