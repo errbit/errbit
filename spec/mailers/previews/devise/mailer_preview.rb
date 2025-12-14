f54a6683238ebf79cac056357c9cb1672125c99f
Igor Zubkov <igor.zubkov@gmail.com>
2025-12-14 14:24:20 +0100

    Add more previews

--- spec/mailers/previews/devise/mailer_preview.rb
+++ spec/mailers/previews/devise/mailer_preview.rb
@@ -2,8 +2,23 @@
 
 module Devise
   class MailerPreview < ActionMailer::Preview
-    def reset_password_instructions
+    def confirmation_instructions
+    end
 
+    def reset_password_instructions
+      user = FactoryBot.create(:user, reset_password_token: "token-123")
+
+      @resource = user
+      @token = user.reset_password_token
+    end
+
+    def unlock_instructions
+    end
+
+    def email_changed
+    end
+
+    def password_change
     end
   end
 end
