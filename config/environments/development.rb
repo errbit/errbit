f3db2558634dca73ced89d559ea50658fd8cbed5
Igor Zubkov <igor.zubkov@gmail.com>
2025-12-14 13:54:14 +0100

    Rails mailer previews

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,4 +60,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # rails mail preview
+  config.action_mailer.preview_paths << "#{Rails.root}/spec/mailers/previews"
 end
---
+++ spec/mailers/previews/devise/mailer/reset_password_instructions_preview.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class Devise::Mailer::ResetPasswordInstructionsPreview < ActionMailer::Preview
+  def reset_password_instructions
+
+  end
+end
