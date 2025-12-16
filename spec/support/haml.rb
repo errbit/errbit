5f254c662d3afced9c38afb7773cf71f7422f345
Igor Zubkov <igor.zubkov@gmail.com>
2025-12-16 18:25:08 +0100

    haml2erb (last one) (#2504)

--- spec/support/haml.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.configure do |config|
-  config.include Haml, type: :helper
-  config.include Haml::Helpers, type: :helper
-end
---
+++ spec/views/notices/_backtrace_line.html.erb_spec.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "notices/_backtrace_line.html.erb", type: :view do
+end
