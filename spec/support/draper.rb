e34b4973950e8a37a77af007e5805bde49aea8f5
Igor Zubkov <igor.zubkov@gmail.com>
2025-12-16 18:14:21 +0100

    Remove haml helpers

--- spec/support/draper.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.configure do |config|
-  config.before(:each, type: :decorator) do |_|
-    Draper::ViewContext.current.class_eval { include Haml::Helpers }
-  end
-end
