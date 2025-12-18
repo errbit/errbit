690b0743d833c068616d9013e06e3bee82e51ec1
Igor Zubkov <igor.zubkov@gmail.com>
2025-12-18 20:32:40 +0100

    Update

---
+++ app/javascript/controllers/forgot_password_controller.js
@@ -0,0 +1,20 @@
+// <script>
+//     $("a#forgot_password").click(function(){
+//     // Set email field on password reset page to email that user entered on this page
+//     location.href = $(this).attr("href") + "?email=" + encodeURIComponent($("#user_email").val());
+//
+//     return false;
+// });
+// </script>
+
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+    static targets = [ "auth_key" ]
+
+    connect() {
+        console.log("Bingo!")
+
+        console.log(this.auth_keyTarget.value)
+    }
+}
