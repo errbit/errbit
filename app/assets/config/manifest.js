893e1df32be401251f148f675ee803ea5175ad55
Igor Zubkov <igor.zubkov@gmail.com>
2025-12-20 22:15:17 +0100

    Add importmaps

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,6 @@
 //= link_tree ../images
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
-
 //= link mailers/mailer.css
+//= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
