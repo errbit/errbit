32b09c241e5bdb67c638673af79df11467fc0f3b
Igor Zubkov <igor.zubkov@gmail.com>
2025-12-21 00:41:20 +0100

    Use stimulus controller

--- app/assets/javascripts/apps.show.js
+++ app/assets/javascripts/apps.show.js
@@ -1,9 +1,5 @@
 $(function() {
 
-  $("#watchers_toggle").click(function() {
-    $("#watchers_div").slideToggle("slow");
-  });
-
   $("#repository_toggle").click(function() {
     $("#repository_div").slideToggle("slow");
   });
