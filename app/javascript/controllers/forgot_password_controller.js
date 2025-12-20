f853edfb66b5855f020a4debb9dcf5423ba83a66
Igor Zubkov <igor.zubkov@gmail.com>
2025-12-20 15:30:23 +0100

    Update

--- app/javascript/controllers/forgot_password_controller.js
+++ app/javascript/controllers/forgot_password_controller.js
@@ -10,12 +10,14 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-    static targets = [ "auth_key" ]
+    static targets = [ "auth_key", "href" ]
 
     connect() {
         console.log("Bingo!")
 
         console.log(this.auth_keyTarget.value)
+
+        console.log(this.hrefTarget.href)
     }
 
     click(event) {
@@ -23,13 +25,30 @@ export default class extends Controller {
 
         console.log("Bingo 222!")
 
-        // Turbo.visit()
+        // console.log(this)
+        // console.log(this.element)
+        // console.log(this.element.action)
+        //
+        // console.log(`${this.element.action}?email=${this.auth_keyTarget.value}`)
+
+        console.log(`${this.hrefTarget.href}?email=${this.auth_keyTarget.value}`)
+
+        console.log(1)
+        
+        Turbo.visit(`${this.hrefTarget.href}?email=${this.auth_keyTarget.value}`, { action: "advance" })
+
+        console.log(2)
+
+        // Turbo.visit(`${this.element.action}?email=${this.auth_keyTarget.value}`)
+
+
+        // Turbo.visit(`${this.element.action}?email=${this.auth_keyTarget}`)
 
         // Set email field on password reset page to email that user entered on this page
         // location.href = $(this).attr("href") + "?email=" + encodeURIComponent($("#user_email").val());
 
 
-        console.log(this.auth_keyTarget.value)
+        // console.log(this.auth_keyTarget.value)
 
 
     }
