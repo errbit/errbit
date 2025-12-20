7b84a03dcdd0b03926eb10a1b0cf7b80db6e6b5e
Igor Zubkov <igor.zubkov@gmail.com>
2025-12-20 14:00:54 +0100

    Sync with Rails 8.1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,6 +45,9 @@ Rails.application.configure do
   # Highlight code that enqueued background job in logs.
   config.active_job.verbose_enqueue_logs = true
 
+  # Highlight code that triggered redirect in logs.
+  config.action_dispatch.verbose_redirect_logs = true
+
   # Raises error for missing translations.
   config.i18n.raise_on_missing_translations = true
 
