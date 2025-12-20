3565a7c3d4b7aac3ab866d7d772a482009e385f7
Igor Zubkov <igor.zubkov@gmail.com>
2025-12-20 23:12:30 +0100

    Stimulus integration (#2536)

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,3 +3,6 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
+pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin_all_from "app/javascript/controllers", under: "controllers"
