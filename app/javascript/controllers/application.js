6e728a403311cd4c62ab486b18d7cd3d0321a0d4
Igor Zubkov <igor.zubkov@gmail.com>
2025-12-21 02:12:08 +0100

    Add stimulus controller

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,10 +1,12 @@
 import { Application } from "@hotwired/stimulus"
 
 import RevealController from '@stimulus-components/reveal'
+import CheckboxSelectAll from '@stimulus-components/checkbox-select-all'
 
 const application = Application.start()
 
 application.register("reveal", RevealController)
+application.register("checkbox-select-all", CheckboxSelectAll)
 
 // Configure Stimulus development experience
 application.debug = true
