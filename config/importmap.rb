dd17549b02c27349c06d1a842ca80d3ebb22a5da
Igor Zubkov <igor.zubkov@gmail.com>
2025-12-21 01:29:23 +0100

    Add stimulus controller

--- config/importmap.rb
+++ config/importmap.rb
@@ -13,3 +13,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
 pin "@stimulus-components/reveal", to: "@stimulus-components--reveal.js" # @5.0.0
+pin "@stimulus-components/checkbox-select-all", to: "@stimulus-components--checkbox-select-all.js" # @6.1.0
