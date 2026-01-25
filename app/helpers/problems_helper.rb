8e57dc697b3fce230dbe670d852088324061c479
Igor Zubkov <igor.zubkov@gmail.com>
2026-01-25 15:33:43 +0100

    Update magic comments

--- app/helpers/problems_helper.rb
+++ app/helpers/problems_helper.rb
@@ -13,20 +13,23 @@ module ProblemsHelper
     )
   end
 
-  # @param email [String, NilClass]
+  # @param email [String, NilClass] Email
+  # @param options [Hash] Options for `gravatar_url()`. Default: {}
   def gravatar_tag(email, options = {})
     return if email.blank?
 
     image_tag(gravatar_url(email, options), alt: email, class: "gravatar")
   end
 
+  # @param email [String, NilClass] Email
+  # @param options [Hash] Extra options. Default: {}
   def gravatar_url(email, options = {})
     return if email.blank?
 
     default_options = {
       d: Errbit::Config.gravatar_default
     }
-    options.reverse_merge! default_options
+    options.reverse_merge!(default_options)
     params = options.extract!(:s, :d).delete_if { |_, v| v.blank? }
     email_hash = Digest::MD5.hexdigest(email)
     url = "https://secure.gravatar.com"
