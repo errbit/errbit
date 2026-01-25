7bca1524b20d5a477c0d2779213869ae82f0630c
Igor Zubkov <igor.zubkov@gmail.com>
2026-01-25 16:36:54 +0100

    Fix rubocop warnings

--- spec/requests/health_controller_spec.rb
+++ spec/requests/health_controller_spec.rb
@@ -8,16 +8,18 @@ RSpec.describe HealthController, type: :request do
   describe "api_key_tester" do
     it "will let you know when the api_key is not valid" do
       get "/health/api-key-tester?api_key=garbagekey"
+
       expect(response).to be_forbidden
-      parsed_response = JSON.parse(response.body)
-      expect(parsed_response["ok"]).to eq false
+
+      expect(response.parsed_body["ok"]).to eq(false)
     end
 
     it "can let you know that the api_key is valid" do
       get "/health/api-key-tester?api_key=#{errbit_app.api_key}"
+
       expect(response).to be_successful
-      parsed_response = JSON.parse(response.body)
-      expect(parsed_response["ok"]).to eq true
+
+      expect(response.parsed_body["ok"]).to eq(true)
     end
   end
 end
