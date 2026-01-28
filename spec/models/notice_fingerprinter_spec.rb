e086e34f17e788ba8cf443d0370617300c75aed6
Igor Zubkov <igor.zubkov@gmail.com>
2026-01-28 20:28:55 +0100

    Cleanup

--- spec/models/notice_fingerprinter_spec.rb
+++ spec/models/notice_fingerprinter_spec.rb
@@ -9,68 +9,68 @@ RSpec.describe NoticeFingerprinter, type: :model do
 
   describe "#generate" do
     it "generates the same fingerprint for the same notice" do
-      f1 = fingerprinter.generate("123", notice, backtrace)
-      f2 = fingerprinter.generate("123", notice, backtrace)
-      expect(f1).to eq(f2)
+      f_1 = fingerprinter.generate("123", notice, backtrace)
+      f_2 = fingerprinter.generate("123", notice, backtrace)
+      expect(f_1).to eq(f_2)
     end
 
     ["error_class", "message", "component", "action", "environment_name"].each do |i|
       it "affects the fingerprint when #{i} is false" do
-        f1 = fingerprinter.generate("123", notice, backtrace)
-        f2 = fingerprinter.generate("123", notice, backtrace)
+        f_1 = fingerprinter.generate("123", notice, backtrace)
+        f_2 = fingerprinter.generate("123", notice, backtrace)
 
         fingerprinter.send(:"#{i}=", false)
-        f3 = fingerprinter.generate("123", notice, backtrace)
+        f_3 = fingerprinter.generate("123", notice, backtrace)
 
-        expect(f1).to eq(f2)
-        expect(f1).not_to eq(f3)
+        expect(f_1).to eq(f_2)
+        expect(f_1).not_to eq(f_3)
       end
     end
 
     it "affects the fingerprint with different backtrace_lines config" do
-      f1 = fingerprinter.generate("123", notice, backtrace)
-      f2 = fingerprinter.generate("123", notice, backtrace)
+      f_1 = fingerprinter.generate("123", notice, backtrace)
+      f_2 = fingerprinter.generate("123", notice, backtrace)
 
       fingerprinter.backtrace_lines = 2
-      f3 = fingerprinter.generate("123", notice, backtrace)
+      f_3 = fingerprinter.generate("123", notice, backtrace)
 
-      expect(f1).to eq(f2)
-      expect(f1).not_to eq(f3)
+      expect(f_1).to eq(f_2)
+      expect(f_1).not_to eq(f_3)
     end
 
     context "two backtraces have the same first two lines" do
-      let(:backtrace1) { create(:backtrace) }
-      let(:backtrace2) { create(:backtrace) }
+      let(:backtrace_1) { create(:backtrace) }
+      let(:backtrace_2) { create(:backtrace) }
 
       before do
-        backtrace1.lines[0] = backtrace2.lines[0]
-        backtrace1.lines[1] = backtrace2.lines[1]
-        backtrace1.lines[2] = {number: 1, file: "a", method: :b}
+        backtrace_1.lines[0] = backtrace_2.lines[0]
+        backtrace_1.lines[1] = backtrace_2.lines[1]
+        backtrace_1.lines[2] = {number: 1, file: "a", method: :b}
       end
 
       it "has the same fingerprint when only considering two lines" do
         fingerprinter.backtrace_lines = 2
-        f1 = fingerprinter.generate("123", notice, backtrace1)
-        f2 = fingerprinter.generate("123", notice, backtrace2)
+        f_1 = fingerprinter.generate("123", notice, backtrace_1)
+        f_2 = fingerprinter.generate("123", notice, backtrace_2)
 
-        expect(f1).to eq(f2)
+        expect(f_1).to eq(f_2)
       end
 
       it "has a different fingerprint when considering three lines" do
         fingerprinter.backtrace_lines = 3
-        f1 = fingerprinter.generate("123", notice, backtrace1)
-        f2 = fingerprinter.generate("123", notice, backtrace2)
+        f_1 = fingerprinter.generate("123", notice, backtrace_1)
+        f_2 = fingerprinter.generate("123", notice, backtrace_2)
 
-        expect(f1).not_to eq(f2)
+        expect(f_1).not_to eq(f_2)
       end
     end
 
     context "two notices with no backtrace" do
       it "has the same fingerprint" do
-        f1 = fingerprinter.generate("123", notice, nil)
-        f2 = fingerprinter.generate("123", notice, nil)
+        f_1 = fingerprinter.generate("123", notice, nil)
+        f_2 = fingerprinter.generate("123", notice, nil)
 
-        expect(f1).to eq(f2)
+        expect(f_1).to eq(f_2)
       end
     end
   end
