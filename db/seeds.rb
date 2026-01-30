f7e51d7464bdae28bee2c1924d31ffc808349c61
Igor Zubkov <igor.zubkov@gmail.com>
2026-01-30 16:33:24 +0100

    Update

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ admin_username = ENV["ERRBIT_ADMIN_USER"] || "errbit"
 def admin_email
   return "admin@example.com" if heroku_pr_review_app?
 
-  ENV["ERRBIT_ADMIN_EMAIL"] || "errbit@#{Config.main.host}"
+  ENV["ERRBIT_ADMIN_EMAIL"] || "errbit@#{Config.host}"
 end
 
 def admin_pass
