7081e009119738cda8fd99faff210b05bb73fc6f
Igor Zubkov <igor.zubkov@gmail.com>
2026-01-30 18:09:20 +0100

    Update

--- spec/support/selenium-webdriver.rb
+++ spec/support/selenium-webdriver.rb
@@ -3,13 +3,13 @@
 RSpec.configure do |config|
   config.before(:each, type: :system) do
     # Chrome headless
-    driven_by :selenium_chrome_headless
+    driven_by :selenium, using: :headless_chrome
     # Chrome non-headless
-    # driven_by :selenium_chrome
+    # driven_by :selenium, using: :chrome
     # Firefox headless
-    # driven_by :selenium_headless
+    # driven_by :selenium, using: :headless_firefox
     # Firefox non-headless
-    # driven_by :selenium
+    # driven_by :selenium, using: :firefox
     # Rack test
     # driven_by :rack_test
   end
