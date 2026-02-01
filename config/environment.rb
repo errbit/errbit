409c0af4456c74c29ab9fb43a67d18bfdb4b2ad6
Igor Zubkov <igor.zubkov@gmail.com>
2026-02-01 14:43:28 +0100

    Update

--- config/environment.rb
+++ config/environment.rb
@@ -6,16 +6,16 @@ require_relative "application"
 # Load up Config with values from the environment
 require_relative "initializers/configurate"
 
-# case Errbit::Config.log_location
-# when "STDOUT"
-#   # Skip. This is rails default behavior
-# when "Syslog::Logger"
-#   require "syslog/logger"
-#
-#   Rails.logger = Syslog::Logger.new("errbit", Syslog::LOG_LOCAL0)
-# else
-#   Rails.logger = ActiveSupport::Logger.new(Errbit::Config.log_location)
-# end
+case Config.errbit.log_location
+when "STDOUT"
+  # Skip. This is rails default behavior
+when "Syslog::Logger"
+  require "syslog/logger"
+
+  Rails.logger = Syslog::Logger.new("errbit", Syslog::LOG_LOCAL0)
+else
+  Rails.logger = ActiveSupport::Logger.new(Config.errbit.log_location)
+end
 
 # Initialize the Rails application.
 Rails.application.initialize!
