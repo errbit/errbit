19658d1fe4b817ac0ad9c45dbc506200366f9590
Igor Zubkov <igor.zubkov@gmail.com>
2026-02-01 20:18:29 +0100

    Update

--- config/initializers/configurate.rb
+++ config/initializers/configurate.rb
@@ -1,32 +1,32 @@
 # frozen_string_literal: true
 
-require "configurate/provider/toml"
-
-Config ||= Configurate::Settings.create do
-  add_provider Configurate::Provider::Env
-  add_provider Configurate::Provider::Dynamic
-  add_provider Configurate::Provider::TOML,
-    Rails.root.join("config", "errbit.toml"), required: true
-end
-
-if ENV.fetch("MONGODB_URI", nil).present?
-  Config.errbit.mongo_url = ENV["MONGODB_URI"]
-end
-
-if ENV.fetch("MONGOLAB_URI", nil).present?
-  Config.errbit.mongo_url = ENV["MONGOLAB_URI"]
-end
-
-if ENV.fetch("MONGOHQ_URL", nil).present?
-  Config.errbit.mongo_url = ENV["MONGOHQ_URL"]
-end
-
-if ENV.fetch("MONGODB_URL", nil).present?
-  Config.errbit.mongo_url = ENV["MONGODB_URL"]
-end
-
-if ENV.fetch("MONGO_URL", nil).present?
-  # warning about deprecated ENV variable
-
-  Config.errbit.mongo_url = ENV["MONGO_URL"]
-end
+# require "configurate/provider/toml"
+#
+# Config ||= Configurate::Settings.create do
+#   add_provider Configurate::Provider::Env
+#   add_provider Configurate::Provider::Dynamic
+#   add_provider Configurate::Provider::TOML,
+#     Rails.root.join("config", "errbit.toml"), required: true
+# end
+#
+# if ENV.fetch("MONGODB_URI", nil).present?
+#   Config.errbit.mongo_url = ENV["MONGODB_URI"]
+# end
+#
+# if ENV.fetch("MONGOLAB_URI", nil).present?
+#   Config.errbit.mongo_url = ENV["MONGOLAB_URI"]
+# end
+#
+# if ENV.fetch("MONGOHQ_URL", nil).present?
+#   Config.errbit.mongo_url = ENV["MONGOHQ_URL"]
+# end
+#
+# if ENV.fetch("MONGODB_URL", nil).present?
+#   Config.errbit.mongo_url = ENV["MONGODB_URL"]
+# end
+#
+# if ENV.fetch("MONGO_URL", nil).present?
+#   # warning about deprecated ENV variable
+#
+#   Config.errbit.mongo_url = ENV["MONGO_URL"]
+# end
