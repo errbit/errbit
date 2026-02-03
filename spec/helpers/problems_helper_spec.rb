5351a949ea6be781e11e26bf9b70d52eb0daf45c
Igor Zubkov <igor.zubkov@gmail.com>
2026-02-03 22:34:30 +0100

    Update

--- spec/helpers/problems_helper_spec.rb
+++ spec/helpers/problems_helper_spec.rb
@@ -17,21 +17,21 @@ RSpec.describe ProblemsHelper, type: :helper do
 
   describe "#gravatar_tag" do
     let(:email) { "gravatar@example.com" }
+
     let(:email_hash) { Digest::MD5.hexdigest(email) }
+
     let(:base_url) { "https://secure.gravatar.com/avatar/#{email_hash}" }
 
     context "default config" do
-      before do
-        Rails.configuration.errbit.use_gravatar = true
-      end
-
       it "should render image_tag with correct alt and src" do
         expected = "<img alt=\"#{email}\" class=\"gravatar\" src=\"#{base_url}?d=identicon&amp;s=48\" />"
+
         expect(helper.gravatar_tag(email, s: 48)).to eq(expected)
       end
 
       it "should override :d" do
         expected = "<img alt=\"#{email}\" class=\"gravatar\" src=\"#{base_url}?d=retro&amp;s=48\" />"
+
         expect(helper.gravatar_tag(email, d: "retro", s: 48)).to eq(expected)
       end
     end
@@ -53,11 +53,8 @@ RSpec.describe ProblemsHelper, type: :helper do
     end
 
     context "with email" do
-      before do
-        Rails.configuration.errbit.use_gravatar = true
-      end
-
       let(:email) { "gravatar@example.com" }
+
       let(:email_hash) { Digest::MD5.hexdigest(email) }
 
       it "should return the https url" do
