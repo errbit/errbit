e6a72331375ff70ea2b656d1712721512e3b4573
Igor Zubkov <igor.zubkov@gmail.com>
2026-02-06 20:19:28 +0100

    Add :openid_connect to devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -302,6 +302,23 @@ Devise.setup do |config|
       google_options
   end
 
+  if ENV.fetch("OIDC_ENABLED", "false") == "true"
+    config.omniauth :openid_connect, {
+      name: :my_provider,
+      scope: [:openid, :email, :profile, :address],
+      response_type: :code,
+      uid_field: "preferred_username",
+      client_options: {
+        port: 443,
+        scheme: "https",
+        host: "myprovider.com",
+        identifier: ENV["OP_CLIENT_ID"],
+        secret: ENV["OP_SECRET_KEY"],
+        redirect_uri: "http://myapp.com/users/auth/openid_connect/callback",
+      }
+    }
+  end
+
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
