bc2394e8eb8bcd370621350c32fcafd5efad90ca
Igor Zubkov <igor.zubkov@gmail.com>
2026-02-07 21:43:28 +0100

    Update

--- spec/system/sign_in_with_openid_connect_spec.rb
+++ spec/system/sign_in_with_openid_connect_spec.rb
@@ -7,29 +7,41 @@ RSpec.describe "Sign in with OpenID Connect", type: :system, retry: 3 do
 
   before { expect(Errbit::Config).to receive(:oidc_site_title).and_return("OpenID Connect").at_least(:once).times }
 
-  let!(:user) { create(:user, email: "me@example.com") }
-
-  before do
-    OmniAuth.config.mock_auth[:openid_connect] = OmniAuth::AuthHash.new(
-      {
-        provider: "openid_connect",
-        info: {
-          email: "me@example.com",
-          name: "Jon Snow"
-        }
-      }
-    )
-  end
-
   after { OmniAuth.config.mock_auth[:openid_connect] = nil }
 
   context "when user is registered" do
+    let!(:user) { create(:user, email: "me@example.com") }
+
+    before do
+      OmniAuth.config.mock_auth[:openid_connect] = OmniAuth::AuthHash.new(
+        {
+          provider: "openid_connect",
+          info: {
+            email: "me@example.com",
+            name: "Jon Snow"
+          }
+        }
+      )
+    end
+
+    after { OmniAuth.config.mock_auth[:openid_connect] = nil }
+
     it "is expected to create an account" do
       visit root_path
 
       click_link "Sign in with OpenID Connect"
 
       expect(page).to have_content(I18n.t("devise.omniauth_callbacks.success", kind: "OpenID Connect"))
+    end
+  end
+
+  context "when user is not registered" do
+    it "is expected to reject unrecognized user" do
+      visit root_path
+
+      click_link "Sign in with OpenID Connect"
+
+      expect(page).to have_content("There are no authorized users with OpenID Connect login")
     end
   end
 end
