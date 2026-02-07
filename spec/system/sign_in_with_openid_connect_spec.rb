47ed7e14f63a476a7d0ca5a4a59a16ea1d9a409f
Igor Zubkov <igor.zubkov@gmail.com>
2026-02-07 21:09:46 +0100

    Update

--- spec/system/sign_in_with_openid_connect_spec.rb
+++ spec/system/sign_in_with_openid_connect_spec.rb
@@ -5,6 +5,10 @@ require "rails_helper"
 RSpec.describe "Sign in with OpenID Connect", type: :system, retry: 3 do
   before { expect(Errbit::Config).to receive(:oidc_enabled).and_return(true).at_least(:once).times }
 
+  before { expect(Errbit::Config).to receive(:oidc_site_title).and_return("OpenID Connect").at_least(:once).times }
+
+  let!(:user) { create(:user, email: "me@example.com") }
+
   before do
     OmniAuth.config.mock_auth[:openid_connect] = OmniAuth::AuthHash.new(
       {
