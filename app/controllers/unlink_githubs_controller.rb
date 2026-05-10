829d5faada8c5b0a28ddd997714710056c3fcfde
Igor Zubkov <igor.zubkov@gmail.com>
2026-05-10 18:14:36 +0200

    Fix rubocop warnings

--- app/controllers/unlink_githubs_controller.rb
+++ app/controllers/unlink_githubs_controller.rb
@@ -2,7 +2,7 @@
 
 class UnlinkGithubsController < ApplicationController
   def update
-    @user = User.find(params[:user_id])
+    @user = User.find(params.expect(:user_id))
 
     authorize @user
 
