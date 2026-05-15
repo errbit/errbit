df9e0a5cafc8a88aedaac5387d91df6a943e403d
Igor Zubkov <igor.zubkov@gmail.com>
2026-05-15 18:42:46 +0200

    Update

--- spec/requests/unlink_githubs_controller_spec.rb
+++ spec/requests/unlink_githubs_controller_spec.rb
@@ -5,51 +5,42 @@ require "rails_helper"
 RSpec.describe UnlinkGithubsController, type: :request do
   describe "#update" do
     context "when user is logged in" do
-      context "when user has access" do
-        let!(:current_user) { create(:user, github_login: "biow0lf", github_oauth_token: "token123") }
+      context "when user unlinks their own GitHub account" do
+        let!(:current_user) { create(:errbit_user, github_login: "biow0lf", github_oauth_token: "token123") }
 
         before { sign_in(current_user) }
-
         before { patch user_unlink_github_path(current_user) }
 
-        it "is expected to unlink github for a user with status found" do
+        it "clears github_login and github_oauth_token and redirects to user_path" do
           expect(response).to redirect_to(user_path(assigns(:user)))
-
           expect(response).to have_http_status(:found)
-
-          expect(assigns(:user).github_login).to eq(nil)
-
-          expect(assigns(:user).github_oauth_token).to eq(nil)
+          expect(assigns(:user).github_login).to be_nil
+          expect(assigns(:user).github_oauth_token).to be_nil
         end
       end
 
-      context "when user has not access" do
-        let!(:current_user) { create(:user, admin: false) }
-
-        let!(:user) { create(:user) }
+      context "when user is not an admin and unlinks another user's GitHub account" do
+        let!(:current_user) { create(:errbit_user, admin: false) }
+        let!(:user) { create(:errbit_user) }
 
         before { sign_in(current_user) }
-
         before { patch user_unlink_github_path(user) }
 
-        it "is expected to redirect to root path with status found" do
+        it "redirects to root with a not-authorized flash" do
           expect(response).to redirect_to(root_path)
-
           expect(response).to have_http_status(:found)
-
           expect(request.flash[:alert]).to eq("You are not authorized to perform this action.")
         end
       end
     end
 
     context "when user is not logged in" do
-      let(:user) { create(:user) }
+      let(:user) { create(:errbit_user) }
 
       before { patch user_unlink_github_path(user) }
 
-      it "is expected to redirect to new user session path with status found" do
+      it "redirects to the sign-in page" do
         expect(response).to redirect_to(new_user_session_path)
-
         expect(response).to have_http_status(:found)
       end
     end
