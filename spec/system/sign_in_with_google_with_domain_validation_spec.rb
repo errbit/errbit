e61b9c5b21285c49cf52afee49311ebe4185a15f
Igor Zubkov <igor.zubkov@gmail.com>
2026-05-16 02:25:30 +0200

    Update

--- spec/system/sign_in_with_google_with_domain_validation_spec.rb
+++ spec/system/sign_in_with_google_with_domain_validation_spec.rb
@@ -4,17 +4,14 @@ require "rails_helper"
 
 RSpec.describe "Sign in with Google with domain validation", type: :system, retry: 3 do
   before { expect(Errbit::Config).to receive(:google_authentication).and_return(true).at_least(:once).times }
-
   before { expect(Errbit::Config).to receive(:google_auto_provision).and_return(true) }
-
   before { expect(Errbit::Config).to receive(:google_authorized_domains).and_return("example.com").twice }
 
-  context "create an account for recognized user if their account email is from a trusted domain" do
+  context "with a Google email from an authorized domain" do
     before { OmniAuth.config.mock_auth[:google_oauth2] = Faker::Omniauth.google(email: "me@example.com", uid: "123456789") }
-
     after { OmniAuth.config.mock_auth[:google_oauth2] = nil }
 
-    it "is expected to create an account" do
+    it "auto-provisions the Errbit::User and signs them in" do
       visit root_path
 
       click_link "Sign in with Google"
@@ -23,12 +20,11 @@ RSpec.describe "Sign in with Google with domain validation", type: :system, retr
     end
   end
 
-  context "don't create an account for user if their account email is from an unauthorized domain" do
+  context "with a Google email from an unauthorized domain" do
     before { OmniAuth.config.mock_auth[:google_oauth2] = Faker::Omniauth.google(email: "me@mail.example.com", uid: "123456789") }
-
     after { OmniAuth.config.mock_auth[:google_oauth2] = nil }
 
-    it "is expected to not create an account" do
+    it "does not create an account and shows the domain-unauthorized error" do
       visit root_path
 
       click_link "Sign in with Google"
