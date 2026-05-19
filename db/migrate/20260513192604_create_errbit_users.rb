dc372a264d03863e0289f9127e0e76f84bab4381
Igor Zubkov <igor.zubkov@gmail.com>
2026-05-19 12:54:55 +0200

    Warnings

--- db/migrate/20260513192604_create_errbit_users.rb
+++ db/migrate/20260513192604_create_errbit_users.rb
@@ -17,11 +17,11 @@ class CreateErrbitUsers < ActiveRecord::Migration[8.1]
       t.datetime :remember_created_at
 
       ## Trackable
-      t.integer  :sign_in_count, default: 0, null: false
+      t.integer :sign_in_count, default: 0, null: false
       t.datetime :current_sign_in_at
       t.datetime :last_sign_in_at
-      t.string   :current_sign_in_ip
-      t.string   :last_sign_in_ip
+      t.string :current_sign_in_ip
+      t.string :last_sign_in_ip
 
       ## Token authenticatable
       t.string :authentication_token
