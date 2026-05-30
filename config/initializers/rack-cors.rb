eeb1d3e0b22252bfbd0ae1b97b51870ebd8cf371
Igor Zubkov <igor.zubkov@gmail.com>
2026-05-30 04:05:54 +0200

    Configure rack-cors

---
+++ config/initializers/rack-cors.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*'
+    resource '*', headers: :any, methods: [:get, :post, :patch, :put]
+  end
+end
