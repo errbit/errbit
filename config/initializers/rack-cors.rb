9e305c5ea15e2720c56f03db356d63a26b2cc34c
Igor Zubkov <igor.zubkov@gmail.com>
2026-05-31 16:48:52 +0200

    Update

--- config/initializers/rack-cors.rb
+++ config/initializers/rack-cors.rb
@@ -2,7 +2,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
-    resource '*', headers: :any, methods: [:get, :post, :patch, :put]
+    origins "*"
+
+    resource "*", headers: :any, methods: [:get, :post, :patch, :put]
   end
 end
