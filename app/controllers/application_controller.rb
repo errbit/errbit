42224dc137896bb582f5e426c7d4506f4399dd93
Igor Zubkov <igor.zubkov@gmail.com>
2026-06-14 22:43:13 +0200

    Update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery
 
+  before_action :store_user_location!, if: :storable_location?
   before_action :authenticate_user_from_token!
   before_action :authenticate_user!
   before_action :set_time_zone
@@ -36,5 +37,18 @@ class ApplicationController < ActionController::Base
     flash[:alert] = t("controllers.application.user_not_authorized")
 
     redirect_to root_path
+  end
+
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+  end
+
+  def store_user_location!
+    # :user is the scope we are authenticating
+    store_location_for(:user, request.fullpath)
+  end
+
+  def after_sign_in_path_for(resource_or_scope)
+    stored_location_for(resource_or_scope) || super
   end
 end
