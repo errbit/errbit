12a728c973b9c033c74194c161a9e123e5031baa
Igor Zubkov <igor.zubkov@gmail.com>
2026-06-14 21:58:55 +0200

    Update

--- spec/system/well_known_change_password_spec.rb
+++ spec/system/well_known_change_password_spec.rb
@@ -3,22 +3,30 @@
 require "rails_helper"
 
 RSpec.describe "/.well-known/change-password", type: :system do
-  it "redirects an already logged-in user to the change password form" do
-    user = create(:user, email: "me@example.com", password: "eidii7EeooVe8ahk")
+  context "when user is not logged in" do
+    it "redirects an already logged-in user to the change password form" do
+      user = create(:user, email: "me@example.com", password: "eidii7EeooVe8ahk")
 
-    visit new_user_session_path
+      visit new_user_session_path
 
-    fill_in "Email", with: "me@example.com"
-    fill_in "Password", with: "eidii7EeooVe8ahk"
+      fill_in "Email", with: "me@example.com"
+      fill_in "Password", with: "eidii7EeooVe8ahk"
 
-    click_button I18n.t("devise.sessions.new.sign_in")
+      click_button I18n.t("devise.sessions.new.sign_in")
 
-    expect(page).to have_text(I18n.t("devise.sessions.signed_in"))
+      expect(page).to have_text(I18n.t("devise.sessions.signed_in"))
 
-    visit "/.well-known/change-password"
+      visit "/.well-known/change-password"
 
-    find_by_id("edit_user")
+      find_by_id("edit_user")
 
-    expect(page).to have_current_path("/users/#{user.id}/edit")
+      expect(page).to have_current_path("/users/#{user.id}/edit")
+    end
+  end
+
+  context "when user is logged in" do
+    it "sign in and redirect to change password form" do
+
+    end
   end
 end
