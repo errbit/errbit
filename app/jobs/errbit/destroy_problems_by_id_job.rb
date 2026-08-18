14fde1fee37e35c5d3c1a676b3da7d1161c04c0c
Mikel Olasagasti Uranga <mikel@olasagasti.info>
2026-08-18 12:20:55 +0200

    Add async jobs for problem deletion (by app and by ID)
    Port async problem deletion jobs to SQL. `DestroyProblemsByIdJob`
    accepts an array of problem IDs and destroys them sequentially.
    `DestroyProblemsByAppJob` finds all problems for an app and
    destroys them. Both use the SQL `Errbit::ProblemDestroy` interactor.

---
+++ app/jobs/errbit/destroy_problems_by_id_job.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Errbit
+  class DestroyProblemsByIdJob < ApplicationJob
+    queue_as :default
+
+    def perform(problem_ids)
+      problems = Errbit::Problem.where(id: problem_ids).to_a
+
+      Errbit::ProblemDestroy.execute(problems)
+    end
+  end
+end
