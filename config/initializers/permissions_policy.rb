ecf6728a81556a94ccb2c443226b44c0ad196469
Igor Zubkov <igor.zubkov@gmail.com>
2026-08-26 15:34:23 +0200

    Configure feature policy

--- config/initializers/permissions_policy.rb
+++ config/initializers/permissions_policy.rb
@@ -5,11 +5,11 @@
 # Define an application-wide HTTP permissions policy. For further
 # information see: https://developers.google.com/web/updates/2018/06/feature-policy
 
-# Rails.application.config.permissions_policy do |policy|
-#   policy.camera      :none
-#   policy.gyroscope   :none
-#   policy.microphone  :none
-#   policy.usb         :none
-#   policy.fullscreen  :self
-#   policy.payment     :self, "https://secure.example.com"
-# end
+Rails.application.config.permissions_policy do |policy|
+  policy.camera      :none
+  policy.gyroscope   :none
+  policy.microphone  :none
+  policy.usb         :none
+  policy.fullscreen  :none
+  policy.payment     :none
+end
