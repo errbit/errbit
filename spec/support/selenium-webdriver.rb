b1f76689a43fea19202ea26276066c9477be1374
Igor Zubkov <igor.zubkov@gmail.com>
2026-09-18 19:50:54 +0200

    Rollback

--- spec/support/selenium-webdriver.rb
+++ spec/support/selenium-webdriver.rb
@@ -3,11 +3,11 @@
 RSpec.configure do |config|
   config.before(:each, type: :system) do
     # Chrome headless
-    # driven_by :selenium_chrome_headless
+    driven_by :selenium_chrome_headless
     # Chrome non-headless
     # driven_by :selenium_chrome
     # Firefox headless
-    driven_by :selenium_headless
+    # driven_by :selenium_headless
     # Firefox non-headless
     # driven_by :selenium
     # Rack test
