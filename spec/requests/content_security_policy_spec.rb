0aa31faf6ca1dc7b757565f1e8fb64d51abd4167
Igor Zubkov <igor.zubkov@gmail.com>
2026-09-22 19:49:51 +0200

    Cleanup

--- spec/requests/content_security_policy_spec.rb
+++ spec/requests/content_security_policy_spec.rb
@@ -3,14 +3,6 @@
 require "rails_helper"
 
 RSpec.describe "Content Security Policy", type: :request do
-  it "includes the nonce on executable script tags" do
-    get new_user_session_path
-
-    nonce = response.headers.fetch("Content-Security-Policy").match(/nonce-([^' ]+)/)[1]
-
-    expect(response.body).to match(/<script[^>]+nonce="#{Regexp.escape(nonce)}"/)
-  end
-
   it "generates a different nonce for each response" do
     get new_user_session_path
     first_nonce = response.headers.fetch("Content-Security-Policy").match(/nonce-([^' ]+)/)[1]
